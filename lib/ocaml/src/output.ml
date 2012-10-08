e411919f832575daecef35f16b9621d39e465956
justus <jm614@cam.ac.uk>
2012-10-08 18:19:28 +0100

    Removed batteries to make Kayvan happy!

--- lib/ocaml/src/output.ml
+++ lib/ocaml/src/output.ml
@@ -3,6 +3,7 @@ open Pervasives_
 type t = string
 
 let file name = name
-
 let write s name =
-  BatFile.with_file_out name (fun o -> BatIO.nwrite o s)
+  let channel = open_out name in  
+  output_string channel;
+  close_out channel
