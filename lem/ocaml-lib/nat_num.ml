976913a4c1e739598a7eea6e6ad69fcc6f96d3c7
Kayvan Memarian <kmemarian@users.noreply.github.com>
2012-10-09 16:35:40 +0100

    correcting the subtraction function in lem.

--- lem/ocaml-lib/nat_num.ml
+++ lem/ocaml-lib/nat_num.ml
@@ -4,12 +4,7 @@ let (<=) = (<=)
 let (>) = (>)
 let (>=) = (>=)
 let (+) = (+)
-let (-) x y =
-  let d = x - y in
-    if d < 0 then
-      0
-    else
-      d
+let (-) = (-)
 let ( * ) = ( * )
 let (/) = (/)
 let (land) = (land)
