82ef434f34f02627127654f6e3d85e68ed8bdbc8
Kayvan Memarian <kmemarian@users.noreply.github.com>
2012-10-11 01:19:31 +0100

    Strangely, substituting symbolic names works better with an equality test ?

--- lib/ocaml/src/boot.ml
+++ lib/ocaml/src/boot.ml
@@ -1,3 +1,3 @@
 let outOfHomeomorphism msg = Pervasives.failwith ("[OutOfHomeomorphism exception> " ^ msg)
 let debug str = Pervasives.failwith ("\x1b[31mDEBUG: " ^ str ^ "\x1b[0m")
-let print_debug str = Pervasives.print_endline ("\x1b[31mDEBUG: " ^ str ^ "\x1b[0m")
+let print_debug str k = Obj.magic (Pervasives.print_endline ("\x1b[31mDEBUG: " ^ str ^ "\x1b[0m")); k
