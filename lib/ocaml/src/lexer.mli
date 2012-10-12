4bd3bcfcbd164885faa89604804b1307ca324856
justus <jm614@cam.ac.uk>
2012-10-12 18:42:23 +0100

    Modular parser/lexer wrappers.

--- lib/ocaml/src/lexer.mli
+++ lib/ocaml/src/lexer.mli
@@ -1,9 +1,23 @@
-type token_stream = unit -> Cparser.token
+module type LEXER_BASE = sig
+  type token
+  val init : Lexing.lexbuf -> token
+end
+module ClexerBase : (LEXER_BASE with type token = Cparser.token)
 
-type t
+module type LEXER = sig
+  type token
+  type token_stream = unit -> token
 
-val name : t -> string
-val lexbuf : (Lexing.lexbuf -> 'a) -> t -> 'a
-val stream : (token_stream -> 'a)  -> t -> 'a
+  type t
 
-val make : Input.t -> t
+  val init : Lexing.lexbuf -> token
+
+  val name : t -> string
+  val lexbuf : (Lexing.lexbuf -> 'a) -> t -> 'a
+  val stream : (token_stream -> 'a)  -> t -> 'a
+
+  val make : Input.t -> t
+end
+
+module MakeLexer (L : LEXER_BASE) : (LEXER with type token = L.token)
+module Clexer : (LEXER with type token = ClexerBase.token)
--- lib/ocaml/src/parser.ml
+++ lib/ocaml/src/parser.ml
@@ -1,14 +1,54 @@
 open Pervasives_
 open Exception.Operators
 
-let parse_exn name lexbuf =
-  try
-    let file = Cparser.start Clexer.initial lexbuf in
-    Exception.return (name, file)
-  with Cparser.Error ->
-    let token = Lexing.lexeme lexbuf in
-    Exception.throw (Errors.PARSER ("Unexpected token: " ^ token ^ "."))
+module type PARSER_BASE = sig
+  exception Error
+  type token
+  type result
 
-let parse lexer =
-  let name = Lexer.name lexer in
-  Lexer.lexbuf (parse_exn name) lexer
+  val init : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> result
+end
+
+module CparserBase = struct
+  exception Error = Cparser.Error
+  type result = Cabs.g_defn_l list
+  type token = Cparser.token
+
+  let init = Cparser.start
+end
+
+module type PARSER = functor (L : Lexer.LEXER) ->
+sig
+  type result
+  val parse : L.t -> (result, Errors.cause) Exception.t
+end
+
+
+module type MAKE_PARSER =
+  functor (P : PARSER_BASE) ->
+  functor (L : Lexer.LEXER with type token = P.token) ->
+  sig
+    type result = string * P.result
+    val parse : L.t -> (result, Errors.cause) Exception.t
+  end
+
+module MakeParser
+  (P : PARSER_BASE)
+  (L : Lexer.LEXER with type token = P.token) =
+struct
+  type result = string * P.result
+
+  let parse_exn name lexbuf =
+    try
+      let result = P.init L.init lexbuf in
+      Exception.return (name, result)
+    with P.Error ->
+      let token = Lexing.lexeme lexbuf in
+      Exception.throw (Errors.PARSER ("Unexpected token: " ^ token ^ "."))
+
+  let parse lexer =
+    let name = L.name lexer in
+    L.lexbuf (parse_exn name) lexer
+end
+
+module Cparser = MakeParser (CparserBase) (Lexer.Clexer)
