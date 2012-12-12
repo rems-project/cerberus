7e949fe1a6d794ddb44ecd17a3543b6cdf2d7ad6
justus <jm614@cam.ac.uk>
2012-12-12 17:10:00 +0000

    Updated script for downloading internal WG14 correspondence.

--- tools/load-wg14.sh
+++ tools/load-wg14.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-PATH=~/wg14
-CUR=`/usr/bin/ls -1v $PATH/new/ | /usr/bin/tail -n 1`
+DIR=/home/jm/work/wg14
+CUR=$(/usr/bin/ls -1v "$DIR"/new/ | /usr/bin/tail -n 1)
+
+if [[ -z "$CUR" ]]; then
+  CUR=0
+fi
 
 STATUS=0
 
 while [ "$STATUS" == "0" ]
 do
-CUR=$(($CUR + 1))
-/usr/bin/wget http://www.open-std.org/jtc1/sc22/wg14/$CUR
-STATUS=$?
+  CUR=$(($CUR + 1))
+  curl -sf http://www.open-std.org/jtc1/sc22/wg14/$CUR -o "$DIR"/new/$CUR
+  STATUS=$?
 done
