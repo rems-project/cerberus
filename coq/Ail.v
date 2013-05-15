207c17e87007f40b1d905a5009910a710d6a83cf
justus <jm614@cam.ac.uk>
2013-05-15 22:03:37 +0100

    Removed outdated copy of type system.

--- coq/Ail.v
+++
@@ -1,8 +0,0 @@
-Require Export AilTypes.
-Require Export AilSyntax.
-Require Export AilAux.
-Require Export Implementation.
-Require Export AilTypingAux.
-Require Export AilWf.
-Require Export AilTyping.
-
