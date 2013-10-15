84191f383af7bbfc1700b2b5b941fa3b187c735d
Kyndylan Nienhuis <8352257+kyndylan@users.noreply.github.com>
2013-10-15 15:39:18 +0100

    Kayvan added stdatomic.h

--- tests/concurrency/LB+acq_rel+acq_rel.c
+++ tests/concurrency/LB+acq_rel+acq_rel.c
@@ -1,12 +1,14 @@
+#include <stdatomic.h>
 // LB+acq_rel+acq_rel
 // Load Buffering, with acquire/release pairs
 // The values of z1 and z2 cannot both be 1. They can be both 0, or one of them can be 1.
 // An exhaustive execution of this program should therefore return the values 0, 1 and 2.
 int main() {
-  atomic_int x=0; atomic_int y=0; int z1; int z2;
-  {{{ { z1=x.load(memory_order_acquire); 
-        y.store(1,memory_order_release); }
-  ||| { z2=y.load(memory_order_acquire);
-        x.store(1,memory_order_release); }  }}}
+  atomic_int x=0, y=0;
+  int z1, z2;
+  {{{ { z1 = atomic_load_explicit(&x, memory_order_acquire); 
+        atomic_store_explicit(&y, 1, memory_order_release); }
+  ||| { z2 = atomic_load_explicit(&y, memory_order_acquire);
+        atomic_store_explicit(&x, 1, memory_order_release); }  }}}
   return z1 + (2 * z2);
 }
