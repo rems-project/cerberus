0b4099c10fe66165a6c46621ce3f6eb471a87b1f
Kyndylan Nienhuis <8352257+kyndylan@users.noreply.github.com>
2013-11-05 12:58:00 +0000

    New tests

--- src/tests.ml
+++ src/tests.ml
@@ -78,11 +78,11 @@ let get_test (file_name:string)
 let get_tests: test list = 
   [get_test "tests/concurrency/coherence+rel_rel_acq.core" [1] [];
    get_test "tests/concurrency/coherence+init+rel_acq.core" [1] [];
-   get_test "tests/concurrency/coherence+rel_rel+rel_rel_acq.core" [0; 1; 3] [];
    get_test "tests/concurrency/datarace+Rna+Rna.core" [0] [];
    get_test "tests/concurrency/datarace+Wna+Wna.core" [] [[Undefined.Data_race]];
    get_test "tests/concurrency/datarace+Rna+Wna.core" [] [[Undefined.Data_race]];
    get_test "tests/concurrency/datarace+Rna+Rna_Wna.core" [] [[Undefined.Data_race]];
+   get_test "tests/concurrency/datarace+Wna_rel+acq_Rna.core" [0] [[Undefined.Data_race]];
    get_test "tests/concurrency/hb-mo-cycle+rel_rel_acq+rel_rel_acq.core" [0; 1; 2; 3] [];
    get_test "tests/concurrency/MP+na_rel+acq_na.core" [1; 2] [];
    get_test "tests/concurrency/LB+acq_rel+acq_rel.core" [0; 1; 2] [];
