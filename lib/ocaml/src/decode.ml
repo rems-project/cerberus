30d1a09c8338e5cd54ebf0d78b0c8a13e3742377
Kayvan Memarian <kmemarian@users.noreply.github.com>
2013-11-23 02:35:37 +0000

    now it should compile on other machines then AddaX ...

--- lib/ocaml/src/decode.ml
+++ lib/ocaml/src/decode.ml
@@ -15,11 +15,11 @@ let decode_integer_constant str =
     if str.[0] = '0' then
       let l = String.length str in
       if String.length str > 1 && (str.[1] = 'x' || str.[1] = 'X') then
-        (String.sub str 2 (l-2), 16, Ail.HEXADECIMAL)
+        (String.sub str 2 (l-2), 16, () (* Ail.HEXADECIMAL *))
       else
-        (String.sub str 1 (l-1), 8, Ail.OCTAL)
+        (String.sub str 1 (l-1), 8, () (* Ail.OCTAL *))
     else
-      (str, 10, Ail.DECIMAL) in
+      (str, 10, () (* Ail.DECIMAL *)) in
   let l = String.length str in
   let ret = ref (num_of_int 0) in
   String.iteri (fun i c -> ret := (num_of_int (read_digit c)) */ (power_num (num_of_int basisN) (num_of_int (l - i - 1))) +/ !ret) str;
