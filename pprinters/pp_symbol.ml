858ec007514845e3706d71bfd315387241c7b419
Kayvan Memarian <kmemarian@users.noreply.github.com>
2014-03-04 18:38:51 +0000

    correcting a error un core stdlib xor function

--- pprinters/pp_symbol.ml
+++ pprinters/pp_symbol.ml
@@ -4,7 +4,7 @@ let to_string (Symbol (n, _)) = "a" ^ "_" ^ string_of_int n
 
 let to_string_pretty (Symbol (n, name_opt) as s) =
   match name_opt with
-    | Some name -> name ^ "{" ^ string_of_int n ^ "}"
+    | Some name -> name (* ^ "{" ^ string_of_int n ^ "}" *)
     | None      -> to_string s
 
 let to_string_latex (n, _) =
