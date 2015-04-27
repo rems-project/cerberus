a3940f55249aaeac20e9fa0b9111e157e727ff44
Kyndylan Nienhuis <8352257+kyndylan@users.noreply.github.com>
2015-04-27 11:18:58 +0100

    Concurrency. Snapshot from 2014-12, also covers rlx and fences.

--- concurrency/pp.ml
+++ concurrency/pp.ml
@@ -1,19 +1,45 @@
-open Language
 open Cmm_aux
-open Cmm_csem
-open Opsem
-                        
+open Cmm_master
+
 let memory_order_tostring (mo:memory_order) : string = 
 match mo with 
-  | Cmm_csem.NA      -> "NA"
-  | Cmm_csem.Seq_cst -> "Seq_cst"
-  | Cmm_csem.Relaxed -> "Relaxed"
-  | Cmm_csem.Release -> "Release"
-  | Cmm_csem.Acquire -> "Acquire"
-  | Cmm_csem.Consume -> "Consume"
-  | Cmm_csem.Acq_rel -> "Acq_rel"          
+  | Cmm_master.NA      -> "NA"
+  | Cmm_master.Seq_cst -> "Seq_cst"
+  | Cmm_master.Relaxed -> "Relaxed"
+  | Cmm_master.Release -> "Release"
+  | Cmm_master.Acquire -> "Acquire"
+  | Cmm_master.Consume -> "Consume"
+  | Cmm_master.Acq_rel -> "Acq_rel"
+  
+let lock_outcome_tostring (l:lock_outcome) : string =
+  match l with
+  | Locked  -> "Locked"
+  | Blocked -> "Blocked"
 
-let rec int_exp_tostring (e:e) : string = 
+let action_tostring (a:action) : string = 
+  match a with
+  | Load (aid, tid, mo, loc, value)         -> Printf.sprintf "Load %s %i %i (aid: %i, thread %i)"
+                                               (memory_order_tostring mo) loc value aid tid
+  | Store (aid, tid, mo, loc, value)        -> Printf.sprintf "Store %s %i %i (aid: %i, thread %i)"
+                                               (memory_order_tostring mo) loc value aid tid
+  | Lock (aid, tid, loc, outcome)           -> Printf.sprintf "Lock %i %s (aid: %i, thread %i)"
+                                               loc (lock_outcome_tostring outcome) aid tid
+  | Unlock (aid, tid, loc)                  -> Printf.sprintf "Unlock %i (aid: %i, thread %i)"
+                                               loc aid tid
+  | RMW (aid, tid, mo, loc, value1, value2) -> Printf.sprintf "RMW %s %i %i %i (aid: %i, thread %i)"
+                                               (memory_order_tostring mo) loc value1 value2 aid tid
+  | Blocked_rmw (aid, tid, loc)             -> Printf.sprintf "Blocked_rmw %i (aid: %i, thread %i)"
+                                               loc aid tid
+  | Fence (aid, tid, mo)                    -> Printf.sprintf "Fence %s (aid: %i, thread %i)"
+                                               (memory_order_tostring mo) aid tid
+  | Alloc (aid, tid, loc)                   -> Printf.sprintf "Alloc %i (aid: %i, thread %i)"
+                                               loc aid tid
+  | Dealloc (aid, tid, loc)                 -> Printf.sprintf "Dealloc %i (aid: %i, thread %i)"
+                                               loc aid tid
+
+(*
+
+let rec int_exp_tostring (e:expression) : string = 
   match e with
   | E_Int n          -> Printf.sprintf "%i" n
   | E_Var v          -> Printf.sprintf "%s" v
@@ -24,7 +50,7 @@ let rec int_exp_tostring (e:e) : string =
                         (int_exp_tostring e1)
                         (int_exp_tostring e2)
                                             
-let rec bool_exp_tostring (be:be) : string = 
+let rec bool_exp_tostring (be:booleanExpression) : string = 
   match be with
   | Be_True          -> Printf.sprintf "true" 
   | Be_False         -> Printf.sprintf "false" 
@@ -42,28 +68,28 @@ let rec bool_exp_tostring (be:be) : string =
                         (bool_exp_tostring e1)
                         (bool_exp_tostring e2)
 
-let rec statement_tostring (stmt:statement) : string = 
+let rec statement_tostring (stmt:command) : string = 
   match stmt with
-  | S_Skip                    -> Printf.sprintf "skip"
-  | S_Assign (v, e)           -> Printf.sprintf "%s := %s" v (int_exp_tostring e)
-  | S_Cond (be, stmt1, stmt2) -> Printf.sprintf "if %s then %s else %s" 
+  | C_Skip                    -> Printf.sprintf "skip"
+  | C_Store (v, e, mo)        -> Printf.sprintf "%s := %s (%s)" 
+                                                v 
+                                                (int_exp_tostring e) 
+                                                (memory_order_tostring mo)
+  | C_Load (v, e, mo, stmt)   -> Printf.sprintf "%s := %s (%s); %s" 
+                                                v 
+                                                (int_exp_tostring e) 
+                                                (memory_order_tostring mo)
+                                                (statement_tostring stmt)
+  | C_If (be, stmt1, stmt2)   -> Printf.sprintf "if %s then %s else %s" 
                                  (bool_exp_tostring be)
                                  (statement_tostring stmt1)
                                  (statement_tostring stmt2)
-  | S_Seq l                   -> String.concat "; " (List.map (fun i -> statement_tostring i) l)
-  | S_Par (stmt1, stmt2)      -> Printf.sprintf "Thread1: %s\nThread2: %s"
+  | C_Seq (stmt1, stmt2)      -> Printf.sprintf "%s; %s"
                                  (statement_tostring stmt1)
                                  (statement_tostring stmt2) 
-
-let program_tostring (p:program) : string =
-  statement_tostring p  
-  
-let action_tostring (a:action) : string = 
-  match a with
-  | Load (aid, tid, mo, loc, value) -> Printf.sprintf "Load %s %s %i (aid: %i, thread %i)"
-                                       (memory_order_tostring mo) loc value aid tid
-  | Store (aid, tid, mo, loc, value) -> Printf.sprintf "Store %s %s %i (aid: %i, thread %i)"
-                                       (memory_order_tostring mo) loc value aid tid
+  | C_Par (stmt1, stmt2)      -> Printf.sprintf "Thread1: %s\nThread2: %s"
+                                 (statement_tostring stmt1)
+                                 (statement_tostring stmt2) 
 
 let actions_tostring (s:state) : string =
   String.concat ", " (List.map (fun (_, i) -> action_tostring i) 
@@ -75,3 +101,4 @@ let memory_tostring (s:state) : string =
 
 let state_tostring (s:state) : string = 
   Printf.sprintf "Memory: %s\nTrace: %s" (memory_tostring s) (actions_tostring s)
+*)
