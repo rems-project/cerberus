812d40550e50d8cbf6093dcdcb15ed1e85b4a80d
Kayvan Memarian <kmemarian@users.noreply.github.com>
2015-10-06 15:23:24 +0100

    no message

---
+++ pprinters/string_defacto_memory.ml
@@ -0,0 +1,7 @@
+open Pp_defacto_memory
+
+let string_of_pointer_value ptr_val =
+  Pp_utils.to_plain_string (pp_pointer_value ptr_val)
+
+let string_of_mem_value mval =
+  Pp_utils.to_plain_string (pp_mem_value mval)
