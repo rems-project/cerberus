d20390853bac158712e97fc056d033460176dbb0
Kayvan Memarian <kmemarian@users.noreply.github.com>
2016-03-25 04:15:23 +0000

    not sure what this was, but we don't use it anymore

--- lib/ocaml/src/output.ml
+++
@@ -1,9 +0,0 @@
-open Pervasives_
-
-type t = string
-
-let file name = name
-let write s name =
-  let channel = open_out name in  
-  output_string channel s;
-  close_out channel
--- lib/ocaml/src/settings.ml
+++
@@ -1,3 +0,0 @@
-(* A bit silly to have a module just for that ... *)
-
-let debug = ref 0
