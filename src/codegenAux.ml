775140d04f6de64e7858041e056adf76968e4a66
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2016-04-27 17:37:15 +0100

    wtf

--- src/codegenAux.ml
+++ src/codegenAux.ml
@@ -26,26 +26,28 @@ let ivmin = ivctor M.min_ival0 "ivmin"
 let ivmax = ivctor M.max_ival0 "ivmax"
 
 (* Loaded - Specified and unspecified values *)
-(*
+
 type 'a loaded =
   | Specified of 'a
   | Unspecified of C.ctype0
 
 let specified x = Specified x
-*)
+
 (* Cast from memory values *)
 
-let mv_to_integer mv =
+let mv_to_integer_loaded mv =
   match mv with
-    | I.MVinteger (at, iv) -> M.case_integer_value0 iv (fun n -> (* Specified *) iv)
-                                (fun () -> iv) (*Unspecified (C.Basic0 (T.Integer
-                                             at))) *)
-    | _ -> raise (Error "type mismatch")
+  | I.MVinteger (at, iv) ->
+    if M.is_specified_ival iv then
+      Specified iv
+    else
+      Unspecified (C.Basic0 (T.Integer at))
+  | _ -> raise (Error "type mismatch")
 
 let mv_to_pointer mv =
   match mv with
-    | I.MVpointer (cy, pt) -> pt
-    | _ -> raise (Error "type mismatch")
+  | I.MVpointer (cy, pt) -> pt
+  | _ -> raise (Error "type mismatch")
 
 
 (* Cast to memory values *)
@@ -54,21 +56,40 @@ let mv_from_integer at iv =
   match iv with
     | I.IV (_, _) -> I.MVinteger (at, iv)
 
+
+let mk_int s = I.IV (I.Prov_none, I.IVconcrete (Nat_big_num.of_string s))
+
+(* Binary operations wrap *)
+
+let eq n m = O.get (M.eq_ival0 M.initial_mem_state0 Symbolic.Constraints_TODO m n)
+let lt n m = O.get (M.lt_ival0 Symbolic.Constraints_TODO m n)
+let gt n m = O.get (M.lt_ival0 Symbolic.Constraints_TODO n m)
+let le n m = O.get (M.le_ival0 Symbolic.Constraints_TODO m n)
+let ge n m = O.get (M.le_ival0 Symbolic.Constraints_TODO n m)
+
 (* Memory actions wrap *)
 
 let create pre al ty = lift $ M.allocate_static0 0 pre al ty
 
 let alloc pre al n = lift $ M.allocate_dynamic0 0 pre al n
 
-let load_integer ty e = lift $ M.bind1 (M.load0 ty e) (M.return1 % mv_to_integer % snd)
+let load_integer ity e = lift $ M.bind1 (M.load0 (C.Basic0 (T.Integer ity)) e)
+                           (M.return1 % mv_to_integer_loaded % snd)
 
-let load_pointer ty e = lift $ M.bind1 (M.load0 ty e) (M.return1 (*% specified*) % mv_to_pointer % snd)
+let load_pointer q cty e = lift $ M.bind1 (M.load0 (C.Pointer0 (q, cty)) e) (M.return1 % specified % mv_to_pointer % snd)
 
 let store ty e1 e2 = lift $ M.store0 ty e1 e2
 
+let store_integer ity e1 le2 =
+  lift $ M.store0 (C.Basic0 (T.Integer ity)) e1 (
+    match le2 with
+    | Specified e2 -> M.integer_value_mval0 ity e2
+    | Unspecified ty -> M.unspecified_mval0 ty
+  )
+
 (* Cast types functions *)
 
-let int_of_integer_value ((* Specified*) iv) = M.eval_integer_value0 iv |> O.get |> Nat_big_num.to_int
+let int_of_integer_value (Specified iv) = M.eval_integer_value0 iv |> O.get |> Nat_big_num.to_int
 
 let pointer_from_integer_value = function
   | I.IV (p, ivb) -> I.PV (p, I.PVfromint ivb, [])
