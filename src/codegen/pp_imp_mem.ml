e28b20b47d2da053921421efc2ab6eec52b1ccd1
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-04-30 14:47:42 +0100

    mem model based on hashtbl

---
+++ src/codegen/pp_imp_mem.ml
@@ -0,0 +1,14 @@
+open Util
+open Nat_big_num
+open Pp_prelude
+open Imp_mem
+
+let print_opt_num (_,n) = !^(Option.case to_string (fun _ -> "Unspecified") n)
+let pp_pointer_value = print_opt_num
+let pp_integer_value = print_opt_num
+let pp_integer_value_for_core = print_opt_num
+let pp_mem_value mv = !^(impl_prettyStringFromMem_value mv)
+let pp_pretty_pointer_value = print_opt_num
+let pp_pretty_integer_value _ = print_opt_num
+let pp_pretty_mem_value _ = pp_mem_value
+
