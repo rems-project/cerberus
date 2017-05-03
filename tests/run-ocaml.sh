df50284e3d1893b80dcf3425005e2c6b3e96d5ca
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-05-03 18:51:12 +0100

    fix array issue

--- tests/run-ocaml.sh
+++ tests/run-ocaml.sh
@@ -13,18 +13,27 @@ fail=0
 
 function test {
   rm -rf ${1%.c}.native
-  cbuild $1 > /dev/null
-  CERB_BATCH=1 ./${1%.c}.native > result
-  cmp --silent result ../$2/expected/$1.expected
-  if [[ "$?" -eq "0" ]]; then
-    res="\033[1m\033[32mPASSED!\033[0m"
-    pass=$((pass+1))
-  else
-    res="\033[1m\033[31mFAILED!\033[0m"
-    fail=$((fail+1))
+  cbuild $1 > /dev/null 2> /dev/null
+  if [ "$?" -ne "0" ]; then
+    echo -e "Test $1: Build fail..."
+    return
   fi
 
-  echo -e "Test $1: $res"
+  if [ -e ../$2/expected/$1.expected ]; then
+    CERB_BATCH=1 ./${1%.c}.native > result
+    cmp --silent result ../$2/expected/$1.expected
+    if [[ "$?" -eq "0" ]]; then
+      res="\033[1m\033[32mPASSED!\033[0m"
+      pass=$((pass+1))
+    else
+      res="\033[1m\033[31mFAILED!\033[0m"
+      fail=$((fail+1))
+    fi
+    echo -e "Test $1: $res"
+  else
+    echo -e "Test $1: Expected file does not exist..."
+  fi
+
 }
 
 # Running ci tests
