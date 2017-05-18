89d573c41f001bb801c0eac4f91be4556fddf62b
Kayvan Memarian <kmemarian@users.noreply.github.com>
2017-05-18 20:13:28 +0100

    scripts to continously generate and test csmith programs

---
+++ tests/csmith/small_int_arith/run_compare.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cerberus --cpp="cc -E -nostdinc -undef -D__cerb__ -I $CERB_PATH/include/c/libc -I $CERB_PATH/include/c/posix -DCSMITH_MINIMAL -I ../runtime" --exec --batch $@
+clang -DCSMITH_MINIMAL -I ../runtime -w $@ && ./a.out
