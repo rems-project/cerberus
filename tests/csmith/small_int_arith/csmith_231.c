866d6664587055da9675c5de262a147ddf574f55
Kayvan Memarian <kmemarian@users.noreply.github.com>
2017-05-19 01:21:17 +0100

    getting rid of the XX

--- tests/csmith/small_int_arith/csmith_231.c
+++ tests/csmith/small_int_arith/csmith_231.c
@@ -1,4 +1,4 @@
-// Options:   --no-argc --no-arrays --no-bitfields --checksum --concise --no-consts --no-float --no-inline-function --max-array-dim 3 --max-array-len-per-dim 10 --max-block-depth 5 --max-block-size 4 --max-expr-complexity 2 --max-funcs 1 --max-pointer-depth 2 --no-pointers --no-structs --no-unions --no-volatiles --no-builtins -o small_int_arith/csmith_231.c
+// Options:   --no-arrays --no-pointers --no-structs --no-unions --argc --no-bitfields --checksum --comma-operators --compound-assignment --concise --consts --divs --embedded-assigns --pre-incr-operator --pre-decr-operator --post-incr-operator --post-decr-operator --unary-plus-operator --jumps --longlong --int8 --uint8 --no-float --main --math64 --muls --safe-math --no-packed-struct --no-paranoid --no-volatiles --no-volatile-pointers --const-pointers --no-builtins --max-array-dim 1 --max-array-len-per-dim 4 --max-block-depth 1 --max-block-size 10 --max-expr-complexity 4 --max-funcs 10 --max-pointer-depth 2 --max-struct-fields 2 --max-union-fields 2 -o csmith_231.c
 #include "csmith.h"
 
 
@@ -6,20 +6,7 @@ static long __undefined;
 
 
 
-static uint32_t g_6 = 18446744073709551606UL;
-static int64_t g_7 = 0xB5CABFC3938DD44ALL;
-static int64_t g_17 = (-1L);
-static int16_t g_18 = 1L;
-static uint8_t g_20 = 0x7AL;
-static int32_t g_25 = 0xF7C2F5A2L;
-static uint16_t g_27 = 65535UL;
-static uint32_t g_28 = 0xB81230B6L;
-static int16_t g_43 = (-4L);
-static int32_t g_44 = 0x97756EB3L;
-static uint32_t g_45 = 0UL;
-static int32_t g_71 = 0xA298B236L;
-static int32_t g_153 = 0xBE0904B3L;
-static uint32_t g_155 = 0x3838C22DL;
+static int64_t g_5 = 0L;
 
 
 
@@ -30,192 +17,23 @@ static uint64_t  func_1(void);
 
 static uint64_t  func_1(void)
 { 
-    uint32_t l_4 = 3UL;
-    int32_t l_5 = 0xA53AA9ACL;
-    int32_t l_19 = 0L;
-    int32_t l_69 = 0L;
-    int16_t l_151 = 0xA051L;
-    l_5 |= (safe_add_func_uint32_t_u_u(4294967293UL, l_4));
-    l_5 = l_5;
-    g_7 &= ((g_6 | g_6) || 0xB7L);
-    if ((safe_add_func_int64_t_s_s(((((!(safe_lshift_func_uint8_t_u_u(((safe_mod_func_int32_t_s_s(g_6, g_6)) > l_4), g_7))) , g_6) <= g_7) || 0xFD9D0890515466BCLL), 0xE3B6F9B6917AB6DALL)))
-    { 
-        uint32_t l_23 = 18446744073709551615UL;
-        int32_t l_26 = 0x5D840DFEL;
-        int32_t l_54 = 0x33D48A24L;
-        int32_t l_72 = 0L;
-        for (l_5 = 0; (l_5 != 5); l_5 = safe_add_func_int16_t_s_s(l_5, 3))
-        { 
-            int8_t l_24 = 0L;
-            int32_t l_52 = (-1L);
-            g_20--;
-            g_28 ^= ((g_27 = ((l_26 = (((g_25 = ((l_23 = g_17) , l_24)) & l_24) , 1UL)) && g_17)) || g_25);
-            if ((safe_add_func_int8_t_s_s((safe_sub_func_uint8_t_u_u(g_7, 0xF3L)), g_6)))
-            { 
-                int16_t l_39 = (-2L);
-                int32_t l_42 = 0x538E0EA6L;
-                g_25 = g_18;
-                if ((safe_mod_func_uint8_t_u_u(((((((safe_mul_func_uint8_t_u_u((safe_lshift_func_uint8_t_u_u(g_6, l_23)), l_23)) > g_18) >= l_39) , l_39) && (-1L)) > g_25), l_39)))
-                { 
-                    for (l_24 = (-23); (l_24 == 2); ++l_24)
-                    { 
-                        return g_20;
-                    }
-                    if (l_5)
-                        continue;
-                    l_26 |= (l_42 ^= 3L);
-                }
-                else
-                { 
-                    g_45--;
-                    for (g_27 = 0; (g_27 != 34); g_27 = safe_add_func_uint32_t_u_u(g_27, 9))
-                    { 
-                        int32_t l_53 = 5L;
-                        l_52 = (safe_mul_func_uint16_t_u_u(5UL, 6UL));
-                        g_25 ^= (l_52 &= (l_53 , l_54));
-                    }
-                    l_52 &= (+(g_27 = (l_42 && l_24)));
-                }
-            }
-            else
-            { 
-                g_25 = ((g_6 , 1UL) || l_19);
-                g_25 &= (safe_lshift_func_int16_t_s_u((g_18 , (-5L)), g_43));
-                l_52 = g_7;
-            }
-            l_26 = ((((g_43 &= g_25) & 1UL) | g_20) == 0x64L);
-        }
-        if (((safe_add_func_int16_t_s_s((safe_rshift_func_int16_t_s_u(l_4, 11)), g_7)) >= l_19))
-        { 
-            uint32_t l_62 = 0x153ED870L;
-            g_25 ^= 0x1BFD6FFBL;
-            l_62--;
-        }
-        else
-        { 
-            uint64_t l_76 = 18446744073709551608UL;
-            for (l_26 = 0; (l_26 >= (-6)); l_26--)
-            { 
-                int64_t l_70 = 0x659E808E27FB601BLL;
-                int32_t l_89 = (-8L);
-                int32_t l_91 = 0xF2DC1352L;
-                uint32_t l_140 = 5UL;
-                for (l_5 = 0; (l_5 != (-10)); --l_5)
-                { 
-                    uint8_t l_73 = 254UL;
-                    int32_t l_97 = 1L;
-                    ++l_73;
-                    if (l_76)
-                    { 
-                        uint16_t l_90 = 5UL;
-                        l_72 &= g_71;
-                        if (g_71)
-                            break;
-                        l_91 = (safe_div_func_int32_t_s_s((((safe_add_func_uint32_t_u_u((safe_div_func_int16_t_s_s((safe_div_func_uint32_t_u_u((safe_div_func_int8_t_s_s((((g_28 = ((safe_sub_func_int64_t_s_s(((l_89 = (g_20 && g_44)) < l_19), 0x1FE0AFDD74211FA8LL)) <= l_73)) >= g_45) , l_90), l_76)), 1L)), 9UL)), l_90)) , g_18) >= g_43), l_73));
-                        l_69 |= (safe_div_func_int8_t_s_s((l_90 >= l_26), 0x8CL));
-                    }
-                    else
-                    { 
-                        int32_t l_94 = 6L;
-                        if (g_71)
-                            break;
-                        if (g_45)
-                            continue;
-                        g_25 = (l_94 || g_6);
-                    }
-                    l_97 = (safe_sub_func_int32_t_s_s(g_17, l_76));
-                }
-                l_72 = (((safe_div_func_uint8_t_u_u(((g_18 = (-8L)) && 0x7409L), 0x02L)) | g_28) == 0x80B3E519B986BC5ALL);
-                if ((18446744073709551610UL & l_76))
-                { 
-                    g_25 |= ((((-1L) != g_20) >= g_28) < g_17);
-                }
-                else
-                { 
-                    int16_t l_110 = (-9L);
-                    for (g_45 = 24; (g_45 != 46); g_45++)
-                    { 
-                        g_25 |= (safe_mul_func_uint16_t_u_u(g_17, g_71));
-                        l_5 |= ((safe_add_func_int32_t_s_s((safe_sub_func_uint8_t_u_u((((l_110 = (safe_lshift_func_uint16_t_u_s((g_27 = 0x9602L), g_7))) == 7L) < g_17), (-1L))), 0xAE5B1EADL)) || l_91);
-                        return l_76;
-                    }
-                    g_71 = (g_44 == l_76);
-                    l_91 = l_5;
-                    if ((g_25 > g_71))
-                    { 
-                        uint16_t l_113 = 65535UL;
-                        g_71 ^= (safe_sub_func_uint64_t_u_u((g_20 , 18446744073709551615UL), 0x7BF50803CBA28DA2LL));
-                        l_113 = 1L;
-                        g_71 = ((+0x66L) & g_44);
-                    }
-                    else
-                    { 
-                        int32_t l_121 = 0xA15A7CFDL;
-                        int32_t l_122 = 0L;
-                        g_71 ^= (safe_rshift_func_int8_t_s_u(l_76, 2));
-                        l_122 |= (safe_mul_func_int8_t_s_s(((safe_div_func_int8_t_s_s((l_23 ^ g_44), l_121)) , l_110), g_6));
-                    }
-                }
-                for (g_28 = 0; (g_28 == 58); g_28++)
-                { 
-                    uint64_t l_139 = 0xFB2C377C446C13A8LL;
-                    int32_t l_141 = 0xC4755369L;
-                    g_71 = (l_5 = l_76);
-                    g_71 = (safe_add_func_int8_t_s_s(((g_28 , (-4L)) , g_44), 0UL));
-                    g_25 ^= g_20;
-                    for (g_18 = 5; (g_18 == 20); g_18++)
-                    { 
-                        g_25 = (g_71 = (safe_mul_func_int16_t_s_s(((safe_div_func_int16_t_s_s(0x8EB2L, l_23)) && 4294967291UL), 65529UL)));
-                        g_71 = (((safe_lshift_func_uint16_t_u_u((++g_27), ((l_5 = ((((safe_add_func_int32_t_s_s(l_5, 9UL)) > l_139) != g_25) || l_76)) , g_25))) , l_140) && 0xB1924C0CL);
-                        l_141 = l_69;
-                    }
-                }
-            }
-        }
-    }
-    else
-    { 
-        uint64_t l_147 = 0x78769263B3E4958FLL;
-        int32_t l_150 = (-2L);
-        int32_t l_152 = 0x3D21EB91L;
-        int32_t l_154 = 0x006811DCL;
-        for (g_71 = (-2); (g_71 <= 28); g_71 = safe_add_func_uint64_t_u_u(g_71, 7))
-        { 
-            int32_t l_144 = 0L;
-            int32_t l_145 = 8L;
-            int32_t l_146 = 0x26AC6D51L;
-            l_147++;
-            g_155++;
-            g_153 = 0x77237EC0L;
-        }
-    }
-    return g_7;
+    uint64_t l_2 = 0UL;
+    l_2--;
+    return g_5;
 }
 
 
 
 
 
-int main (void)
+int main (int argc, char* argv[])
 {
     int print_hash_value = 0;
+    if (argc == 2 && strcmp(argv[1], "1") == 0) print_hash_value = 1;
     platform_main_begin();
     crc32_gentab();
     func_1();
-    transparent_crc(g_6, "g_6", print_hash_value);
-    transparent_crc(g_7, "g_7", print_hash_value);
-    transparent_crc(g_17, "g_17", print_hash_value);
-    transparent_crc(g_18, "g_18", print_hash_value);
-    transparent_crc(g_20, "g_20", print_hash_value);
-    transparent_crc(g_25, "g_25", print_hash_value);
-    transparent_crc(g_27, "g_27", print_hash_value);
-    transparent_crc(g_28, "g_28", print_hash_value);
-    transparent_crc(g_43, "g_43", print_hash_value);
-    transparent_crc(g_44, "g_44", print_hash_value);
-    transparent_crc(g_45, "g_45", print_hash_value);
-    transparent_crc(g_71, "g_71", print_hash_value);
-    transparent_crc(g_153, "g_153", print_hash_value);
-    transparent_crc(g_155, "g_155", print_hash_value);
+    transparent_crc(g_5, "g_5", print_hash_value);
     platform_main_end(crc32_context ^ 0xFFFFFFFFUL, print_hash_value);
     return 0;
 }
--- tests/csmith/small_int_arith/run_compare.sh
+++ tests/csmith/small_int_arith/run_compare.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-cerberus --cpp="cc -E -nostdinc -undef -D__cerb__ -I $CERB_PATH/include/c/libc -I $CERB_PATH/include/c/posix -DCSMITH_MINIMAL -I ../runtime" --exec --batch $@
-clang -DCSMITH_MINIMAL -I ../runtime -w $@ && ./a.out
+gtimeout 10s clang -DCSMITH_MINIMAL -I ../runtime -w -o compare.out $@ && ./compare.out
+gtimeout 30s cerberus --cpp="cc -E -nostdinc -undef -D__cerb__ -I $CERB_PATH/include/c/libc -I $CERB_PATH/include/c/posix -DCSMITH_MINIMAL -I ../runtime" --exec --batch $@
+rm compare.out
