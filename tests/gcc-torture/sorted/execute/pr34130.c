9a269a031bee80be04318540a3ff9e5dcd245926
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-06-23 13:29:58 +0100

    add gcc-torture tests

---
+++ tests/gcc-torture/sorted/execute/pr34130.c
@@ -0,0 +1,13 @@
+#include "cerberus.h"
+extern void abort (void);
+int foo (int i)
+{
+  return -2 * __builtin_abs(i - 2);
+}
+int main()
+{
+  if (foo(1) != -2
+      || foo(3) != -2)
+    abort ();
+  return 0;
+}
