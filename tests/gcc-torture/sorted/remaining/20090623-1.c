da8110400effb6fd124a70ab86ed5e4925b3d0f1
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-06-27 09:36:35 +0100

    move some memberptr issues test to valid ones, and execute them

---
+++ tests/gcc-torture/sorted/remaining/20090623-1.c
@@ -0,0 +1,19 @@
+#include "cerberus.h"
+int * __restrict__ x;
+
+int foo (int y)
+{
+  *x = y;
+  return *x;
+}
+
+extern void abort (void);
+
+int main()
+{
+  int i = 0;
+  x = &i;
+  if (foo(1) != 1)
+    abort ();
+  return 0;
+}
