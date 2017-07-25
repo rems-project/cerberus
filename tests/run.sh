6d6f2821b3babae0f24d74b42b11dd1a08e9ed9a
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-07-25 11:39:40 +0100

    add z3 as dep

--- tests/run.sh
+++ tests/run.sh
@@ -9,20 +9,29 @@ mkdir -p tmp
 pass=0
 fail=0
 
+JOUTPUT=""
+JOUTPUT_FILE="ci_results.xml"
+
 function test {
   ../cerberus --exec --batch $2/$1 > tmp/result 2> /dev/null
   cmp --silent tmp/result $2/expected/$1.expected
+
   if [[ "$?" -eq "0" ]]; then
     res="\033[1m\033[32mPASSED!\033[0m"
     pass=$((pass+1))
+    JOUTPUT+="\t<testcase name=\"$1\"/>\n"
   else
     res="\033[1m\033[31mFAILED!\033[0m"
     fail=$((fail+1))
     cat tmp/result
+    JOUTPUT+="\t<testcase name=\"$1\">\n"
+    JOUTPUT+="\t\t<error message=\"fail\">`cat tmp/result`</error>"
+    JOUTPUT+="\t</testcase>"
   fi
 
   echo -e "Test $1: $res"
 }
+
 
 
 # Running ci tests
@@ -33,3 +42,11 @@ done
 
 echo "PASSED: $pass"
 echo "FAILED: $fail"
+
+# JUnit XML output (for Jenkins report)
+echo "<testsuites>" > $JOUPUT_FILE
+echo "<testsuite name=\"ci\" tests=\"$((pass + failt))\" failures=\"${fail}\" timestamp=\"$(date)\">\n" >> $JOUTPUT_FILE
+echo $JOUTPUT >> $JOUTPUT_FILE
+echo "</testsuite>" >> $JOUTPUT_FILE
+echo "</testsuites>" >> $JOUTPUT_FILE
+
