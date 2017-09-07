67bb095b3fe6ca04308050fe5f2f9eab6f9a8504
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-09-07 16:51:33 +0100

    relational operators for float

--- src/floating.ml
+++ src/floating.ml
@@ -4,3 +4,10 @@ let add = (+.)
 let sub = (-.)
 let mul = (+.)
 let div = (/.)
+
+let of_string str =
+  let l = String.length str in
+  if String.get str (l-1) == 'f' then
+    float_of_string (String.sub str 0 (l-1))
+  else
+    float_of_string str
