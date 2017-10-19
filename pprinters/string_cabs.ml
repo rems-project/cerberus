b521d473d74b20d32fa155248438e5ed9045b9e8
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-10-19 16:52:36 +0100

    complete pp for cabs

--- pprinters/string_cabs.ml
+++ pprinters/string_cabs.ml
@@ -1,8 +1,9 @@
 open Pp_cabs
 
+(*
 let string_of_cabs_type_specifier tspec =
   Pp_utils.to_plain_string (pp_cabs_type_specifier tspec)
-
+*)
 
 let string_of_declarator decltor =
   "Pp_utils.to_plain_string (pp_declarator decltor)"
