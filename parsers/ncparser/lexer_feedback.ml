58e4042e5c74299e543b89fe3465da278748324b
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-10-23 12:30:18 -0700

    wip

--- parsers/ncparser/lexer_feedback.ml
+++ parsers/ncparser/lexer_feedback.ml
@@ -10,7 +10,7 @@ type context = IdSet.t
 let cerb_builtin_types =
   [ "jmp_buf";        "sig_atomic_t";     "va_list";
     "ptrdiff_t";      "wchar_t";
-    "int8_t";         "int16_t";        "in32_t";         "int64_t";
+    "int8_t";         "int16_t";        "int32_t";        "int64_t";
     "uint8_t";        "uint16_t";       "uin32_t";        "uint64_t";
     "int_least8_t";   "int_least16_t";  "int_least32_t";  "int_least64_t";
     "uint_least8_t";  "uint_least16_t"; "uint_least32_t"; "uint_least64_t";
