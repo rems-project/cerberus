253076f7c29b282cc1a83e1ba141e9416eda1d1a
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2017-11-09 14:15:45 +0000

    add core rewrite UI

--- server/simple_http_server.ml
+++ server/simple_http_server.ml
@@ -191,6 +191,10 @@ let mk_result file out err =
 
 (* Cerberus interaction *)
 
+let elab_rewrite =
+  Printf.sprintf
+    "cerberus --pp=cabs,ail,core --rewrite --pp_flags=annot,fout %s > %s 2> %s"
+
 let elab =
   Printf.sprintf
     "cerberus --pp=cabs,ail,core --pp_flags=annot,fout %s > %s 2> %s"
@@ -261,6 +265,7 @@ let post ~docroot uri path content =
     | "/exhaustive" -> cerberus (run "exhaustive") content
     | "/random" -> cerberus (run "random") content
     | "/graph" -> create_graph content
+    | "/elab_rewrite"  -> cerberus elab_rewrite content
     | "/elab"  -> cerberus elab content
     | _ -> forbidden path
   in catch try_with (fun _ -> forbidden path)
