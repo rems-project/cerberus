e83e096e4ac218a8ef4219e11571b364a52c5e6e
Victor Gomes <548275+victorgomes@users.noreply.github.com>
2018-01-26 17:57:25 +0000

    fix ctype

--- include/c/libc/ctype.h
+++ include/c/libc/ctype.h
@@ -15,44 +15,42 @@ int isdigit(int c) {
   return (unsigned)c-'0' < 10;
 }
 
-int isalnum(int c) {
-  return isalpha(c) || isdigit(c);
-}
-
 int isgraph(int c) {
   return (unsigned)c-0x21 < 0x5e;
-}
-
-int isalpha(int c) {
-  return isupper(c) || islower(c);
 }
 
 int isprint(int c) {
   return (unsigned)c-0x20 < 0x5f;
 }
 
-int ispunct(int c) {
-  if (isspace(c) || isalnum(c))
-    return false;
-  return isgraph(c);
-}
-
 int isspace(int c) {
-  if (isalnum(c)) {
-    return false;
-  return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c = '\t' || c = '\v';
+  return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v';
 }
 
 int islower(int c) {
-  if (iscntrl(c) || isdigit(c) || ispunct(c) || isspace(c))
-    return false;
+  if (iscntrl(c) || isdigit(c) || isgraph(c) || isspace(c))
+    return 0;
   return (unsigned)c-'a' < 26;
 }
 
 int isupper(int c) {
-  if (iscntrl(c) || isdigit(c) || ispunct(c) || isspace(c))
-    return false;
+  if (iscntrl(c) || isdigit(c) || isgraph(c) || isspace(c))
+    return 0;
   return (unsigned)c-'A' < 26;
+}
+
+int isalpha(int c) {
+  return isupper(c) || islower(c);
+}
+
+int isalnum(int c) {
+  return isalpha(c) || isdigit(c);
+}
+
+int ispunct(int c) {
+  if (isspace(c) || isalnum(c))
+    return 0;
+  return isgraph(c);
 }
 
 int isxdigit(int c) {
