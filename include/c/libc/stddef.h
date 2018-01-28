186c8e58983f421e7065ab8d1a040d3d905aefaa
Kayvan Memarian <kmemarian@users.noreply.github.com>
2018-01-28 07:22:23 +0000

    random things to make itc-benchmarks and tiny_sha3 work better

--- include/c/libc/stddef.h
+++ include/c/libc/stddef.h
@@ -2,14 +2,13 @@
 #define	_STDDEF_H_
 
 typedef __cerbty_ptrdiff_t   ptrdiff_t;
-
-// typedef __cerb_size_t      size_t;
-// typedef __cerb_max_align_t max_align_t;
+typedef __cerbty_size_t      size_t;
+//TODO: typedef __cerbty_max_align_t max_align_t;
 
 typedef __cerbty_wchar_t wchar_t;
 
 #define NULL                 __cerbvar_NULL
-// TODO: #define offsetof(type, memb) __cerb_offsetof(type, memb)
+#define offsetof(type, memb) __cerb_offsetof(type, memb)
 
 // Annex K: Bounds-checking interfaces
 // typedef size_t rsize_t;
