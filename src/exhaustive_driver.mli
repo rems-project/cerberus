d3d90fd814f2613241c1e5381c7ca5e5e433e03d
Victor Gomes <victorborgesfg@gmail.com>
2018-02-05 13:15:14 +0000

    change batch_driver outpu

--- src/exhaustive_driver.mli
+++ src/exhaustive_driver.mli
@@ -9,7 +9,7 @@ type execution_result = (Core.value list, Errors.error) Exception.exceptM
 
 
 val batch_drive:
-  Symbol.sym UniqueId.supply -> 'a Core.file -> string list -> driver_conf -> unit
+  Symbol.sym UniqueId.supply -> 'a Core.file -> string list -> driver_conf -> string list
 
 val drive:
   Symbol.sym UniqueId.supply -> 'a Core.file -> string list -> driver_conf -> execution_result
