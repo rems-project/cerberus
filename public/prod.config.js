8517b330f18fcd7ecb7366d4b23cc30e27d373d8
Victor Gomes <victorborgesfg@gmail.com>
2018-04-29 12:36:57 +0100

    minimising CSS when deploying

--- public/prod.config.js
+++ public/prod.config.js
@@ -1,5 +1,7 @@
 const merge = require('webpack-merge');
 const baseConfig = require('./base.config.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractCSS = new ExtractTextPlugin({ filename: 'style.bundle.css' })
 
 module.exports = merge(baseConfig, {
   mode: 'production',
@@ -19,5 +21,18 @@ module.exports = merge(baseConfig, {
       },
     },
   },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: extractCSS.extract({
+        fallback: 'style-loader',
+        use: [ { loader: 'css-loader', options: { minimize: true } } ]
+      })
+    }
+   ]
+  },
+  plugins: [
+    extractCSS
+  ]
 });
 
