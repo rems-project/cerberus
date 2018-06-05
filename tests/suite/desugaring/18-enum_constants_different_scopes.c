a1e13d78929542cd9e44c18fc1cd2ffa068c711f
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2018-06-05 20:11:17 +0100

    cleanup desug tests

--- tests/suite/desugaring/18-enum_constants_different_scopes.c
+++ tests/suite/desugaring/18-enum_constants_different_scopes.c
@@ -1,8 +1,9 @@
-enum { A };
+enum { A = 10 };
 
 int main(void)
 {
   // this is valid because the scope differs from the one where A was
   // previously declared
-  enum { A };
+  enum { A = 2 };
+  return A; // EXPECTED: 2
 }
