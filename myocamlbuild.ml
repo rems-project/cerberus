52f4b2c76eb06efb0bc002e66aafe0a1353bdd65
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2018-06-08 21:16:16 +0100

    Finally got a cstubs to work. (Victor I may have broken the building of your stuff)

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,2 +1,19 @@
+(*
 open Ocamlbuild_plugin
  let () = dispatch Bisect_ppx_plugin.dispatch
+*)
+
+open Ocamlbuild_plugin
+
+let () =
+  dispatch begin function
+  | After_rules ->
+      flag_and_dep ["link"; "ocaml"; "link_cstubs"]
+        (A "src/cerberus_cstubs.o");
+(*
+      flag ["link"; "ocaml"; "use_cstubs"]
+        (S [A "-ccopt"; A "-Lsrc"]);
+*)
+  | _ ->
+      ()
+end
