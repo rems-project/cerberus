dafcc868da1b223e1bfbac3207f361176bb5e46e
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2018-06-12 05:22:58 +0100

    cleaner cstubs

--- src/cerberus_cstubs.c
+++ src/cerberus_cstubs.c
@@ -4,7 +4,9 @@
 #include <unistd.h>
 
 #include "caml/alloc.h"
+#include "caml/memory.h"
 #include "caml/mlvalues.h"
+
 
 // Rounding an ocaml float (which on a 64bits
 // machine is a C double into a C float
@@ -12,28 +14,36 @@
 // https://stackoverflow.com/questions/45362323/ieee-64-and-32-bit-float-validation-in-ocaml?rq=1#comment77712839_45362890
 value round_double_to_float32(value d)
 {
+  CAMLparam1(d);
   float f = Double_val(d);
-  return caml_copy_double(f);
+  CAMLreturn(caml_copy_double(f));
 }
 
 
 value bits_of_float32(value d)
 {
+  CAMLparam1(d);
   union {
     float f;
     int32_t i32;
   } un = { .f = (float)Double_val(d) };
-  return caml_copy_int64((int64_t)un.i32);
+  CAMLreturn(caml_copy_int64((int64_t)un.i32));
 }
 
 
 value terminal_size(value unit)
 {
+  CAMLparam1(unit);
+  CAMLlocal2(pair, ret);
   struct winsize wsz;
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsz);
-  
-  value ret = caml_alloc_tuple(2);
-  Field(ret, 0) = Val_int(wsz.ws_row);
-  Field(ret, 1) = Val_int(wsz.ws_col);
-  return ret;
+  if ( -1 == ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsz) )
+    // returns: None
+    return Val_int(0);
+  // returns: Some (row,col)
+  pair = caml_alloc_tuple(2);
+  Field(pair, 0) = Val_int(wsz.ws_row);
+  Field(pair, 1) = Val_int(wsz.ws_col);
+  ret = caml_alloc(1, 0);
+  Store_field(ret, 0, pair);
+  CAMLreturn(ret);
 }
