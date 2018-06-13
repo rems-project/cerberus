04db806b0d2158adc6949e2876a74d8c3cd1682f
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2018-06-13 16:35:11 +0100

    offsetof() is apparently now built in the C parser

--- include/c/libc/stddef.h
+++ include/c/libc/stddef.h
@@ -8,7 +8,9 @@ typedef __cerbty_max_align_t max_align_t;
 typedef __cerbty_wchar_t wchar_t;
 
 #define NULL                 __cerbvar_NULL
-#define offsetof(type, memb) __cerb_offsetof(type, memb)
+
+// offsetof() is now builtin in the parser
+// #define offsetof(type, memb) __cerb_offsetof(type, memb)
 
 // Annex K: Bounds-checking interfaces
 // typedef size_t rsize_t;
