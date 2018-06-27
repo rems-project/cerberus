8b693c6671d7e8b30252de574e4ffe5e011c8d1f
Victor Gomes <victorborgesfg@gmail.com>
2018-06-27 11:22:20 +0100

    remove some tests based on GNU extensions

--- tests/gcc-torture/breakdown/fail/parsing/920428-1.c
+++ tests/gcc-torture/breakdown/fail/parsing/920428-1.c
@@ -1,4 +1,4 @@
 #include "cerberus.h"
-x(const char*s){char a[1];const char*ss=s;a[*s++]|=1;return(int)ss+1==(int)s;}
+int x(const char*s){char a[1];const char*ss=s;a[*s++]|=1;return(int)ss+1==(int)s;}
 int 
 main (void){if(x("")!=1)abort();exit(0);}
