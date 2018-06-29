f2b9b47e285e9384b3643b30dc43e4d873b4710f
Victor Gomes <victorborgesfg@gmail.com>
2018-06-29 11:35:01 +0100

    ail typing foldeR

--- tests/gcc-torture/cerberus.h
+++ tests/gcc-torture/cerberus.h
@@ -19,21 +19,43 @@
 #define __extension__
 #define __attribute(a)
 #define __attribute__(a)
-
-#define __SIZE_TYPE__     size_t
-#define __INT16_TYPE__    int16_t
-#define __INT32_TYPE__    int32_t
-#define __UINT32_TYPE__   unsigned int
-#define __INTPTR_TYPE__   int*
-#define __UINTPTR_TYPE__  unsigned int*
-#define __WCHAR_TYPE__    char
-#define __INT_LEAST8_TYPE__ char
-#define __UINT_LEAST32_TYPE__ unsigned int
-#define __PTRDIFF_TYPE__  ptrdiff_t
-
 #define __inline          inline
 #define __inline__        inline
 #define __alignof__       alignof
+
+#define __SIZE_TYPE__         size_t
+#define __PTRDIFF_TYPE__      ptrdiff_t
+#define __WCHAR_TYPE__        wchar_t
+#define __WINT_TYPE__         wint_t
+#define __INTMAX_TYPE__       intmax_t
+#define __UINTMAX_TYPE__      uintmax_t
+#define __SIG_ATOMIC_TYPE__   sig_atomic_t
+#define __INT8_TYPE__         int8_t
+#define __INT16_TYPE__        int16_t
+#define __INT32_TYPE__        int32_t
+#define __INT64_TYPE__        int64_t
+#define __UINT8_TYPE__        uint8_t
+#define __UINT16_TYPE__       uint16_t
+#define __UINT32_TYPE__       uint32_t
+#define __UINT64_TYPE__       uint64_t
+#define __INT_LEAST8_TYPE__   int_least8_t
+#define __INT_LEAST16_TYPE__  int_least16_t
+#define __INT_LEAST32_TYPE__  int_least32_t
+#define __INT_LEAST64_TYPE__  int_least64_t
+#define __UINT_LEAST8_TYPE__  uint_least8_t
+#define __UINT_LEAST16_TYPE__ uint_least16_t
+#define __UINT_LEAST32_TYPE__ uint_least32_t
+#define __UINT_LEAST64_TYPE__ uint_least64_t
+#define __INT_FAST8_TYPE__    int_fast8_t
+#define __INT_FAST16_TYPE__   int_fast16_t
+#define __INT_FAST32_TYPE__   int_fast32_t
+#define __INT_FAST64_TYPE__   int_fast64_t
+#define __UINT_FAST8_TYPE__   uint_fast8_t
+#define __UINT_FAST16_TYPE__  uint_fast16_t
+#define __UINT_FAST32_TYPE__  uint_fast32_t
+#define __UINT_FAST64_TYPE__  uint_fast64_t
+#define __INTPTR_TYPE__       intptr_t
+#define __UINTPTR_TYPE__      uintptr_t
 
 // Hard-coded sizes
 #define __SIZEOF_LONG_LONG__  8
