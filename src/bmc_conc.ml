f75575f5cd51029d3bb054d8a1ac4c98a16f86e3
Stella Lau <sl715@cam.ac.uk>
2018-07-19 10:12:32 +0100

    [BMC] Compute sb relation

--- src/bmc_conc.ml
+++ src/bmc_conc.ml
@@ -28,6 +28,13 @@ let aid_of_action (a: action) = match a with
   | Fence (aid, _, _) ->
       aid
 
+let tid_of_action (a: action) = match a with
+  | Load  (_, tid, _, _, _)
+  | Store (_, tid, _, _, _)
+  | RMW   (_, tid, _, _, _, _)
+  | Fence (_, tid, _) ->
+      tid
+
 let wval_of_action (a: action) = match a with
   | Store (_, _, _, _, v) -> v
   | _ -> assert false
@@ -35,19 +42,31 @@ let wval_of_action (a: action) = match a with
 let get_action (BmcAction(_, _, action): bmc_action) =
   action
 
+let tid_of_bmcaction (bmcaction: bmc_action) =
+  tid_of_action (get_action bmcaction)
+
+let is_pos_action (BmcAction(pol, _, _): bmc_action) = match pol with
+  | Pos -> true
+  | Neg -> false
+
 let bmc_action_cmp (BmcAction(_, _, a1)) (BmcAction(_, _, a2)) =
   compare (aid_of_action a1) (aid_of_action a2)
 
 (* ===== PREEXECS ===== *)
 
+type action_rel = bmc_action * bmc_action
+
 type preexec = {
   actions         : bmc_action list;
   initial_actions : bmc_action list;
+
+  sb              : action_rel list;
 }
 
 let mk_initial_preexec : preexec =
   { actions         = []
   ; initial_actions = []
+  ; sb              = []
   }
 
 let add_action action (preexec: preexec) : preexec =
@@ -66,7 +85,18 @@ let combine_preexecs (preexecs: preexec list) =
   List.fold_left (fun acc preexec ->
     { actions         = preexec.actions @ acc.actions
     ; initial_actions = preexec.initial_actions @ acc.initial_actions
+    ; sb              = preexec.sb @ acc.sb
     }) mk_initial_preexec preexecs
+
+(* Sequence actions in xs before ys; computes (threadwise) cartesian product *)
+let compute_sb (xs: bmc_action list) (ys: bmc_action list) : action_rel list =
+  List.fold_left (fun outer x ->
+    List.fold_left (fun inner y ->
+      if (tid_of_bmcaction x = tid_of_bmcaction y)
+      then (x,y)::inner
+      else inner
+    ) outer ys
+  ) [] xs
 
 (* ===== PPRINTERS ===== *)
 let string_of_memory_order = function
@@ -100,7 +130,12 @@ let pp_bmcaction (BmcAction(pol, guard, action): bmc_action) =
           (*(Expr.to_string guard)*)   ""
           (pp_action action)
 
+let pp_actionrel ((a,b): action_rel) =
+  sprintf "(%d,%d)" (aid_of_action (get_action a))
+                    (aid_of_action (get_action b))
+
 let pp_preexec (preexec: preexec) =
-  sprintf ">>Initial:\n%s\n>>Actions:\n%s\n"
+  sprintf ">>Initial:\n%s\n>>Actions:\n%s\n>>SB:\n%s\n"
           (String.concat "\n" (List.map pp_bmcaction preexec.initial_actions))
           (String.concat "\n" (List.map pp_bmcaction preexec.actions))
+          (String.concat "\n" (List.map pp_actionrel preexec.sb))
