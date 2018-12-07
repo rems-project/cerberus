4b576dd495b2b8e5eed067658b8790ab1f415a2a
Victor Gomes <victorborgesfg@gmail.com>
2018-12-07 12:04:33 +0000

    add tests for Chacha20, Poly1305 and Curve25119

--- tests/hacl-star/build.sh
+++ tests/hacl-star/build.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 #gcc -fPIC -Wall -Wextra -Wno-parentheses -Wno-unused compact/*.c minimal/*.c -I. -Iinclude -Icompact -Iminimal -shared -olibhacl.so -Xlinker -z -Xlinker noexecstack -Xlinker --unresolved-symbols=report-all
 
-echo "Testing Hacl_Hash.c"
+echo "Testing Hash"
 cerberus compact/Hacl_Hash.c -I include -I minimal --exec --batch # it works faster without --sequentialise --rewrite
+
+echo "Testing Chacha2"
+cerberus compact/Hacl_Chacha20.c -I include -I minimal --exec --batch
+
+echo "Testing Poly1305"
+cerberus compact/Hacl_Poly1305_64.c -I include -I minimal --exec --batch
+
+# It takes too long to execute!
+#echo "Testing Curve25519"
+#cerberus compact/Hacl_Curve25519.c -I include -I minimal --exec --batch --rewrite --sequentialise
 
