4d3a33ffa19ae3328ca2d750dfc83f4e0adf1192
Stella Lau <stellal@mit.edu>
2019-02-04 14:56:40 -0500

    [BMC] s/smb/smp in linux.h; disable rmw

--- bmc/linux.h
+++ bmc/linux.h
@@ -16,18 +16,18 @@ typedef enum linux_memory_order {
 } linux_memory_order;
 
 
-#define smb_rmb() __cerbvar_linux_fence(linux_memory_order_rmb)
-#define smb_wmb() __cerbvar_linux_fence(linux_memory_order_wmb)
-#define smb_mb()  __cerbvar_linux_fence(linux_memory_order_mb)
-#define smb_read_barrier_depends() __cerbvar_linux_fence(linux_memory_order_rbdep)
-#define smb_load_acquire(object) __cerbvar_linux_read(&(object),linux_memory_order_acquire)
-#define smb_store_release(object,desired) __cerbvar_linux_write(&(object),(desired),linux_memory_order_release)
+#define smp_rmb() __cerbvar_linux_fence(linux_memory_order_rmb)
+#define smp_wmb() __cerbvar_linux_fence(linux_memory_order_wmb)
+#define smp_mb()  __cerbvar_linux_fence(linux_memory_order_mb)
+#define smp_read_barrier_depends() __cerbvar_linux_fence(linux_memory_order_rbdep)
+#define smp_load_acquire(object) __cerbvar_linux_read(&(object),linux_memory_order_acquire)
+#define smp_store_release(object,desired) __cerbvar_linux_write(&(object),(desired),linux_memory_order_release)
 
 // TODO: not the best api
-#define xchg_relaxed(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_once)
-#define xchg_acquire(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_acquire)
-#define xchg_release(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_release)
-#define xchg(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_mb)
+//#define xchg_relaxed(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_once)
+//#define xchg_acquire(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_acquire)
+//#define xchg_release(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_release)
+//#define xchg(ptr,desired) __cerbvar_linux_rmw((ptr),(desired),linux_memory_order_mb)
 
 #define READ_ONCE(object) __cerbvar_linux_read(&(object),linux_memory_order_once)
 #define WRITE_ONCE(object,desired) __cerbvar_linux_write(&(object),(desired),linux_memory_order_once)
