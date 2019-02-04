dceff75066f76f1fcf92854bc5fb877321a1ed86
Stella Lau <stellal@mit.edu>
2019-02-04 17:06:09 -0500

    [BMC] Add example Linux litmus test

---
+++ tests/bmc/concurrency/linux/C-LB+o-mb-o+o-rmb-o.c
@@ -0,0 +1,18 @@
+#include "linux.h"
+
+int main() {
+  int x = 0, y = 0;
+  int r1, r3;
+  {-{ { r1 = READ_ONCE(x);
+        smp_mb();
+        WRITE_ONCE(y, 1);
+      }
+  ||| { r3 = READ_ONCE(y); 
+        smp_rmb();
+        WRITE_ONCE(x, 1); 
+      }
+  }-}
+  //exists(0:r1=1 /\ 1:r3=1)
+  //allowed
+  return (r1 + 2* r3);
+}
