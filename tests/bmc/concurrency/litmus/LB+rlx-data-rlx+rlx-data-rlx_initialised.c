669dbcfbc8ee0d478fb80a19ad47bb214a3c4772
Jean Pichon <jp622@cam.ac.uk>
2019-02-06 19:00:47 +0000

    tests: tweaked

--- tests/bmc/concurrency/litmus/LB+rlx-data-rlx+rlx-data-rlx_initialised.c
+++ tests/bmc/concurrency/litmus/LB+rlx-data-rlx+rlx-data-rlx_initialised.c
@@ -1,12 +1,15 @@
 #include <stdatomic.h>
 int main() {
   _Atomic(int) x = 0, y = 0;
-  int z1 = 0, z2 = 0;
-  {-{ { z1 = atomic_load_explicit(&x, memory_order_relaxed);
-        atomic_store_explicit(&y, z1, memory_order_relaxed); }
-  ||| { z2 = atomic_load_explicit(&y, memory_order_relaxed);
-        atomic_store_explicit(&x, z2, memory_order_relaxed); }  }-};
-  __BMC_ASSUME((z1 == 1 && z2 == 1)); // for C11
-  // assert(!(z1 == 1 && z2 == 1)); // for RC11
-  return z1 + (2 * z2);
+  int r1 = 0, r2 = 0;
+  {-{ {
+    r1 = atomic_load_explicit(&x, memory_order_relaxed);
+    atomic_store_explicit(&y, r1, memory_order_relaxed); }
+  ||| {
+    r2 = atomic_load_explicit(&y, memory_order_relaxed);
+    atomic_store_explicit(&x, r2, memory_order_relaxed);
+  } }-};
+  __BMC_ASSUME((r1 == 1 && r2 == 1)); // for C11
+  // assert(!(r1 == 1 && r2 == 1)); // for RC11
+  return r1 + (2 * r2);
 }
