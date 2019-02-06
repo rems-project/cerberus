abc095a8ffce5df00d20623b98c2c5f09bde2369
Jean Pichon <jp622@cam.ac.uk>
2019-02-06 18:52:04 +0000

    tests: tweaked

--- tests/bmc/concurrency/litmus/IRIW+rel+rel+acq_acq+acq_acq.c
+++ tests/bmc/concurrency/litmus/IRIW+rel+rel+acq_acq+acq_acq.c
@@ -1,19 +1,21 @@
 // IRIW with release/acquire
 // The reading threads do not have to see the writes to x and y in the same order.
-
 #include <stdatomic.h>
-
-int main(void) {
-  _Atomic(int) x = 0; 
-  _Atomic(int) y = 0;
-  int z1; int z2; int z3; int z4;
-  {-{ atomic_store_explicit(&x, 1, memory_order_release);
-  ||| atomic_store_explicit(&y, 1, memory_order_release);
-  ||| { z1=atomic_load_explicit(&x, memory_order_acquire);
-        z2=atomic_load_explicit(&y, memory_order_acquire); }
-  ||| { z3=atomic_load_explicit(&y, memory_order_acquire);
-        z4=atomic_load_explicit(&x, memory_order_acquire); }
-  }-};
-  return (z1 + 2 * (z2 + 2 * (z3 + 2 * z4)));
+int main() {
+  _Atomic(int) x = 0, y = 0;
+  int r1, r2, r3, r4;
+  {-{
+    atomic_store_explicit(&x, 1, memory_order_release);
+  |||
+    atomic_store_explicit(&y, 1, memory_order_release);
+  ||| {
+    r1 = atomic_load_explicit(&x, memory_order_acquire);
+    r2 = atomic_load_explicit(&y, memory_order_acquire);
+  } ||| {
+    r3 = atomic_load_explicit(&y, memory_order_acquire);
+    r4 = atomic_load_explicit(&x, memory_order_acquire);
+  } }-};
+  __BMC_ASSUME(r1 == 1 && r2 == 0 && r3 == 1 && r4 == 0);
+  return (r1 + 2 * (r2 + 2 * (r3 + 2 * r4)));
 }
 
