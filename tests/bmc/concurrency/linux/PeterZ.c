1e3d1e2c5ffb60ae383e3437d699e0e33d09b588
Jean Pichon <jp622@cam.ac.uk>
2019-02-06 18:53:02 +0000

    tests: tweaked

--- tests/bmc/concurrency/linux/PeterZ.c
+++ tests/bmc/concurrency/linux/PeterZ.c
@@ -1,23 +1,19 @@
 #include "linux.h"
-
 int main() {
   int x = 0, y = 0, z = 0;
   int r1, r2, r3;
   {-{ {
-        WRITE_ONCE(x, 1);
-        smp_mb();
-        r1 = READ_ONCE(y);
-      }
-  ||| {
-        WRITE_ONCE(y, 1);
-        smp_store_release(&z, 1);
-      }
-  ||| {
-        r2 = smp_load_acquire(&z);
-        smp_mb();
-        r3 = READ_ONCE(x); 
-      }
-  }-}
+    WRITE_ONCE(x, 1);
+    smp_mb();
+    r1 = READ_ONCE(y);
+  } ||| {
+    WRITE_ONCE(y, 1);
+    smp_store_release(&z, 1);
+  } ||| {
+    r2 = smp_load_acquire(&z);
+    smp_mb();
+    r3 = READ_ONCE(x);
+  } }-}
   assert (!(r1 == 0 && r2 == 1 && r3 == 0));
   return r1 + 2 * r2 + 4 * r3;
 }
