7693bc7a495af5399db3bb4935a1fa6070312e4f
Jean Pichon <jp622@cam.ac.uk>
2019-02-06 21:22:31 +0000

    tests: added RCU litmus test

---
+++ tests/bmc/concurrency/linux/LISA2Rec1G.c
@@ -0,0 +1,24 @@
+#include "linux.h"
+int main() {
+  int x0, x1, x2, x3;
+  int r1, r2, r3;
+  {-{ {
+    rcu_read_lock();
+    r2 = READ_ONCE(x0);
+    smp_mb();
+    WRITE_ONCE(x1, 1);
+    WRITE_ONCE(x2, 1);
+    rcu_read_unlock();
+  } ||| {
+    r1 = READ_ONCE(x1);
+    synchronize_rcu();
+    WRITE_ONCE(x3, 1);
+    r3 = READ_ONCE(x2);
+  } ||| {
+    rcu_read_lock();
+    r2 = READ_ONCE(x3);
+    WRITE_ONCE(x0, 1);
+    rcu_read_unlock();
+  } }-};
+  return 0;
+}
