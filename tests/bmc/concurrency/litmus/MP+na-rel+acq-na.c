ea6b40a80d058c3a52f1c8778659c10a25a383d1
Jean Pichon <jp622@cam.ac.uk>
2019-02-07 11:55:44 +0000

    tests+public: added yaml file for litmus tests + added some tests

--- tests/bmc/concurrency/litmus/MP+na-rel+acq-na.c
+++ tests/bmc/concurrency/litmus/MP+na-rel+acq-na.c
@@ -1,6 +1,6 @@
-// MP+na_rel+acq_na
+// MP+na-rel+acq-na
 // Message Passing, of data held in non-atomic x, with release/acquire synchronisation on y.
-// If the value of z1 is 1, then the value of z2 should also be 1.
+// If the value of r1 is 1, then the value of r2 should also be 1.
 // An exhaustive execution of this program should therefore return the value 1 and 2, but not 0.
 #include <stdatomic.h>
 int main() {
