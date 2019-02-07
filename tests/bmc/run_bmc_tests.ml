b7549480afca256960aa1efe50229dc1e244c672
Jean Pichon <jp622@cam.ac.uk>
2019-02-07 10:10:57 +0000

    tests: improved test harness

--- tests/bmc/run_bmc_tests.ml
+++ tests/bmc/run_bmc_tests.ml
@@ -22,9 +22,9 @@ let iter_on_c_files f filenames =
       else ())
     filenames
 
-let start_log cmd log_file () =
+let write_to_file cmd log_file () =
   let oc = open_out log_file in
-  output_string oc (cmd ^ "\n");
+  output_string oc cmd;
   close_out oc
 
 let is_success log_file () =
@@ -43,28 +43,56 @@ let is_success log_file () =
   close_in ic;
   success
 
-let run_tests h opts filenames =
-  let opts = if opts = "" then "" else opts ^ " " in
-  iter_on_c_files
-    (fun filename ->
-      print_string filename;
-      flush Pervasives.stdout;
-      let log_file = filename ^ ".log" in
-      let cmd = "DYLD_LIBRARY_PATH=`ocamlfind query z3` ../../cerberus " ^ opts ^ filename ^ " >> " ^ log_file ^ " 2>&1" in
-      start_log cmd log_file ();
-      let r = Unix.system cmd in
-      match r with
-      | WEXITED 0 ->
-        if is_success log_file () then (print_string (" " ^ start_green ^ "OK" ^ reset_colour ^ "\n"); flush Pervasives.stdout)
-        else (print_string (" " ^ start_red ^ "FAIL" ^ reset_colour ^ "\n"); flush Pervasives.stdout)
-      | WEXITED _ -> print_string (" " ^ start_red ^ "!!!FAILED!!!" ^ reset_colour ^ "\n"); flush Pervasives.stdout
-      | _ -> failwith "TODO run_typing_tests")
-    filenames
+type config = {
+  only_cmd : bool;
+  only_c11 : bool;
+  produce_graphs : bool;
+}
 
+let run_test cfg opts filename =
+  print_string filename;
+  let dir = (Filename.chop_suffix filename ".c") ^ "/" in
+  let _ = Unix.system ("mkdir -p " ^ dir) in
+  flush Pervasives.stdout;
+  let cmd_file = dir ^ "cmd.sh" in
+  let log_file = dir ^ "log.txt" in
+  let cmd = "DYLD_LIBRARY_PATH=`ocamlfind query z3` ../../cerberus " ^ opts ^ filename ^ " > " ^ log_file ^ " 2>&1" in
+  write_to_file cmd cmd_file ();
+  if cfg.only_cmd then ()
+  else
+    let r = Unix.system cmd in
+    match r with
+    | WEXITED 0 ->
+      if is_success log_file () then
+        (if cfg.produce_graphs then let _ = Unix.system ("mv graph_*.dot " ^ dir) in ();
+         print_string (" " ^ start_green ^ "OK" ^ reset_colour ^ "\n"); flush Pervasives.stdout)
+      else (print_string (" " ^ start_red ^ "FAIL" ^ reset_colour ^ "\n"); flush Pervasives.stdout);
+    | WEXITED _ -> print_string (" " ^ start_red ^ "!!!ERROR!!!" ^ reset_colour ^ "\n"); flush Pervasives.stdout
+    | _ -> failwith "TODO run_typing_tests"
+
+let run_tests cfg h opts filenames =
+  let opts = if opts = "" then "" else opts ^ " " in
+  iter_on_c_files (fun filename -> run_test cfg opts filename) filenames
+
+let find_flags () =
+  let flag_only_cmd = ref false in
+  let flag_only_c11 = ref false in
+  let flag_no_graphs = ref false in
+  let speclist = [
+    ("--only-cmd", Arg.Set flag_only_cmd, "Only produces the command files");
+    ("--only-c11", Arg.Set flag_only_c11, "Only run C11 tests");
+    ("--no-graphs", Arg.Set flag_no_graphs, "Do not produce graphs")
+  ] in
+  let usage_msg = "run_bmc_tests:"
+  in Arg.parse speclist print_endline usage_msg;
+  { only_cmd = !flag_only_cmd; only_c11 = !flag_only_c11; produce_graphs = not (!flag_no_graphs) }
 
 let main () =
-  run_tests () "--bmc --bmc_conc --bmc-mode=linux --bmc-cat=$CERB_PATH/bmc/linux.cat" (my_readdir2 "concurrency/linux");
-  run_tests () "--bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/c11.cat" (my_readdir2 "concurrency/litmus")
+  let cfg = find_flags () in
+  let graph_opt = (if cfg.produce_graphs then " --bmc_output_model=true" else "") in
+  (if cfg.only_c11 then ()
+  else run_tests cfg () ("--bmc --bmc_conc --bmc-mode=linux --bmc-cat=$CERB_PATH/bmc/linux.cat" ^ graph_opt) (my_readdir2 "concurrency/linux"));
+  run_tests cfg () ("--bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/c11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus")
   (* TODO: do some magic for RC11 *)
 
 let _ = main ()
