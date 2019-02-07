b45e53d80baf9b0f44cec81273935d7e85a1fd56
Jean Pichon <jp622@cam.ac.uk>
2019-02-07 13:32:03 +0000

    tests: tweaked

--- tests/bmc/concurrency/linux/C-LB+o-mb-o+o-rmb-o.c
+++ tests/bmc/concurrency/linux/C-LB+o-mb-o+o-rmb-o.c
@@ -3,15 +3,15 @@
 int main() {
   int x = 0, y = 0;
   int r1, r3;
-  {-{ { r1 = READ_ONCE(x);
-        smp_mb();
-        WRITE_ONCE(y, 1);
-      }
-  ||| { r3 = READ_ONCE(y); 
-        smp_rmb();
-        WRITE_ONCE(x, 1); 
-      }
-  }-}
+  {-{ {
+    r1 = READ_ONCE(x);
+    smp_mb();
+    WRITE_ONCE(y, 1);
+  } ||| {
+    r3 = READ_ONCE(y);
+    smp_rmb();
+    WRITE_ONCE(x, 1);
+  } }-}
   __BMC_ASSUME(r1 == 1 && r3 == 1);
   return r1 + 2 * r3;
 }
