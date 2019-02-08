719d449d2c4cb24eafc9f970abd2138b3c9bec13
Jean Pichon <jp622@cam.ac.uk>
2019-02-08 11:06:42 +0000

    tests: tweaked assertion to match herd, not text

--- public/dist/litmus/C-wrc+o-mb-o+o-rmb-o.c
+++ public/dist/litmus/C-wrc+o-mb-o+o-rmb-o.c
@@ -1,22 +1,19 @@
 #include "linux.h"
-int main(void)
-{
-int x = 0;
-int y = 0;
-
-int T1_r3;
-int T2_r1;
-int T2_r2;
-{-{
-{ WRITE_ONCE(y,1); }
-|||
-{ T1_r3 = READ_ONCE(y);
-smp_mb();
-WRITE_ONCE(x,1); }
-|||
-{ T2_r1 = READ_ONCE(x);
-smp_rmb();
-T2_r2 = READ_ONCE(y); }
-}-};
- __BMC_ASSUME (T1_r3 == 1 && T2_r1 == 1 && T2_r2 == 0);
+int main(void) {
+int x = 0, y = 0;
+int T1_r3, T2_r1, T2_r2;
+{-{ {
+  WRITE_ONCE(y,1);
+} ||| {
+  T1_r3 = READ_ONCE(y);
+  smp_mb();
+  WRITE_ONCE(x,1);
+} ||| {
+  T2_r1 = READ_ONCE(x);
+  smp_rmb();
+  T2_r2 = READ_ONCE(y);
+  } }-};
+  // Herd says this is forbidden
+  // the text description says 'allowed'
+  assert(!(T1_r3 == 1 && T2_r1 == 1 && T2_r2 == 0));
 }
