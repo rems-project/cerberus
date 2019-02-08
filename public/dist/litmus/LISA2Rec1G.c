f9f548c6b7662c62bb2a3d796f84eabcbffbba58
Victor Gomes <victorborgesfg@gmail.com>
2019-02-08 10:51:38 +0000

    more tests

---
+++ public/dist/litmus/LISA2Rec1G.c
@@ -0,0 +1,27 @@
+// LISA2Rec1G
+// https://github.com/paulmckrcu/litmus/blob/master/manual/rcu/LISA2Rec1G.litmus
+#include "linux.h"
+int main() {
+  int x0 = 0, x1 = 0, x2 = 0, x3 = 0;
+  int r1, r2, r3;
+  {-{ {
+    rcu_read_lock();
+    r2 = READ_ONCE(x0);
+    smp_mb();
+    WRITE_ONCE(x1, 1);
+    WRITE_ONCE(x2, 1);
+    rcu_read_unlock();
+  } ||| {
+    r1 = READ_ONCE(x1);
+    synchronize_rcu();
+    WRITE_ONCE(x3, 1);
+    r3 = READ_ONCE(x2);
+  } ||| {
+    rcu_read_lock();
+    r2 = READ_ONCE(x3);
+    WRITE_ONCE(x0, 1);
+    rcu_read_unlock();
+  } }-};
+  assert(!((r1 == 1 && r2 == 1 && r2 == 1) || (r1 == 1 && r3 == 0)));
+  // return r1 + 2 * (r2 + 2 * r3);
+}
---
+++ public/dist/litmus/LISA2Rftx1GM.c
@@ -0,0 +1,28 @@
+// LISA2Rftx1GM
+// https://github.com/paulmckrcu/litmus/blob/master/manual/rcu/LISA2Rftx1GM.litmus
+#include "linux.h"
+int main() {
+  int x0 = 0, x1 = 0, x2 = 0, x3 = 0;
+  int r0, r1, r2, r3;
+  {-{ {
+    rcu_read_lock();
+    WRITE_ONCE(x1, 1);
+    smp_mb();
+    WRITE_ONCE(x0, 1);
+    rcu_read_unlock();
+    rcu_read_lock();
+    WRITE_ONCE(x3, 1);
+    WRITE_ONCE(x2, 1);
+    rcu_read_unlock();
+  } ||| {
+    r0 = READ_ONCE(x0);
+    synchronize_rcu();
+    r3 = READ_ONCE(x3);
+  } ||| {
+    r2 = READ_ONCE(x2);
+    smp_mb();
+    r1 = READ_ONCE(x1);
+  } }-}
+  assert(!(r0 == 1 && r3 == 0 && r2 == 1 && r1 == 0));
+  // return r0 + 2 * (r3 + 2 * (r2 + 2 * r1));
+}
