47bd5d6681787a8569e849a580563c2239074861
Jean Pichon <jp622@cam.ac.uk>
2019-02-08 08:59:37 +0000

    tests: improved test harness

--- tests/bmc/run_bmc_tests.ml
+++ tests/bmc/run_bmc_tests.ml
@@ -27,29 +27,13 @@ let write_to_file cmd log_file () =
   output_string oc cmd;
   close_out oc
 
-let is_success log_file () =
-  let ic = open_in log_file in
-  let rec f () =
-    try
-      let l = input_line ic in
-      if String.length l < 2 then f ()
-      else
-        let suff = String.sub l (String.length l - 2) 2 in
-        if suff = ":)" then true
-        else f ()
-    with
-    | End_of_file -> false in
-  let success = f () in
-  close_in ic;
-  success
-
 type config = {
   only_write_command : bool;
   skip_linux : bool;
   produce_graphs : bool;
 }
 
-let run_test cfg model opts filename =
+let run_test validator cfg model opts filename =
   print_string (model ^ " " ^ filename);
   let dir = (Filename.chop_suffix filename ".c") ^ "-" ^ model ^ "/" in
   let _ = Unix.system ("mkdir -p " ^ dir) in
@@ -63,16 +47,16 @@ let run_test cfg model opts filename =
     let r = Unix.system cmd in
     match r with
     | WEXITED 0 ->
-      if is_success log_file () then
+      if validator log_file () then
         (if cfg.produce_graphs then let _ = Unix.system ("mv graph_*.dot " ^ dir) in ();
          print_string (" " ^ start_green ^ "OK" ^ reset_colour ^ "\n"); flush Pervasives.stdout)
       else (print_string (" " ^ start_red ^ "FAIL" ^ reset_colour ^ "\n"); flush Pervasives.stdout);
     | WEXITED _ -> print_string (" " ^ start_red ^ "!!!ERROR!!!" ^ reset_colour ^ "\n"); flush Pervasives.stdout
     | _ -> failwith "TODO run_typing_tests"
 
-let run_tests cfg model opts filenames =
+let run_tests validator cfg model opts filenames =
   let opts = if opts = "" then "" else opts ^ " " in
-  iter_on_c_files (fun filename -> run_test cfg model opts filename) filenames
+  iter_on_c_files (fun filename -> run_test validator cfg model opts filename) filenames
 
 let find_flags () =
   let flag_only_write_command = ref false in
@@ -87,12 +71,40 @@ let find_flags () =
   in Arg.parse speclist print_endline usage_msg;
   { only_write_command = !flag_only_write_command; skip_linux = !flag_skip_linux; produce_graphs = not (!flag_no_graphs) }
 
+let validator_of_line_validator line_validator log_file () =
+  let ic = open_in log_file in
+  let rec f () =
+    try
+      let l = input_line ic in
+      if line_validator l then true else f ()
+    with
+    | End_of_file -> false in
+  let success = f () in
+  close_in ic;
+  success
+
+let smiley_line_validator l =
+  let pattern = ":)" in
+  if String.length l < String.length pattern then false
+  else String.sub l (String.length l - 2) (String.length pattern) = pattern
+
+let ub_line_validator l =
+  let pattern = "UB found" in
+  if String.length l < String.length pattern then false
+  else String.sub l 0 (String.length pattern) = pattern
+
+let smiley_validator log_file () = validator_of_line_validator smiley_line_validator log_file ()
+
+let ub_validator log_file () = validator_of_line_validator ub_line_validator log_file ()
+
 let main () =
   let cfg = find_flags () in
   let graph_opt = (if cfg.produce_graphs then " --bmc_output_model=true" else "") in
   (if cfg.skip_linux then ()
-  else run_tests cfg "linux" ("-D__memory_model_linux__ --bmc --bmc_conc --bmc-mode=linux --bmc-cat=$CERB_PATH/bmc/linux.cat" ^ graph_opt) (my_readdir2 "concurrency/linux"));
-  run_tests cfg "c11" ("-D__memory_model_c11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/c11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus");
-  run_tests cfg "rc11" ("-D__memory_model_rc11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/rc11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus")
+  else run_tests smiley_validator cfg "linux" ("-D__memory_model_linux__ --bmc --bmc_conc --bmc-mode=linux --bmc-cat=$CERB_PATH/bmc/linux.cat" ^ graph_opt) (my_readdir2 "concurrency/linux"));
+  run_tests smiley_validator cfg "c11" ("-D__memory_model_c11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/c11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus");
+  run_tests ub_validator cfg "c11" ("-D__memory_model_c11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/c11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus-ub");
+  run_tests smiley_validator cfg "rc11" ("-D__memory_model_rc11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/rc11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus");
+  run_tests ub_validator cfg "rc11" ("-D__memory_model_rc11__ --bmc --bmc_conc --bmc-mode=c --bmc-cat=$CERB_PATH/bmc/rc11.cat" ^ graph_opt) (my_readdir2 "concurrency/litmus-ub")
 
 let _ = main ()
