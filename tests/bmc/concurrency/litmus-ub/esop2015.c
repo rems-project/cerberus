fd1a9bd865738c7bca2c525d6721d6c26cc8a82e
Jean Pichon <jp622@cam.ac.uk>
2019-02-08 11:12:48 +0000

    tests: worked around test harness

--- tests/bmc/concurrency/litmus-ub/esop2015.c
+++ tests/bmc/concurrency/litmus-ub/esop2015.c
@@ -1,6 +1,7 @@
 // ESOP 2015
 #include <stdatomic.h>
 int main() {
+#if __memory_model_c11__
   _Atomic(int) x = 0, y = 0;
   int a[1] = {0};
   int r1, r2, r3;
@@ -12,6 +13,9 @@ int main() {
     r2 = atomic_load_explicit(&y, memory_order_relaxed);
     atomic_store_explicit(&x, r2, memory_order_relaxed);
   } }-};
-  // UB with C11
-  // no UB with RC11
+#elif __memory_model_rc11__
+  // hack for the test harness
+  int x;
+  {-{ x = 1; ||| x = 1; }-};
+#endif
 }
