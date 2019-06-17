95cc66791b92fb17d4c2551cdda0c8500748d45c
Victor Gomes <victorborgesfg@gmail.com>
2019-06-17 16:25:36 +0100

    running ci for ocaml backend

--- tests/run-ocaml.sh
+++ tests/run-ocaml.sh
@@ -2,25 +2,20 @@
 
 source tests.sh
 
-rm -rf _cbuild
-
-mkdir -p tmp
-cd tmp
-
 pass=0
 fail=0
 
 function test {
   rm -rf a.out
-  cbuild $1 > /dev/null 2> /dev/null
+  cbuild $2/$1 > /dev/null 2> /dev/null
   if [ "$?" -ne "0" ]; then
     echo -e "Test $1: Cerberus failed..."
     return
   fi
 
-  if [ -e ../$2/expected/$1.expected ]; then
+  if [ -e $2/expected/$1.expected ]; then
     CERB_BATCH=1 ./a.out > result
-    cmp --silent result ../$2/expected/$1.expected
+    cmp --silent result $2/expected/$1.expected
     if [[ "$?" -eq "0" ]]; then
       res="\033[1m\033[32mPASSED!\033[0m"
       pass=$((pass+1))
@@ -35,44 +30,13 @@ function test {
 
 }
 
-function run_ci {
-  # Running ci tests
-  for file in "${citests[@]}"
-  do
-    test $file ci
-  done
+# Running ci tests
+for file in "${citests[@]}"
+do
+  test $file ci
+done
 
-  echo "PASSED: $pass"
-  echo "FAILED: $fail"
-}
+rm -rf result a.out a.out.ml
 
-if [ $# -eq 0 ]; then
-  cp ../ci/*.c .
-  run_ci
-else
-  if [ "$1" == "csmith" ]; then
-    cp ../csmith/small_int_arith/*.c .
-    for f in csmith_*.c
-    do
-      echo "File: $f"
-      cbuild --fcsmith $f > /dev/null 2> /dev/null
-      if [ $? -eq 0 ]; then
-        ./a.out
-      else
-        echo "FAIL"
-      fi
-    done
-  else
-    cp ../suite/$1/*.c .
-    for f in *.c
-    do
-      echo "File: $f"
-      cbuild $f > /dev/null 2> /dev/null
-      if [ $? -eq 0 ]; then
-        ./${f%.c}.native
-      else
-        echo "FAIL"
-      fi
-    done
-  fi
-fi
+echo "PASSED: $pass"
+echo "FAILED: $fail
