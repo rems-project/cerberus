f5f0eb77873ed50c34001308ecacef851a2d22c9
Victor Gomes <victorborgesfg@gmail.com>
2019-07-05 11:48:54 +0100

    fix max_align_t

--- runtime/libc/include/stddef.h
+++ runtime/libc/include/stddef.h
@@ -3,10 +3,9 @@
 
 typedef __cerbty_ptrdiff_t   ptrdiff_t;
 typedef __cerbty_size_t      size_t;
-typedef __cerbty_max_align_t max_align_t;
+typedef __cerbty_wchar_t     wchar_t;
 
-typedef __cerbty_wchar_t wchar_t;
-
+#define max_align_t          __cerbty_max_align_t;
 #define NULL                 __cerbvar_NULL
 
 // offsetof() is now builtin in the parser
