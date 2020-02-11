b53be16979ee4342c86b974f131829304a68029c
Jean Pichon <jean.pichonp@gmail.com>
2020-02-11 15:08:09 +0000

    sketch rustic

--- backend/ail_rustic/src/rustic.ml
+++ backend/ail_rustic/src/rustic.ml
@@ -1,5 +1,13 @@
-let run ail_file =
+let foo2 (Symbol.Symbol (_, _, Some id), (_, _, attrs, _)) =
+  print_string (id ^ ": " ^ string_of_int (List.length attrs) ^ "\n")
+
+let foo fs =
+  List.iter foo2 fs
+
+let run_rustic ail_file =
   match ail_file with
   | (id, s) ->
-    failwith "Jean add stuff in this directory";
+    let fs = s.AilSyntax.function_definitions in
+    foo fs;
     ()
+
