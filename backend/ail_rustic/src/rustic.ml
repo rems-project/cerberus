86e51fb03b4f61edae4cd8b8bad35cb05e17f7ac
Jean Pichon <jean.pichonp@gmail.com>
2020-02-11 15:34:44 +0000

    sketch rustic

--- backend/ail_rustic/src/rustic.ml
+++ backend/ail_rustic/src/rustic.ml
@@ -1,5 +1,10 @@
-let foo2 (Symbol.Symbol (_, _, Some id), (_, _, attrs, _)) =
-  print_string (id ^ ": " ^ string_of_int (List.length attrs) ^ "\n")
+let plop (Symbol.Identifier (_, s)) = s
+
+let foo3 attrs =
+  String.concat ", " (List.map (fun a -> plop a.Annot.attr_id) attrs)
+
+let foo2 (Symbol.Symbol (_, _, Some id), (_, Annot.Attrs attrs, _, _)) =
+  print_string (id ^ ": " ^ foo3 attrs ^ "\n")
 
 let foo fs =
   List.iter foo2 fs
