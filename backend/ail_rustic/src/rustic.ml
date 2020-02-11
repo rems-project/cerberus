6b4744e2471be45113fda921a8d84f1088a45498
Jean Pichon <jean.pichonp@gmail.com>
2020-02-11 17:07:13 +0000

    rustic

--- backend/ail_rustic/src/rustic.ml
+++ backend/ail_rustic/src/rustic.ml
@@ -1,3 +1,5 @@
+open AilSyntax
+
 let plop (Symbol.Identifier (_, s)) = s
 
 let plop2 = function
@@ -14,10 +16,43 @@ let foo2 (Symbol.Symbol (_, _, Some id), (_, Annot.Attrs attrs, _, _)) =
 let foo fs =
   List.iter foo2 fs
 
+let rec map_option_aux f acc = function
+  | [] -> List.rev acc
+  | x :: xs ->
+    (match f x with
+     | None -> map_option_aux f acc xs
+     | Some y -> map_option_aux f (y :: acc) xs)
+
+let map_option f xs = map_option_aux f [] xs
+
+module Ail_identifier = struct
+  type t = AilSyntax.ail_identifier
+  let compare (x : t) y = Symbol.symbol_compare
+end
+
+module Ail_identifier_map = Map.Make(Ail_identifier)
+
+let add_left id x m =
+  match Ail_identifier_map.find_opt id m with
+  | None -> Ail_identifier_map.add id (Some x, None)
+  | Some (_, y) -> Ail_identifier_map.add id (Some x, y)
+
+let add_left id y m =
+  match Ail_identifier_map.find_opt id m with
+  | None -> Ail_identifier_map.add id (None, Some y)
+  | Some (x, _) -> Ail_identifier_map.add id (x, Some y)
+
+let collect_functions s =
+  let ds = map_option (function (id, (_, Decl_function (a, b, c, d, e, f))) -> Some (id, (a, b, c, d, e, f)) | (_, (_, Decl_object _)) -> None) s.declarations in
+  let fs = s.function_definitions in
+  let m = Ail_identifier_map.empty in
+  let m = List.fold_left (fun m (id, d) -> add_left id d m) m ds in
+  let m = List.fold_left (fun m (id, f) -> add_right id f m) m fs in
+  m
+
 let run_rustic ail_file =
   match ail_file with
   | (id, s) ->
-    let fs = s.AilSyntax.function_definitions in
+    let x = collect_functions s in
     foo fs;
     ()
-
