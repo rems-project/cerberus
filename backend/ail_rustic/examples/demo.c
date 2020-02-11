f4787c06e8781db230863c701a5b1d239a2da231
Jean Pichon <jean.pichonp@gmail.com>
2020-02-11 15:34:44 +0000

    sketch rustic

--- backend/ail_rustic/examples/demo.c
+++ backend/ail_rustic/examples/demo.c
@@ -1,16 +1,16 @@
 struct s {
   int f1;
-  int [[rc::cell_scalar]] f2;
-  struct s2 * [[rc::cell("f3get", "f3set")]] f3;
+  int f2 [[rc::cell_scalar]];
+  struct s2 * f3 [[rc::cell("f3get", "f3set")]];
 };
 
-[[rc::forall("a"), rc::funlifetime("f"), rc::leq("f", "a")]]
+[[rc::forall("a"), rc::function_lifetime("f"), rc::leq("f", "a")]]
 void f([[rc::mut("a")]] struct s * [[rc::mut("f"), rc::nonnull]] p) {
   p->f1 = 1;
 }
 
-[[rc::forall("a"), rc::funtionlifetime("f"), rc::leq("f", "a")]]
-void g([[rc::read("a"), rc::inited("s{f1:init,f2:notinit,f3:notinit}")]] struct s * [[rc::mut("f"),rc::nonnull]] p) {
+[[rc::forall("a"), rc::funtion_lifetime("f"), rc::leq("f", "a")]]
+void g([[rc::read("a"), rc::inited("s{f1:init,f2:notinit,f3:notinit}")]] struct s * [[rc::mut("f"), rc::nonnull]] p) {
   int x = p->f1 + 1;
 }
 
