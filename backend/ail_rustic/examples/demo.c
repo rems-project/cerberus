2e419cebb4cd4e185960db8e5cd9c21c4eb8ddcd
Jean Pichon <jean.pichonp@gmail.com>
2020-02-14 14:12:09 +0000

    fix rustic

--- backend/ail_rustic/examples/demo.c
+++ backend/ail_rustic/examples/demo.c
@@ -15,19 +15,52 @@ void g([[rc::read("a"), rc::inited("s{f1:init,f2:notinit,f3:notinit}")]] struct 
 }
 
 struct ll_node {
-  int x;
+  int f;
   struct ll_node * next [[rc::recursive]];
 };
+
+[[rc::forall("a"), rc::function_lifetime("f"), rc::leq("f", "a")]]
+void f2(struct ll_node * x [[rc::read("a")]]) {
+  if (x) {
+    int y = x->f;
+    f2(x->next);
+  }
+}
 
 // TODO: actual mutex
 struct mutex {
   int taken;
 };
 
+[[rc::is_lock_function("mutex")]]
+void lock(struct mutex *);
+[[rc::is_unlock_function("mutex")]]
+void unlock(struct mutex *);
+
 struct lk_s {
-  struct mutex m [[rc::struct_owner]];
-  int x [[rc::owned_by("m")]];
+  struct mutex m [[rc::mutex]];
+  int f [[rc::owned_by("m")]];
   int * p [[rc::owned_by("m")]];
+  int f2;
+};
+
+void f3(struct lk_s * x) {
+  if (x) {
+    lock(&x->m);
+    int y = x->f;
+    if (x->p) {
+      int z = *x->p;
+    }
+    unlock(&x->m);
+    // can't access f or p
+    int w = x->f2;
+  }
+}
+
+struct ll_lk_node {
+  struct mutex m [[rc::mutex]];
+  int f [[rc::owned_by("m")]];
+  struct ll_lk_node * next [[rc::recursive, rc::owned_by("m")]];
 };
 
 int main(void) {
