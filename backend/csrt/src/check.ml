eb3185d432f1f861b2c34ccac08325d6a9124507
Christopher Pulte <cp526@cam.ac.uk>
2020-02-26 16:30:22 +0000

    cleanup

--- backend/csrt/src/check.ml
+++ backend/csrt/src/check.ml
@@ -2,10 +2,7 @@
 open Core
 open Impl_mem
 open Nat_big_num
-
-
 open Sexplib
-open Sexp
 open Printf
 
 
@@ -38,113 +35,148 @@ let pp_list f xs =
   "[" ^ (String.concat ", " (List.map f xs)) ^ "]"
 
 let sexp_list_to f = function
-  | Atom _ -> failwith "expected List"
-  | List sxs -> List.map f sxs
+  | Sexp.Atom _ -> failwith "expected Sexp.List"
+  | Sexp.List sxs -> List.map f sxs
+
+
+let unreachable () = failwith "unreachable"
+
 
 
 (* Types and sorts *)
 
 type index_term =
-  | IT_Var of id
-  | IT_Int of num
-  | IT_Bool of bool
+  | Var of id
+  | Num of num
+  | Bool of bool
   (* | IT_Loc of loc *)
-  | IT_Add of index_term * index_term
-  | IT_Sub of index_term * index_term
-  | IT_Mul of index_term * index_term
-  | IT_Div of index_term * index_term
-  | IT_Exp of index_term * index_term
-  | IT_Fun of id * index_term
-  | IT_App of index_term * index_term
+  | Add of index_term * index_term
+  | Sub of index_term * index_term
+  | Mul of index_term * index_term
+  | Div of index_term * index_term
+  | Exp of index_term * index_term
+  | Fun of id * index_term
+  | App of index_term * index_term
 
 
 let rec pp_index_term = function
-  | IT_Var id -> 
+  | Var id -> 
      id
-  | IT_Int num -> 
+  | Num num -> 
      Nat_big_num.to_string num
-  | IT_Bool bool -> 
+  | Bool bool -> 
      pp_bool bool
-  | IT_Add (it1,it2) -> 
-     sprintf "(%s + %s)" (pp_index_term it1) (pp_index_term it2)
-  | IT_Sub (it1,it2) -> 
-     sprintf "(%s - %s)" (pp_index_term it1) (pp_index_term it2)
-  | IT_Mul (it1,it2) -> 
-     sprintf "(%s * %s)" (pp_index_term it1) (pp_index_term it2)
-  | IT_Div (it1,it2) -> 
-     sprintf "(%s / %s)" (pp_index_term it1) (pp_index_term it2)
-  | IT_Exp (it1,it2) -> 
-     sprintf "(%s ^ %s)" (pp_index_term it1) (pp_index_term it2)
-  | IT_Fun (id,it) ->
-     sprintf "(fn %s %s)" id (pp_index_term it)
-  | IT_App (it1,it2) ->
-     sprintf "(%s %s)" (pp_index_term it1) (pp_index_term it2)
+  | Add (it1,it2) -> 
+     sprintf "(%s + %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
+  | Sub (it1,it2) -> 
+     sprintf "(%s - %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
+  | Mul (it1,it2) -> 
+     sprintf "(%s * %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
+  | Div (it1,it2) -> 
+     sprintf "(%s / %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
+  | Exp (it1,it2) -> 
+     sprintf "(%s ^ %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
+  | Fun (id,it) ->
+     sprintf "(fn %s %s)" 
+       id 
+       (pp_index_term it)
+  | App (it1,it2) ->
+     sprintf "(%s %s)" 
+       (pp_index_term it1)
+       (pp_index_term it2)
 
 
-let rec sexp_to_index_term = function
+let rec sexp_to_index_term sx = 
+  let open Sexp in
+  match sx with
   | Atom "true" -> 
-     IT_Bool true
+     Bool true
   | Atom "false" -> 
-     IT_Bool false
+     Bool false
   | Atom str -> 
-     begin try IT_Int (Nat_big_num.of_string str) with
-     | _ -> IT_Var str
+     begin try Num (Nat_big_num.of_string str) with
+     | _ -> Var str
      end
   | List [o1;Atom "+";o2] -> 
-     IT_Add (sexp_to_index_term o1, sexp_to_index_term o2)
+     Add (sexp_to_index_term o1, sexp_to_index_term o2)
   | List [o1;Atom "-";o2] -> 
-     IT_Sub (sexp_to_index_term o1, sexp_to_index_term o2)
+     Sub (sexp_to_index_term o1, sexp_to_index_term o2)
   | List [o1;Atom "*";o2] -> 
-     IT_Mul (sexp_to_index_term o1, sexp_to_index_term o2)
+     Mul (sexp_to_index_term o1, sexp_to_index_term o2)
   | List [o1;Atom "/";o2] -> 
-     IT_Div (sexp_to_index_term o1, sexp_to_index_term o2)
+     Div (sexp_to_index_term o1, sexp_to_index_term o2)
   | List [o1;Atom "^";o2] -> 
-     IT_Exp (sexp_to_index_term o1, sexp_to_index_term o2)
+     Exp (sexp_to_index_term o1, sexp_to_index_term o2)
   | List [Atom "fn";Atom id;it] -> 
-     IT_Fun (id,sexp_to_index_term it)
+     Fun (id,sexp_to_index_term it)
   | List [o1;o2] -> 
-     IT_App (sexp_to_index_term o1, sexp_to_index_term o2)
+     App (sexp_to_index_term o1, sexp_to_index_term o2)
   | t -> parse_error "index term" t
 
 
 
 
 type logical_predicate = 
-  | LP_EQ of index_term * index_term
-  | LP_NE of index_term * index_term
-  | LP_LT of index_term * index_term
-  | LP_GT of index_term * index_term
-  | LP_LE of index_term * index_term
-  | LP_GE of index_term * index_term
-  | LP_Null of index_term
+  | EQ of index_term * index_term
+  | NE of index_term * index_term
+  | LT of index_term * index_term
+  | GT of index_term * index_term
+  | LE of index_term * index_term
+  | GE of index_term * index_term
+  | Null of index_term
 
 let pp_logical_predicate = function
-  | LP_EQ (o1,o2) -> 
-     sprintf "(%s = %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_NE (o1,o2) -> 
-     sprintf "(%s <> %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_LT (o1,o2) -> 
-     sprintf "(%s < %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_GT (o1,o2) -> 
-     sprintf "(%s > %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_LE (o1,o2) -> 
-     sprintf "(%s <= %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_GE (o1,o2) -> 
-     sprintf "(%s >= %s)" (pp_index_term o1) (pp_index_term o2)
-  | LP_Null o1 ->
-     sprintf "(null %s)" (pp_index_term o1)
+  | EQ (o1,o2) -> 
+     sprintf "(%s = %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | NE (o1,o2) -> 
+     sprintf "(%s <> %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | LT (o1,o2) -> 
+     sprintf "(%s < %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | GT (o1,o2) -> 
+     sprintf "(%s > %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | LE (o1,o2) -> 
+     sprintf "(%s <= %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | GE (o1,o2) -> 
+     sprintf "(%s >= %s)" 
+       (pp_index_term o1)
+       (pp_index_term o2)
+  | Null o1 ->
+     sprintf "(null %s)" 
+       (pp_index_term o1)
 
-let sexp_to_logical_predicate = function
+let sexp_to_logical_predicate sx =
+  let open Sexp in
+  match sx with 
   | List [o1;op;o2] -> 
      let lp_o1 = sexp_to_index_term o1 in
      let lp_o2 = sexp_to_index_term o2 in
      begin match op with
-     | Atom "=" -> LP_EQ (lp_o1, lp_o2)
-     | Atom "<>" -> LP_NE (lp_o1, lp_o2)
-     | Atom "<" -> LP_LT (lp_o1, lp_o2)
-     | Atom ">" -> LP_GT (lp_o1, lp_o2)
-     | Atom "<=" -> LP_LE (lp_o1, lp_o2)
-     | Atom ">=" -> LP_GE (lp_o1, lp_o2)
+     | Atom "=" -> EQ (lp_o1, lp_o2)
+     | Atom "<>" -> NE (lp_o1, lp_o2)
+     | Atom "<" -> LT (lp_o1, lp_o2)
+     | Atom ">" -> GT (lp_o1, lp_o2)
+     | Atom "<=" -> LE (lp_o1, lp_o2)
+     | Atom ">=" -> GE (lp_o1, lp_o2)
      | t -> parse_error "logical predicate" t
      end
   | t -> parse_error "logical predicate" t
@@ -153,26 +185,26 @@ let sexp_to_logical_predicate = function
 
 
 type logical_constraint =
-  | LC_true
   | LC_Pred of logical_predicate
   | LC_And of logical_constraint * logical_constraint
   | LC_Not of logical_constraint
 
 
 let rec pp_logical_constraint = function
-  | LC_true ->
-     "true"
   | LC_And (o1,o2) -> 
-     sprintf "(%s & %s)" (pp_logical_constraint o1) (pp_logical_constraint o2)
+     sprintf "(%s & %s)" 
+       (pp_logical_constraint o1)
+       (pp_logical_constraint o2)
   | LC_Not (o1) -> 
-     sprintf "(not %s)" (pp_logical_constraint o1)
+     sprintf "(not %s)" 
+       (pp_logical_constraint o1)
   | LC_Pred p -> 
      pp_logical_predicate p
 
 
-let rec sexp_to_logical_constraint = function
-  | Atom "true" -> 
-     LC_true
+let rec sexp_to_logical_constraint sx =
+  let open Sexp in
+  match sx with
   | List [Atom "not";op] -> 
      LC_Not (sexp_to_logical_constraint op)
   | List [o1; Atom "&"; o2] ->
@@ -192,7 +224,9 @@ let pp_base_type = function
   | BT_Int -> "int"
   | BT_Loc -> "loc"
 
-let sexp_to_base_type = function
+let sexp_to_base_type sx = 
+  let open Sexp in
+  match sx with
   | Atom "unit" -> BT_Unit
   | Atom "bool" -> BT_Bool
   | Atom "int" -> BT_Int
@@ -230,7 +264,9 @@ let rec pp_resource_type = function
        (pp_resource_type rt)
        (pp_list pp_resource_type rts)
 
-let rec sexp_to_resource_type = function
+let rec sexp_to_resource_type sx = 
+  let open Sexp in
+  match sx with 
   | List [Atom "block";op] -> 
      RT_Block (sexp_to_index_term op)
   | List [Atom "int"; it1; it2] ->
@@ -265,7 +301,9 @@ let rec pp_logical_sort = function
   | LS_Base bt -> 
        pp_base_type bt
 
-let rec sexp_to_logical_sort = function
+let rec sexp_to_logical_sort sx =
+  let open Sexp in
+  match sx with
   | List [Atom "list"; a] ->
      LS_List (sexp_to_base_type a)
   | List [o1; Atom "->"; o2] ->
@@ -311,7 +349,9 @@ and pp_return_type rt =
   sprintf "(%s)" (pp_list_return_type rt)
 
 
-let rec list_sexp_to_return_type = function
+let rec list_sexp_to_return_type sx = 
+  let open Sexp in 
+  match sx with
   | Atom "E" :: List [Atom id; Atom ":"; t] :: Atom "." :: rtyp ->
      let t = try Left (sexp_to_base_type t) with 
              | _ -> Right (sexp_to_return_type t)
@@ -327,6 +367,7 @@ let rec list_sexp_to_return_type = function
   | rt -> parse_error "return type" (List rt)
 
 and sexp_to_return_type rt = 
+  let open Sexp in
   begin match rt with
   | List rt -> list_sexp_to_return_type rt
   | Atom _ -> list_sexp_to_return_type [rt]
@@ -366,7 +407,9 @@ let pp_function_type ftyp =
   sprintf "(%s)" (pp_list_function_type ftyp)
   
 
-let rec list_sexp_to_function_type = function
+let rec list_sexp_to_function_type sx = 
+  let open Sexp in
+  match sx with
   | Atom "A" :: List [Atom id; Atom ":"; bt] :: Atom "." :: ftyp ->
      FT_AC ((id, sexp_to_base_type bt), list_sexp_to_function_type ftyp)
   | Atom "AL":: List [Atom id; Atom ":"; ls] :: Atom "." :: ftyp ->
@@ -379,6 +422,7 @@ let rec list_sexp_to_function_type = function
 
 
 let sexp_to_function_type ftyp = 
+  let open Sexp in
   begin match ftyp with
   | List ftyp -> list_sexp_to_function_type ftyp
   | Atom a -> list_sexp_to_function_type [ftyp]
@@ -430,13 +474,11 @@ end
 open Core
 open ID_M
 
-let empty_constraint = LC_true
-
 let var_equal_to (id : id) (it : index_term) = 
-  LC_Pred (LP_EQ (IT_Var id, it))
+  LC_Pred (EQ (Var id, it))
 
 let var_null (id : id) = 
-  LC_Pred (LP_Null (IT_Var id))
+  LC_Pred (Null (Var id))
 
 
 let option_get = function
@@ -445,117 +487,90 @@ let option_get = function
 
 
 
+  let rec object_aux (cv,lv,lc) ov inner_rt : return_type m = 
+    fresh >>= fun n ->
+    begin match ov with
 
+    | OVinteger i -> 
+       let i = option_get (eval_integer_value i) in
+       return (RT_EC ((n, Left BT_Int), 
+                      RT_AndL (var_equal_to n (Num i), inner_rt)))
+
+    | OVfloating _ -> failwith "floats not supported"
+
+    | OVpointer p -> 
+
+       let case_null _ctype =
+         return (RT_EC ((n, Left BT_Loc), RT_AndL (var_null n, inner_rt))) in
+       let case_function_pointer _sym = 
+         failwith "function pointers not supported" in
+       let case_concrete_pointer _prov i = 
+         return (RT_EC ((n, Left BT_Loc), 
+                        RT_AndL (var_equal_to n (Num i), inner_rt))) in
+       let case_unspecified_value _ = unreachable () in
+
+       case_ptrval 
+         p
+         case_null 
+         case_function_pointer 
+         case_concrete_pointer 
+         case_unspecified_value
+
+    | OVarray _ -> failwith "OVarray not implemented"
+    | OVstruct _ -> failwith "OVstruct not implemented"
+    end
 
 
 let infer_type__value (cv,lv,lc) v : return_type m = 
-
-  let rec aux (cv,lv,lc) v : (return_type -> return_type) m =
-    fresh >>= fun n ->
-    begin match v with
-    | Vobject ov -> 
-
-      begin match ov with
-
-      | OVinteger i -> 
-         let i = option_get (eval_integer_value i) in
-         let t rt_i = 
-           RT_EC ((n, Left BT_Int), 
-                  RT_AndL (var_equal_to n (IT_Int i), rt_i)) 
-         in
-         return t
-
-      | OVfloating _ -> failwith "floats not supported"
-
-      | OVpointer p -> 
-
-         let case_null _ctype =
-           let t rt_i = 
-             RT_EC ((n, Left BT_Loc), 
-                    RT_AndL (var_null n, rt_i))
-           in
-           return t
-         in
-         let case_function_pointer _sym = 
-           failwith "function pointers not supported"
-         in
-
-         let case_concrete_pointer _prov i = 
-         let t rt_i = 
-           RT_EC ((n, Left BT_Loc), 
-                  RT_AndL (var_equal_to n (IT_Int i), rt_i)) 
-         in
-         return t
-         in
-
-         let case_unspecified_value _ = 
-           failwith "unspecified value pointer"
-         in
-
-         case_ptrval 
-           p
-           case_null 
-           case_function_pointer 
-           case_concrete_pointer 
-           case_unspecified_value
-
-      | OVarray _ -> failwith "OVarray not implemented"
-      | OVstruct _ -> failwith "OVstruct not implemented"
-      end
-
-    | Vloaded lv -> 
-       failwith "infer_type_value Vloaded"
+    
+  let rec value_aux (cv,lv,lc) outer_rt inner_rt : return_type m =
+    begin match outer_rt with
+    | Vobject ov -> object_aux (cv,lv,lc) ov inner_rt
+    | Vloaded lv -> failwith "infer_type_value Vloaded"
     | Vunit -> 
-       let t rt_i = RT_EC ((n, Left BT_Unit), rt_i) in
-       return t
+       fresh >>= fun n ->
+       return (RT_EC ((n, Left BT_Unit), inner_rt))
     | Vtrue -> 
-       let t rt_i = 
-         RT_EC ((n, Left BT_Bool), 
-                RT_AndL (var_equal_to n (IT_Bool true), rt_i))  
-       in
-       return t
+       fresh >>= fun n ->
+       return (RT_EC ((n, Left BT_Bool), 
+                      RT_AndL (var_equal_to n (Bool true), inner_rt)))
     | Vfalse ->
-       let t rt_i = 
-         RT_EC ((n, Left BT_Bool), 
-                RT_AndL (var_equal_to n (IT_Bool false), rt_i))
-       in
-       return t
-    | Vctype _ ->
-       failwith "Vctype not supported"
-    | Vlist _ -> 
-       failwith "infer_type_value Vlist"
+       fresh >>= fun n ->
+       return (RT_EC ((n, Left BT_Bool), 
+                      RT_AndL (var_equal_to n (Bool false), inner_rt)))
+    | Vctype _ -> failwith "Vctype not supported"
+    | Vlist _ ->  failwith "infer_type_value Vlist"
     | Vtuple vs -> 
-       aux_list (cv,lv,lc) vs >>= fun ts ->
-       let t rt_i = RT_EC ((n, Right ts), rt_i) in
-       return t
+       fresh >>= fun n ->
+       value_aux_list (cv,lv,lc) vs >>= fun ts ->
+       return (RT_EC ((n, Right ts), inner_rt))
     end
 
-  and aux_list (cv,lv,lc) vs : return_type m = 
+  and value_aux_list (cv,lv,lc) vs : return_type m = 
     begin match vs with
     | [] -> 
        return RT_Done
     | v :: vs -> 
-       aux (cv,lv,lc) v >>= fun vt ->
-       aux_list (cv,lv,lc) vs >>= fun vts ->
-       return (vt vts)
+       value_aux_list (cv,lv,lc) vs >>= fun vts ->
+       value_aux (cv,lv,lc) v vts
     end
   in
 
-  aux (cv,lv,lc) v >>= fun vt ->
-  return (vt RT_Done)
+  value_aux (cv,lv,lc) v RT_Done
+
 
 
 let test_parse () = 
   let s = "(not ((1 + (x * 3)) < (2 + (x * 3))))" in
-  print_endline (pp_logical_constraint (sexp_to_logical_constraint (of_string s)));
+  print_endline (pp_logical_constraint (sexp_to_logical_constraint (Sexp.of_string s)));
   let s = "(array x (1 + (5 * y)) (fn i (i + 10)))" in
-  print_endline (pp_resource_type (sexp_to_resource_type (of_string s)));
+  print_endline (pp_resource_type (sexp_to_resource_type (Sexp.of_string s)));
   let s = "((list int) -> loc)" in
-  print_endline (pp_logical_sort (sexp_to_logical_sort (of_string s)));
+  print_endline (pp_logical_sort (sexp_to_logical_sort (Sexp.of_string s)));
   let s = "(E (r : int) . (r = (f i)) & (array x n f) * I)" in
-  print_endline (pp_return_type (sexp_to_return_type (of_string s)));
+  print_endline (pp_return_type (sexp_to_return_type (Sexp.of_string s)));
   let s = "(A (x : loc) . A (i : int) . AL (n : int) . AL (f : (int -> int)) . ((0 <= i) & (i < n)) => (array x n f) =* EL (r : int) . (r = (f i)) & (array x n f) * I)" in
-  print_endline (pp_function_type (sexp_to_function_type (of_string s)));
+  print_endline (pp_function_type (sexp_to_function_type (Sexp.of_string s)));
   print_endline "\n\n";
   ()
 
@@ -577,10 +592,11 @@ let test_value_infer () =
 
 
 let pp_fun_map_decl f = 
-  print_string (Pp_utils.to_plain_string (Pp_core.All.pp_funinfo_with_attributes f))
+  let pp = Pp_core.All.pp_funinfo_with_attributes f in
+  print_string (Pp_utils.to_plain_string pp)
 
-let check (core_file : unit Core.file) = 
-  (* let _tags = Tags.tagDefs () in *)
-  let _ = pp_fun_map_decl core_file.funinfo in
+let check (core_file : unit Core.file) =
+  let () = Tags.set_tagDefs core_file.tagDefs in
+  let () = pp_fun_map_decl core_file.funinfo in
   test_value_infer ()
 
