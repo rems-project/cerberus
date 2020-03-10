affdb9ad7f19f4caf153efbb61d60ef7054fe377
Christopher Pulte <cp526@cam.ac.uk>
2020-03-10 13:18:32 +0000

    make anormalisation target mucore directly

--- backend/csrt/src/check.ml
+++ backend/csrt/src/check.ml
@@ -7,7 +7,38 @@ open Sexp
 open Printf
 
 
-type id = string
+
+module Sym = struct
+
+  type sym = Symbol.sym
+
+
+
+  module Env = struct
+
+    type t = (string, Symbol.sym) Pmap.map
+
+    let new_env = Pmap.empty
+    let lookup name env = Pmap.lookup name env
+    let add name sym env = Pmap.add name sym env
+
+  end
+
+  let fresh = Symbol.fresh
+  let fresh_pretty = Symbol.fresh_pretty
+  let pp = Pp_symbol.to_string_pretty
+
+  let parse (env : Env.t) name = 
+    match Env.lookup name env with
+    | None -> failwith (sprintf "unbound variable %s" name)
+    | Some sym -> sym
+
+end
+
+open Sym
+
+
+
 type num = Nat_big_num.num
 type loc = num
 
@@ -31,7 +62,7 @@ let unreachable () = failwith "unreachable"
 module IT = struct
 
   type index_term =
-    | Var of id
+    | Var of sym
     | Num of num
     | Bool of bool
     | Add of index_term * index_term
@@ -42,10 +73,10 @@ module IT = struct
     | App of index_term * index_term
     | List of index_term list
 
-  let var id = Var id
+  let var sym = Var sym
 
   let rec pp = function
-    | Var id -> id
+    | Var sym -> Sym.pp sym
     | Num i -> Nat_big_num.to_string i
     | Bool true -> "true"
     | Bool false -> "false"
@@ -58,22 +89,31 @@ module IT = struct
     | List its -> sprintf "(list %s)" (pp_to_sexp_list pp its)
 
 
-  let rec parse_sexp sx = 
+  let rec parse_sexp env sx = 
     match sx with
-    | Atom "true" -> Bool true
-    | Atom "false" -> Bool false
+    | Atom "true" -> 
+       Bool true
+    | Atom "false" -> 
+       Bool false
     | Atom str when Str.string_match (Str.regexp "[0-9]+") str 0 ->
        Num (Nat_big_num.of_string str)
-    | Atom str -> Var str
-    | List [o1;Atom "+";o2] -> Add (parse_sexp o1, parse_sexp o2)
-    | List [o1;Atom "-";o2] -> Sub (parse_sexp o1, parse_sexp o2)
-    | List [o1;Atom "*";o2] -> Mul (parse_sexp o1, parse_sexp o2)
-    | List [o1;Atom "/";o2] -> Div (parse_sexp o1, parse_sexp o2)
-    | List [o1;Atom "^";o2] -> Exp (parse_sexp o1, parse_sexp o2)
-    | List [Atom "list"; its] -> List (parse_sexp_list parse_sexp its)
-    | List [o1;o2] -> App (parse_sexp o1, parse_sexp o2)
-    | t -> parse_error "index term" t
-
+    | Atom str -> Var (Sym.parse env str)
+    | List [o1;Atom "+";o2] -> 
+       Add (parse_sexp env o1, parse_sexp env o2)
+    | List [o1;Atom "-";o2] -> 
+       Sub (parse_sexp env o1, parse_sexp env o2)
+    | List [o1;Atom "*";o2] -> 
+       Mul (parse_sexp env o1, parse_sexp env o2)
+    | List [o1;Atom "/";o2] -> 
+       Div (parse_sexp env o1, parse_sexp env o2)
+    | List [o1;Atom "^";o2] -> 
+       Exp (parse_sexp env o1, parse_sexp env o2)
+    | List [Atom "list"; its] -> 
+       List (parse_sexp_list (parse_sexp env) its)
+    | List [o1;o2] -> 
+       App (parse_sexp env o1, parse_sexp env o2)
+    | t -> 
+       parse_error "index term" t
 end
 
 
@@ -97,35 +137,42 @@ module LP = struct
     | GE (o1,o2) -> sprintf "(%s >= %s)" (IT.pp o1) (IT.pp o2)
     | Null o1 -> sprintf "(null %s)" (IT.pp o1)
 
-  let parse_sexp sx =
+  let parse_sexp env sx =
     match sx with 
-    | List [o1;Atom "=";o2]  -> EQ (IT.parse_sexp o1, IT.parse_sexp o2)
-    | List [o1;Atom "<>";o2] -> NE (IT.parse_sexp o1, IT.parse_sexp o2)
-    | List [o1;Atom "<";o2]  -> LT (IT.parse_sexp o1, IT.parse_sexp o2)
-    | List [o1;Atom ">";o2]  -> GT (IT.parse_sexp o1, IT.parse_sexp o2)
-    | List [o1;Atom "<=";o2] -> LE (IT.parse_sexp o1, IT.parse_sexp o2)
-    | List [o1;Atom ">=";o2] -> GE (IT.parse_sexp o1, IT.parse_sexp o2)
-    | t -> parse_error "logical predicate" t
+    | List [o1;Atom "=";o2]  -> 
+       EQ (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | List [o1;Atom "<>";o2] -> 
+       NE (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | List [o1;Atom "<";o2]  -> 
+       LT (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | List [o1;Atom ">";o2]  -> 
+       GT (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | List [o1;Atom "<=";o2] -> 
+       LE (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | List [o1;Atom ">=";o2] -> 
+       GE (IT.parse_sexp env o1, IT.parse_sexp env o2)
+    | t -> 
+       parse_error "logical predicate" t
 end
 
 
 module LC = struct
 
   type logical_constraint =
-    | Pred of LP.logical_predicate
     | And of logical_constraint * logical_constraint
     | Not of logical_constraint
+    | Pred of LP.logical_predicate
 
   let rec pp = function
     | And (o1,o2) -> sprintf "(%s & %s)" (pp o1) (pp o2)
     | Not (o1) -> sprintf "(not %s)" (pp o1)
     | Pred p -> LP.pp p
 
-  let rec parse_sexp sx =
+  let rec parse_sexp env sx =
     match sx with
-    | List [Atom "not";op] -> Not (parse_sexp op)
-    | List [o1; Atom "&"; o2] -> And (parse_sexp o1, parse_sexp o2)
-    | l -> Pred (LP.parse_sexp l)
+    | List [Atom "not";op] -> Not (parse_sexp env op)
+    | List [o1; Atom "&"; o2] -> And (parse_sexp env o1, parse_sexp env o2)
+    | l -> Pred (LP.parse_sexp env l)
 
 end
 
@@ -137,22 +184,22 @@ module BT = struct
     | Bool
     | Int
     | Loc
-    | Struct of id
+    | Struct of sym
 
   let rec pp = function
     | Unit -> "unit"
     | Bool -> "bool"
     | Int -> "int"
     | Loc -> "loc"
-    | Struct id -> sprintf "(struct %s)" id
+    | Struct sym -> sprintf "(struct %s)" (Sym.pp sym)
 
-  let rec parse_sexp sx = 
+  let rec parse_sexp env sx = 
     match sx with
     | Atom "unit" -> Unit
     | Atom "bool" -> Bool
     | Atom "int" -> Int
     | Atom "loc" -> Loc
-    | List [Atom "struct"; Atom id] -> Struct id
+    | List [Atom "struct"; Atom id] -> Struct (Sym.parse env id)
     | a -> parse_error "base type" a
 
 end
@@ -191,24 +238,24 @@ module RE = struct
          (IT.pp it2)
          (pp rt)
 
-  let rec parse_sexp sx = 
+  let rec parse_sexp env sx = 
     match sx with 
     | List [Atom "block";it1;it2] -> 
-       Block (IT.parse_sexp it1,
-              IT.parse_sexp it2)
+       Block (IT.parse_sexp env it1,
+              IT.parse_sexp env it2)
     | List [Atom "int"; it1; it2] ->
-       Int (IT.parse_sexp it1, 
-            IT.parse_sexp it2)
+       Int (IT.parse_sexp env it1, 
+            IT.parse_sexp env it2)
     | List [Atom "array"; it1; it2] ->
-       Array (IT.parse_sexp it1, 
-              IT.parse_sexp it2)
+       Array (IT.parse_sexp env it1, 
+              IT.parse_sexp env it2)
     | List [Atom "points"; it1; it2] ->
-       Points (IT.parse_sexp it1, 
-               IT.parse_sexp it2)
+       Points (IT.parse_sexp env it1, 
+               IT.parse_sexp env it2)
     | List [Atom "uninit"; it1; it2; rt] ->
-       Uninit (IT.parse_sexp it1, 
-               IT.parse_sexp it2, 
-               parse_sexp rt)
+       Uninit (IT.parse_sexp env it1, 
+               IT.parse_sexp env it2, 
+               parse_sexp env rt)
     | t -> parse_error "resource type" t
 end
 
@@ -232,56 +279,82 @@ module LS = struct
     | Base bt -> 
          BT.pp bt
 
-  let rec parse_sexp sx =
+  let rec parse_sexp env sx =
     match sx with
     | Sexp.List [Atom "list"; a] ->
-       List (BT.parse_sexp a)
+       List (BT.parse_sexp env a)
     | Sexp.List [o1; Atom "->"; o2] ->
-       Fun (parse_sexp o1, parse_sexp o2)
-    | ((Sexp.Atom _) as a) -> Base (BT.parse_sexp a)
+       Fun (parse_sexp env o1, parse_sexp env o2)
+    | ((Sexp.Atom _) as a) -> 
+       Base (BT.parse_sexp env a)
     | ls -> parse_error "logical sort" ls
 
 end
 
 
+
+
+
 module RT = struct
 
-  type return_item = 
-    | A of id * BT.base_type
-    | L of id * LS.logical_sort
-    | R of RE.resource
-    | C of LC.logical_constraint
+  type return_type = 
+    | A of (sym * BT.base_type) * return_type
+    | L of (sym * LS.logical_sort) * return_type
+    | R of (sym * RE.resource) * return_type
+    | C of (sym * LC.logical_constraint) * return_type
+    | I
 
-  type return_type = return_item list
+  let a sym bt rt = A ((sym,bt),rt)
+  let l sym ls rt = L ((sym,ls),rt)
+  let r sym re rt = R ((sym,re),rt)
+  let c sym lc rt = C ((sym,lc),rt)
+
+  let comb f g x = f (g (x))
+
+  let rec combine (ris : (return_type -> return_type) list) : return_type -> return_type = 
+    match ris with
+    | [] -> (fun ri -> ri)
+    | ri :: ris -> comb ri (combine ris)
+
+  let make ris = combine ris I
 
 
   let rec pp = function
-    | A (id, typ) :: ret -> 
-       sprintf "EC (%s : %s) . %s" id (BT.pp typ) (pp ret)
-    | L (id, ls) :: ret  -> 
-       sprintf "EL (%s : %s) . %s" id (LS.pp ls) (pp ret)
-    | R rt :: ret -> 
-       sprintf "%s * %s" (RE.pp rt) (pp ret)
-    | C lc :: ret -> 
-       sprintf "%s & %s" (LC.pp lc) (pp ret)
-    | [] -> 
+    | A ((id, typ), rt) -> 
+       sprintf "EC (%s : %s) . %s" (Sym.pp id) (BT.pp typ) (pp rt)
+    | L ((id, ls), rt)  -> 
+       sprintf "EL (%s : %s) . %s" (Sym.pp id) (LS.pp ls) (pp rt)
+    | R ((id, re), rt) -> 
+       sprintf "(%s : %s) * %s" (Sym.pp id) (RE.pp re) (pp rt)
+    | C ((id, lc), rt) -> 
+       sprintf "(%s : %s) & %s" (Sym.pp id) (LC.pp lc) (pp rt)
+    | I -> 
        "I"
 
-  let rec parse_sexp = function
-    | Atom "EC" :: List [Atom id; Atom ":"; t] :: Atom "." :: ret ->
-       A (id, BT.parse_sexp t) :: parse_sexp ret
-    | Atom "EL" :: List [Atom id; Atom ":"; ls] :: Atom "." :: ret ->
-       L (id, LS.parse_sexp ls) :: parse_sexp ret
-    | rt :: Atom "*" :: ret ->
-       R (RE.parse_sexp rt) :: parse_sexp ret
-    | lc :: Atom "&" :: ret ->
-       C (LC.parse_sexp lc) :: parse_sexp ret
+  let rec parse_sexp env = function
+    | Atom "EC" :: List [Atom id; Atom ":"; t] :: Atom "." :: rt ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       A ((sym, BT.parse_sexp env t), parse_sexp env' rt)
+    | Atom "EL" :: List [Atom id; Atom ":"; ls] :: Atom "." :: rt ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       L ((sym, LS.parse_sexp env ls), parse_sexp env' rt)
+    | List [Atom id; Atom ":"; re] :: Atom "*" :: rt ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       R ((sym, RE.parse_sexp env re), parse_sexp env' rt)
+    | List [Atom id; Atom ":"; lc] :: Atom "&" :: rt ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       C ((sym, LC.parse_sexp env lc), parse_sexp env' rt)
     | Atom "I" :: [] -> 
-       []
+       I
     | rt -> 
-       parse_error "return type" (List rt)
+       parse_error "rturn type" (List rt)
 
 end
+
 
 
 module FT = struct
@@ -290,44 +363,52 @@ module FT = struct
   (* basically the same as return_item, so could unify if it stays
      like this? *)
   type argument = 
-    | A of id * BT.base_type
-    | L of id * LS.logical_sort
-    | R of RE.resource
-    | C of LC.logical_constraint
+    | A of sym * BT.base_type
+    | L of sym * LS.logical_sort
+    | R of sym * RE.resource
+    | C of sym * LC.logical_constraint
 
   type arguments = argument list
 
   type function_type = Fn of arguments * RT.return_type
 
   let rec pp_arguments = function
-    | A (id, bt) :: args -> 
-       sprintf "AC (%s : %s) . %s" id (BT.pp bt) (pp_arguments args)
-    | L (id, ls) :: args -> 
-       sprintf "AL (%s : %s) . %s" id (LS.pp ls) (pp_arguments args)
-    | R (rt) :: args ->
-       sprintf "%s =* %s" (RE.pp rt) (pp_arguments args)
-    | C (lc) :: args ->
-       sprintf "%s => %s" (LC.pp lc) (pp_arguments args)
+    | A (sym, bt) :: args -> 
+       sprintf "AC (%s : %s) . %s" (Sym.pp sym) (BT.pp bt) (pp_arguments args)
+    | L (sym, ls) :: args -> 
+       sprintf "AL (%s : %s) . %s" (Sym.pp sym) (LS.pp ls) (pp_arguments args)
+    | R (sym, rt) :: args ->
+       sprintf "(%s : %s) =* %s" (Sym.pp sym) (RE.pp rt) (pp_arguments args)
+    | C (sym, lc) :: args ->
+       sprintf "(%s : %s) => %s" (Sym.pp sym) (LC.pp lc) (pp_arguments args)
     | [] -> 
        ""
 
   let pp (Fn (args, rt)) = 
     sprintf "%s%s" (pp_arguments args) (RT.pp rt)
 
-  let rec parse_sexp_aux acc = function
+  let rec parse_sexp_aux acc env = function
     | Atom "AC" :: List [Atom id; Atom ":"; bt] :: Atom "." :: args ->
-       parse_sexp_aux (A (id, BT.parse_sexp bt) :: acc) args
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       parse_sexp_aux (A (sym, BT.parse_sexp env bt) :: acc) env' args
     | Atom "AL":: List [Atom id; Atom ":"; ls] :: Atom "." :: args ->
-       parse_sexp_aux (L (id, LS.parse_sexp ls) :: acc) args
-    | rt :: Atom "=*" :: args ->
-       parse_sexp_aux (R (RE.parse_sexp rt) :: acc) args
-    | lc :: Atom "=>" :: args ->
-       parse_sexp_aux (C (LC.parse_sexp lc) :: acc) args
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       parse_sexp_aux (L (sym, LS.parse_sexp env ls) :: acc) env' args
+    | List [Atom id; Atom ":"; re] :: Atom "=*" :: args ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       parse_sexp_aux (R (sym, RE.parse_sexp env re) :: acc) env' args
+    | List [Atom id; Atom ":"; lc] :: Atom "=>" :: args ->
+       let sym = Sym.fresh_pretty id in
+       let env' = Sym.Env.add id sym env in
+       parse_sexp_aux (C (sym, LC.parse_sexp env lc) :: acc) env' args
     | rt -> 
-       Fn (List.rev acc, RT.parse_sexp rt)
+       Fn (List.rev acc, RT.parse_sexp env rt)
 
-  let parse_sexp = function
-    | List ft -> parse_sexp_aux [] ft
+  let parse_sexp env = function
+    | List ft -> parse_sexp_aux [] env ft
     | t -> parse_error "function type" t
          
 end
@@ -339,7 +420,7 @@ end
 
 (* Programs and terms *)
 
-type 'a variable_context = (id * 'a) list
+type 'a variable_context = (sym * 'a) list
 
 type cv_context = BT.base_type variable_context
 type lv_context = LS.logical_sort variable_context
@@ -354,55 +435,18 @@ let empty_context = []
 
 (* value type inference *)
 
-module ID : sig
-  type id = string
-  type id_state
-  val init : id_state
-  val fresh : id_state -> id * id_state
-end = struct 
-  type id = string
-  type id_state = int
-  let init = 0
-  let fresh ids = (sprintf "%s%d" "_tv_" ids, ids + 1)
-end
 
-module ID_M = struct
-  include ID
-  type 'a m = id_state -> ('a * id_state)
-  let return (a : 'a) : 'a m = 
-    fun s -> (a,s)
-  let bind (m : 'a m) (f : 'a -> 'b m) : 'b m = 
-    fun s -> let a, s' = m s in f a s'
-  let (>>=) = bind
-
-  let rec doM (ms : ('a m) list) : ('a list) m = 
-    match ms with
-    | [] -> return []
-    | m :: ms -> 
-       m >>= fun x -> 
-       doM ms >>= fun xs ->
-       return (x :: xs)
-  let foreach (f : 'a -> 'b m) (xs : 'a list) : ('b list) m = 
-    doM (List.map f xs)
-    
-  let liftM (f : 'a -> 'b) (x : 'a) : 'b m =
-    return (f x)
-end
 
 
 
 open Core
-open ID_M
 
 
+let var_equal_to (sym : sym) (it : IT.index_term) = 
+  LC.Pred (EQ (Var sym, it))
 
-
-
-let var_equal_to (id : id) (it : IT.index_term) = 
-  LC.Pred (EQ (Var id, it))
-
-let var_null (id : id) = 
-  LC.Pred (Null (Var id))
+let var_null (sym : sym) = 
+  LC.Pred (Null (Var sym))
 
 
 let option_get = function
@@ -422,40 +466,48 @@ let option_get = function
 
 
 
-let rec infer_lv lv : (id * RT.return_type) m = 
+let rec infer_lv lv : sym * (RT.return_type -> RT.return_type) = 
   match lv with
   | LVspecified ov -> infer_ov ov
   | LVunspecified _ -> failwith "LVunspecified not implemented"
 
-and infer_ov ov : (id * RT.return_type) m = 
+and infer_ov ov : sym * (RT.return_type -> RT.return_type) = 
   match ov with
   | OVinteger i -> 
-     fresh >>= fun n ->
+     let n, lc = fresh (), fresh () in
      let i = option_get (eval_integer_value i) in
-     return (n, [RT.A (n,BT.Int); RT.C (var_equal_to n (Num i))])
+     let constr = var_equal_to n (Num i) in
+     (n, RT.combine [RT.a n BT.Int; RT.c lc constr])
   | OVfloating _ -> 
      failwith "floats not supported"
   | OVpointer p -> 
-     fresh >>= fun n ->
-     case_ptrval p 
-       (fun _ctype ->  (* case null *)
-         return (n, [RT.A (n,BT.Loc); RT.C (var_null n)]))
-       (fun _sym ->    (* case function pointer *)
-         failwith "function pointers not supported")
-       (fun _prov i -> (* case concrete pointer *)
-        return (n, [RT.A (n,BT.Loc); RT.C (var_equal_to n (Num i))]))
-       (fun _ ->       (* case unspecified_value *)
-         unreachable ())
+     let n = fresh () in
+     let rt = 
+       case_ptrval p 
+         (fun _ctype ->  (* case null *)
+           let lc = fresh () in
+           RT.combine [RT.a n BT.Loc; RT.c lc (var_null n)])
+         (fun _sym ->    (* case function pointer *)
+           failwith "function pointers not supported")
+         (fun _prov i -> (* case concrete pointer *)
+           let constr = var_equal_to n (Num i) in
+           let lc = fresh () in
+           RT.combine [RT.a n BT.Loc; RT.c lc constr])
+         (fun _ ->       (* case unspecified_value *)
+           unreachable ())
+     in
+     (n, rt)
   | OVarray os -> 
      (* let n = of_int (List.length os) in *)
-     fresh >>= fun pointer ->
-     foreach infer_lv os >>= fun os ->
-     let names, items = List.split os in
+     let pointer = fresh () in
+     let names, items = List.split (List.map infer_lv os) in
+     let vars = List.map IT.var names in
+     let r = fresh () in
      let ts = 
-       (RT.A (pointer, BT.Loc) :: List.concat items) @
-       [RT.R (Array (IT.Var pointer, IT.List (List.map IT.var names)))]
+       ((RT.a pointer BT.Loc) :: List.concat items) @
+       [RT.r r (Array (IT.Var pointer, IT.List vars))]
      in
-     return (pointer, ts)
+     (pointer, RT.make ts)
   | OVstruct (sym,flds) -> 
      failwith "OVstruct not implemented"
   | OVunion _ -> 
@@ -540,9 +592,9 @@ let print_core_file core_file filename =
 
 let check (core_file : unit Core.typed_file) =
   let normalised_core_file = Core_anormalise.normalise_file core_file in
-  print_core_file core_file "out1";
   Tags.set_tagDefs core_file.tagDefs;
   pp_fun_map_decl core_file.funinfo;
+  print_core_file core_file "out1";
   print_core_file normalised_core_file "out2";
   let _mu_file = Mucore.core_to_mu__file normalised_core_file in
   (* test_parse (); *)
