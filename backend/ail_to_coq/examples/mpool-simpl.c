f1a81ec3f852544c359a82f86fade1c909e24b0d
Rodolphe Lepigre <lepigre@mpi-sws.org>
2020-03-14 01:40:13 +0100

    [ail_to_coq] annotation parsing (needs "opam install earley")

--- backend/ail_to_coq/examples/mpool-simpl.c
+++ backend/ail_to_coq/examples/mpool-simpl.c
@@ -1,25 +1,30 @@
 #define ENTRY_SIZE 4096
-#define NULL ((void*) 0)
+#define NULL (void*)0
 
-struct mpool_entry {
-    /*@ size: ENTRY_SIZE, refined by: n: nat
-      next := (n > 0):option<owned_ptr<(n - 1):struct mpool_entry>> */
+struct [[rc::size("ENTRY_SIZE"), rc::refined_by("n : nat")]] mpool_entry {
+    [[rc::field("(n > 0) @ optional<&own<(n - 1) @ struct mpool_entry>>")]]
     struct mpool_entry *next;
 };
 
-struct mpool {
-    /*@ refined by: n: nat
-      entry_list := (n > 0):option<owned_ptr<(n - 1):struct mpool_entry>> */
+struct [[rc::refined_by("n : nat")]] mpool {
+    [[rc::field("(n > 0) @ optional<&own<(n - 1) @ struct mpool_entry>>")]]
     struct mpool_entry *entry_list;
 };
 
-//@ mpool_init := fn(p:owned_ptr<uninit<struct mpool>>) -> void; p:owned_ptr<0:struct mpool>>
+[[
+  rc::args("{p} @ &own<!uninit<{struct_mpool}>>"),
+  rc::returns("void"),
+  rc::ensures("p @ &own<{0} @ mpool>")
+]]
 void mpool_init(struct mpool *p) {
     p->entry_list = NULL;
 }
 
-/*@ mpool_get := fn(p:owned_ptr<n:struct mpool>) ->
-      (n > 0):option<owned_ptr<uninit<ENTRY_SIZE>>>; p:owned_ptr<(max(0, n - 1)):struct mpool>> */
+[[
+  rc::args("{p} @ &own<{n} @ mpool>"),
+  rc::returns("{n > 0} @ optional<&own<!uninit<{ENTRY_SIZE}>>>"),
+  rc::ensures("p @ &own<{n - 1} @ mpool>")
+]]
 void *mpool_get(struct mpool *p) {
     if (p->entry_list == NULL) {
         return NULL;
@@ -29,19 +34,23 @@ void *mpool_get(struct mpool *p) {
     return entry;
 }
 
-/*@ mpool_put := fn(p:owned_ptr<n:struct mpool>, owned_ptr<uninit<ENTRY_SIZE>>) -> void;
-      p:owned_ptr<(n + 1):struct mpool>> */
+[[
+  rc::args("{p} @ &own<{n} @ mpool>", "&own<!uninit<{ENTRY_SIZE}>>"),
+  rc::returns("void"),
+  rc::ensures("p @ &own<{n + 1} @ mpool>")
+]]
 void mpool_put(struct mpool *p, void *ptr) {
-    //@ ptr -> owned_ptr<padded<uninit (struct mpool_entry), _, ENTRY_SIZE>>
     struct mpool_entry *e = ptr;
+    [[rc::subtype("padded<uninit (struct mpool_entry), _, ENTRY_SIZE>>")]]
+    *e;
     e->next = p->entry_list;
     p->entry_list = e;
 }
 
-void * e1, * e2;
+void* e1, *e2;
 int main(void) {
     struct mpool p;
-    void * p1, * p2;
+    void * p1, *p2;
     mpool_init(&p);
     mpool_put(&p, &e1);
     mpool_put(&p, &e2);
