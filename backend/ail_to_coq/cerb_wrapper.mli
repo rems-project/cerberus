73b71b6693fb4179c4d8d7add84c91080540d72c
Rodolphe Lepigre <lepigre@mpi-sws.org>
2020-04-13 22:31:02 +0200

    [ail_to_coq] add a [-I] option for include dirs.
    Note that we do not rely on the HAFNIUM_PATH environment any more.
    There is also a new [--nostdinc] option to disable the automatic
    addition of the include directory for the standard library.

--- backend/ail_to_coq/cerb_wrapper.mli
+++ backend/ail_to_coq/cerb_wrapper.mli
@@ -1,4 +1,7 @@
-(** [c_file_to_ail fname] uses the Cerberus frontend to preprocess, parse, and
-    type-check the C source file [fname]. In case of error an error message is
-    displayed and the program fails with error code [-1]. *)
-val c_file_to_ail : string -> Ail_to_coq.typed_ail
+(** [c_file_to_ail includes nostdinc fname] uses Cerberus to preprocess, parse
+    and also type-check the C source file [fname]. All the directory in of the
+    [includes] list are passed to the C preprocessor through the ["-I"] option
+    (they are hence searched for header files). If [nostdinc] then the default
+    system directories are not searched for header files. In case of error, an
+    a message is displayed and the program exits with error code [-1]. *)
+val c_file_to_ail : string list -> bool -> string -> Ail_to_coq.typed_ail
