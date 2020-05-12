31811ecbfda75c18dbdb4b3e13c9001061f2a0f0
Christopher Pulte <cp526@cam.ac.uk>
2020-05-12 21:20:14 +0100

    make Points have size

--- backend/csrt/resources.ml
+++ backend/csrt/resources.ml
@@ -8,7 +8,7 @@ module Loc=Locations
 type t = 
   | Block of IndexTerms.t * IndexTerms.t (* size *)
   (* | Uninitialised of IndexTerms.t * Ctype.ctype *)
-  | Points of IndexTerms.t * IndexTerms.t
+  | Points of IndexTerms.t * IndexTerms.t * IndexTerms.t
   | Struct of Sym.t
 
 let pp = function
@@ -16,8 +16,8 @@ let pp = function
      parens (!^"block" ^^^ IndexTerms.pp it1 ^^^ IndexTerms.pp it2)
   (* | Uninitialised (it, ct) ->
    *    parens (!^"uninit" ^^^ IndexTerms.pp it ^^^ squotes (Pp_core_ctype.pp_ctype ct) *)
-  | Points (it1,it2) -> 
-     parens (!^"points" ^^^ IndexTerms.pp it1 ^^^ IndexTerms.pp it2)
+  | Points (it1,it2,it3) ->     (* it2: what it points to, it3: size *)
+     parens (!^"points" ^^^ IndexTerms.pp it1 ^^^ IndexTerms.pp it2 ^^^ IndexTerms.pp it3)
   | Struct sym ->
      Colour.pp_ansi_format [Red;Bold] (parens (!^"struct" ^^^ Sym.pp sym))
 
@@ -31,9 +31,10 @@ let subst sym with_it t =
              IndexTerms.subst sym with_it it')
   (* | Uninitialised (it,ct) ->
    *    Uninitialised (IndexTerms.subst sym with_it it, ct) *)
-  | Points (it, it') -> 
+  | Points (it, it', it'') -> 
      Points (IndexTerms.subst sym with_it it, 
-             IndexTerms.subst sym with_it it')
+             IndexTerms.subst sym with_it it',
+             IndexTerms.subst sym with_it it'')
   | Struct s ->
      Struct (Sym.subst sym with_it s)
 
@@ -46,9 +47,10 @@ let types_equal env ts1 ts2 =
 
 let unify r1 r2 res = 
   match r1, r2 with
-  | Points (it1, it2), Points (it1', it2') ->
+  | Points (it1, it2, it3), Points (it1', it2', it3') ->
      IndexTerms.unify it1 it1' res >>= fun res ->
-     IndexTerms.unify it2 it2' res
+     IndexTerms.unify it2 it2' res >>= fun res ->
+     IndexTerms.unify it3 it3' res
   | Block (it1, it2), Block (it1', it2') ->
      IndexTerms.unify it1 it1' res >>= fun res ->
      IndexTerms.unify it2 it2' res
@@ -71,14 +73,14 @@ let unify r1 r2 res =
 
 
 let owner = function
-  | Points (S (v,_), _) -> Some v
+  | Points (S (v,_), _, _) -> Some v
   | Block (S (v,_), _) -> Some v
   | Struct s -> Some s
   | _ -> None
 
 let owned = function
-  | Points (_, S (v,_)) -> [v]
-  | Points (_, _) -> []
+  | Points (_, S (v,_), _) -> [v]
+  | Points (_, _, _) -> []
   | Struct _ -> []
   | Block _ -> []
 
