8a270f9956664e31a7ce33921ab215f14ac6075f
Christopher Pulte <cp526@cam.ac.uk>
2020-06-17 20:30:18 +0100

    alternative symbol pp

--- ocaml_frontend/pprinters/pp_symbol.mli
+++ ocaml_frontend/pprinters/pp_symbol.mli
@@ -4,6 +4,10 @@ open Symbol
 val to_string: sym -> string
 val to_string_pretty: sym -> string
 
+val alt_to_string: sym -> string
+val alt_to_string_pretty: sym -> string
+
+
 val pp_prefix: prefix -> PPrint.document
 
 
