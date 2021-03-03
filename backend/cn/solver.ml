1d4c892446aa5edb00d6a521d2cc9b74a9defe46
Christopher Pulte <cp526@cam.ac.uk>
2021-03-03 21:07:46 +0000

    tweaks

--- backend/cn/solver.ml
+++ backend/cn/solver.ml
@@ -8,14 +8,25 @@ module Make (G : sig val global : Global.t end) = struct
   let solver = Z3.Solver.mk_simple_solver context
 
   let holds local (it : IndexTerms.t) = 
+    (* let () = 
+     *   let open Pp in
+     *   let open Printexc in
+     *   Pp.print stderr (!^(raw_backtrace_to_string (get_callstack 2)))
+     * in *)
     let constraints = 
       SolverConstraints.of_index_term G.global (not_ it) ::
       L.all_solver_constraints local
     in
-    match Z3.Solver.check solver constraints with
+    let result = 
+      try Z3.Solver.check solver constraints with
+      | Z3.Error err -> 
+         Debug_ocaml.error ("Z3 error: " ^ err)
+    in
+    match result with
     | Z3.Solver.UNSATISFIABLE -> true
     | Z3.Solver.SATISFIABLE -> false
     | Z3.Solver.UNKNOWN -> Debug_ocaml.error "SMT solver returned 'unknown'"
+
 
   let is_inconsistent local = holds local (bool_ false)
 
