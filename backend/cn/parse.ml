68f54201d1638c7f947aad827a0a733dd423c756
Dhruv Makwana <dcm41@cl.cam.ac.uk>
2021-04-20 20:47:33 +1200

    Tmp

--- backend/cn/parse.ml
+++ backend/cn/parse.ml
@@ -18,7 +18,7 @@ let parse_condition default_label (loc, string) =
   let lexbuf = Lexing.from_string string in
   let () = 
     let open Location_ocaml in
-    Lexing.set_position lexbuf
+    lexbuf.lex_curr_p <-
       (* revisit *)
       begin match Location_ocaml.to_raw loc with
       | Loc_unknown -> lexbuf.lex_curr_p
