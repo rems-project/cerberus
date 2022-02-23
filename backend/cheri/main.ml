234b171e8dd354812ee26dcda823fcf6f2ac74d5
Vadim Zaliva <lord@crocodile.org>
2022-02-23 11:01:59 -0800

    morello implementation

--- backend/cheri/main.ml
+++ backend/cheri/main.ml
@@ -51,6 +51,7 @@ let frontend cpp_str filename =
     ; cpp_stderr= true
   } in
   Global_ocaml.(set_cerb_conf false Random false Basic false false false false);
+  Cerb_frontend.Ocaml_implementation.(set (MorelloImpl.impl));
   load_core_stdlib ()                                  >>= fun stdlib ->
   load_core_impl stdlib impl_name                      >>= fun impl   ->
   c_frontend (conf, io) (stdlib, impl) ~filename
