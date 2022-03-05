d95c62e0b1cfff09e7c4ce5fff551708271a5253
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-03-05 13:49:26 +0000

    removing invalid setting of tagDefs in `Pipeline.core_frontend`

--- backend/common/pipeline.ml
+++ backend/common/pipeline.ml
@@ -212,7 +212,7 @@ let core_frontend (conf, io) (core_stdlib, core_impl) ~filename =
   io.print_debug 2 (fun () -> "Using the Core frontend") >>= fun () ->
   Core_parser_driver.parse core_stdlib filename >>= function
     | Core_parser_util.Rfile (sym_main, globs, funs, tagDefs) ->
-        Tags.set_tagDefs tagDefs;
+        (* Tags.set_tagDefs "Pipeline.core_frontend" tagDefs; *)
         return {
            Core.main=   Some sym_main;
            Core.tagDefs= tagDefs;
