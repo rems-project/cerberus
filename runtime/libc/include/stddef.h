ab16c56386c8fa7192931c79c8b1ac121d160294
Vadim Zaliva <lord@crocodile.org>
2022-03-08 11:07:37 -0800

    re-defined max_align_t for CHERI

--- runtime/libc/include/stddef.h
+++ runtime/libc/include/stddef.h
@@ -5,7 +5,12 @@ typedef __cerbty_ptrdiff_t   ptrdiff_t;
 typedef __cerbty_size_t      size_t;
 typedef __cerbty_wchar_t     wchar_t;
 
+#ifdef __CHERI_PURE_CAPABILITY__
+typedef (void*) max_align_t;
+#else
 #define max_align_t          __cerbty_max_align_t;
+#endif
+
 #define NULL                 __cerbvar_NULL
 
 // offsetof() is now builtin in the parser
