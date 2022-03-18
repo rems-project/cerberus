9fb3aec38e96c16ef200169517a58016af6bf56b
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-03-18 01:54:50 +0000

    example for testing CN predicate definitions

---
+++ tests/cn/pred_def01.c
@@ -0,0 +1,36 @@
+// example for testing the predicate definition frontend
+
+struct T {
+  int something;
+};
+
+predicate {integer z, integer out2} MyPred (pointer p, integer n) = {
+  if ( n == 10 ) {
+    let foo = Owned<struct T>(p) ;
+    return { z = 42, out2 = 55 } ;
+  } else {
+    let r = MyPred(p, n + 10) ;
+    let x = n + 30 /*r.z*/ ;
+    return { z = n + 100, out2 = 55 } ;
+  }
+}
+
+
+struct int_list_items {
+  int value;
+  struct int_list_items* next;
+};
+
+/*
+TODO: list syntax is not done yet
+
+predicate {list<integer> v} IntList(pointer l) = {
+  if ( l == NULL ) {
+    return { v = [] } ;
+  } else {
+    let head_item = Owned<struct int_list_item>(l) ;
+    let tail = IntList(head_item.next) ;
+    return { v = head_item.value :: tail.v } ;
+  }
+}
+*/
