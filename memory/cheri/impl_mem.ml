6668d4eddf59bf33e1943187d7e7c31fecaa68bb
Vadim Zaliva <lord@crocodile.org>
2022-04-13 16:52:03 -0700

    int_bin simpified

--- memory/cheri/impl_mem.ml
+++ memory/cheri/impl_mem.ml
@@ -2659,35 +2659,21 @@ module CHERI (C:Capability
     match v1,v2 with
     | IV (prov1, n1), IV (prov2, n2)
       -> IV (pf prov1 prov2, vf n1 n2)
-    | IC (prov1, is_signed, c), IV (prov2, n2)
-      ->
-       let n1 = unwr is_signed @@ C.cap_get_value c in
-       (* TODO(CHERI): representability check? *)
-       (* The signedess follow IC value as intptr_t is higher
-          rank than any integer *)
-       let c = C.cap_set_value c (vfc is_signed n1 n2) in
-       IC (pf prov1 prov2, is_signed, c)
-    | IV (prov1, n1), IC (prov2, is_signed, c)
-      ->
-       let n2 = unwr is_signed @@ C.cap_get_value c in
-       (* TODO(CHERI): representability check? *)
-       (* The signedess follow IC value as intptr_t is higher
-          rank than any integer *)
-       let c = C.cap_set_value c (vfc is_signed n1 n2) in
-       IC (pf prov1 prov2, is_signed, c)
+    | IC _, IV _
+      | IV _, IC _ ->
+       failwith "int_bin value mismatch"
     | IC (prov1, is_signed1, c1), IC (prov2, is_signed2, c2)
       ->
-       let n1 = unwr is_signed1 @@ C.cap_get_value c1 in
-       let n2 = unwr is_signed2 @@ C.cap_get_value c2 in
-       (* TODO(CHERI): representability check? *)
-       (* Here we have emulate integer promotion rule where
-          if one of argumnets is unsgined the other has
-          to be cast to unsigned as well *)
-       let is_signed = is_signed1 && is_signed2 in
-       let n1 = if not is_signed && is_signed1 then wrap_cap n1 else n1 in
-       let n2 = if not is_signed && is_signed2 then wrap_cap n2 else n2 in
-       let c = C.cap_set_value c1 (vfc is_signed n1 n2) in
-       IC (pf prov1 prov2, is_signed, c)
+       if is_signed1 <> is_signed2
+       then failwith "int_bin sign mismatch"
+       else
+         let n1 = unwr is_signed1 @@ C.cap_get_value c1 in
+         let n2 = unwr is_signed1 @@ C.cap_get_value c2 in
+         (* TODO(CHERI): representability check? *)
+         let n1 = if not is_signed1 then wrap_cap n1 else n1 in
+         let n2 = if not is_signed1 then wrap_cap n2 else n2 in
+         let c = C.cap_set_value c1 (vfc is_signed1 n1 n2) in
+         IC (pf prov1 prov2, is_signed1, c)
 
   let op_ival iop v1 v2 =
     (* NOTE: for PNVI we assume that prov1 = prov2 = Prov_none *)
