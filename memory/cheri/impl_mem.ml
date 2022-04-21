eb876bf4fe952754bffa79a65c1b026956404079
Vadim Zaliva <lord@crocodile.org>
2022-04-21 16:59:00 -0700

    int_bin simplified

--- memory/cheri/impl_mem.ml
+++ memory/cheri/impl_mem.ml
@@ -301,6 +301,10 @@ module CHERI (C:Capability
        let n = C.cap_get_value c in
        if is_signed then unwrap_cap_value n else n
 
+  let prov_of_int = function
+    | IV (p,_) -> p
+    | IC (p,_,_) ->p
+
   type floating_value =
     (* TODO: hack hack hack ==> OCaml's float are 64bits *)
     float
@@ -2692,25 +2696,11 @@ module CHERI (C:Capability
        failwith "CHERI.combine_prov: found a Prov_symbolic"
 
   let int_bin loc pf vf v1 v2 =
-    let unwr s n = if s then unwrap_cap_value n else n in
-    (* NOTE: for PNVI we assume that prov1 = prov2 = Prov_none *)
-    match v1,v2 with
-    | IV (prov1, n1), IV (prov2, n2)
-      -> IV (pf prov1 prov2, vf n1 n2)
-    (* The two IC vs IV cases correspond to Core arithmetic
-       (e.g. from the body of wrapI()), this means that the IV is a
-       "logical" integer, not an value coming from C *)
-    | IC (prov1, is_signed, c), IV (prov2, n2) ->
-       let n1 = unwr is_signed @@ C.cap_get_value c in
-       IV (pf prov1 prov2, vf  n1 n2)
-    | IV (prov1, n1), IC (prov2, is_signed, c) ->
-       let n2 = unwr is_signed @@ C.cap_get_value c in
-       IV (pf prov1 prov2, vf  n1 n2)
-    | IC (prov1, is_signed1, c1), IC (prov2, is_signed2, c2)
-      ->
-       let n1 = unwr is_signed1 @@ C.cap_get_value c1 in
-       let n2 = unwr is_signed2 @@ C.cap_get_value c2 in
-       IV (pf prov1 prov2, vf n1 n2)
+    let n1 = num_of_int v1 in
+    let n2 = num_of_int v2 in
+    let p1 = prov_of_int v1 in
+    let p2 = prov_of_int v2 in
+    IV (pf p1 p2, vf n1 n2)
 
   let op_ival iop v1 v2 =
     (* NOTE: for PNVI we assume that prov1 = prov2 = Prov_none *)
