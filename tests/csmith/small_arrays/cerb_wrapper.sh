454711c97c562df4d904caedb7b1c49e9964055a
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-05-15 04:12:04 +0100

    using the locally built cerby in csmith tests

--- tests/csmith/small_arrays/cerb_wrapper.sh
+++ tests/csmith/small_arrays/cerb_wrapper.sh
@@ -1,1 +1,1 @@
-DYLD_LIBRARY_PATH=/Users/catzilla/Applications/usr/lib cerberus --cpp='cc -E -nostdinc -undef -D__cerb__ -I $CERB_PATH/include/c/libc -I $CERB_PATH/include/c/posix -DCSMITH_MINIMAL -I ../runtime' --sequentialise --exec $@
+../../../_build/default/backend/driver/main.exe -DCSMITH_MINIMAL -I ../runtime --sequentialise --typecheck-core --exec $@
