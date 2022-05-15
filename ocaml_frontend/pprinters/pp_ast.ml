ee650e8520178209dfe581b57ff135280285368d
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-05-15 04:24:43 +0100

    fix missing dquote when pprinting attributes

--- ocaml_frontend/pprinters/pp_ast.ml
+++ ocaml_frontend/pprinters/pp_ast.ml
@@ -29,7 +29,7 @@ let add_dtree_of_attributes (Annot.Attrs xs) dtrees =
                     end ^ string_of_ident attr.attr_id ^
                     "(" ^ String.concat ", " (List.map (fun (_, _, l) ->
                       String.concat " ++ " (List.map (fun (loc, z) ->
-                      String.escaped z ^ "\"") l)) attr.attr_args) ^  ")" in
+                      "\"" ^ String.escaped z ^ "\"") l)) attr.attr_args) ^  ")" in
                   Dleaf (!^ str)
                 ) xs )
         :: dtrees
