e9c3f9a3adfb5dff9f2fc1563310b548d9ba809c
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-06-06 00:40:54 +0100

    Update 0309-comma_void_operand.c

--- tests/ci/0309-comma_void_operand.c
+++ tests/ci/0309-comma_void_operand.c
@@ -5,7 +5,7 @@ void func(void)
 
 int main(void)
 {
-  // making sure the elaboration deal with the fact the
-  // first operand has type void
+  // making sure that the elaboration deals with
+  // the fact that the first operand can have type void
   return func(), 10;
 }
