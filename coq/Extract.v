5598325287346a4336b46de1861f34ccc9c6dca2
Vadim Zaliva <lord@crocodile.org>
2022-08-25 11:22:20 -0700

    build/OCaml extraction with dune

---
+++ coq/Extract.v
@@ -0,0 +1,12 @@
+Require Cerberus.Capabilities.
+
+Require ExtrOcamlBasic.
+Require ExtrOcamlString.
+Require ExtrOcamlIntConv.
+
+Extraction Language OCaml.
+Extraction Blacklist String List Char Core Monad Bool Z Format.
+Set Extraction AccessOpaque.
+
+Extraction Library ExtrOcamlIntConv.
+Extraction Library Capabilities.
