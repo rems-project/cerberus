4dfd75a005664a2e89ff05d08aab0cfbeecba5a7
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2022-09-17 01:39:38 +0100

    fixing typo in `stddef.h`

--- runtime/libc/include/stddef.h
+++ runtime/libc/include/stddef.h
@@ -5,7 +5,7 @@ typedef __cerbty_ptrdiff_t   ptrdiff_t;
 typedef __cerbty_size_t      size_t;
 typedef __cerbty_wchar_t     wchar_t;
 
-#define max_align_t          __cerbty_max_align_t;
+#define max_align_t          __cerbty_max_align_t
 #define NULL                 __cerbvar_NULL
 
 // offsetof() is now builtin in the parser
