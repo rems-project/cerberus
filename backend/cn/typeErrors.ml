230abdf0cbc4a6adcc3663bf8b0c6956eddd5e11
Christopher Pulte <cp526@cam.ac.uk>
2022-10-08 11:28:54 +0100

    - remove hard-coded early allocator predicates
    - add into constraint context constraints `f != NULL` and
      `good<void*>(f)` for function pointers f. We used to have those but
      I removed them when they didn't seem necessary. The early allocator
      relies on these.
    
    - fix html escape codes in terminal error messages

--- backend/cn/typeErrors.ml
+++ backend/cn/typeErrors.ml
@@ -542,8 +542,6 @@ let output_trace trace =
 
 (* stealing some logic from pp_errors *)
 let report ?state_file:to_ {loc; msg} =
-  let prev = ! Pp.html_escapes in
-  Pp.html_escapes := true;
   let report = pp_message msg in
   let consider = match report.state with
     | Some state ->
@@ -561,7 +559,6 @@ let report ?state_file:to_ {loc; msg} =
     | None ->
        None
   in
-  Pp.html_escapes := prev;
   Pp.error loc report.short
     ((Option.to_list report.descr) @
        (Option.to_list consider))
