638a76956342ad669191462c33734289d188e436
Vadim Zaliva <lord@crocodile.org>
2022-11-24 16:39:29 -0800

    more wip

--- coq/extracted/patch.sh
+++ coq/extracted/patch.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-if [ "$(basename "$1")" = "CRelationClasses.mli" ]; then
-    patch -p0 -r 0 -s --follow-symlinks -o - $1 -i coq/extracted/CRelationClasses.mli.patch
-else
-    cat $1
-fi
+patch -p0 -r 0 -s --read-only=ignore --follow-symlinks -o - $1 -i extracted/CRelationClasses.mli.patch
 
