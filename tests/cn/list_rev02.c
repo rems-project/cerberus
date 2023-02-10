0977e7d325eb9e4710cb0b8d1ceacc4678d60c91
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-02-10 22:12:05 +0000

    CN: update another test to new syntax

--- tests/cn/list_rev02.c
+++ tests/cn/list_rev02.c
@@ -10,16 +10,16 @@ predicate {integer len} List (pointer p) {
     return { len = 0 };
   }
   else {
-    let point = Owned<struct node>(p);
-    let r = List (point.value.next);
-    return { len = r.len + 1 };
+    let Point = Owned<struct node>(p);
+    let R = List (Point.value.next);
+    return { len = R.len + 1 };
   }
 }
 
 #define NULL ((void *)0)
 
-[[cn::requires("List(p)")]]
-[[cn::ensures("List(return)")]]
+[[cn::requires("let L = List(p)")]]
+[[cn::ensures("let L2 = List(return)")]]
 struct node *
 rev_list (struct node *p) {
   struct node *rev = NULL;
@@ -27,8 +27,8 @@ rev_list (struct node *p) {
 
   /* leaving loop var uninitialised */
   pack List(NULL);
-  [[cn::inv("List(p)")]]
-  [[cn::inv("List(rev)")]]
+  [[cn::inv("let L_p = List(p)")]]
+  [[cn::inv("let L_rev = List(rev)")]]
   while (p) {
     unpack List(p);
     p2 = p->next;
