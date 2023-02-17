433ec5133966353cd2cac4ac6b5ff49ddcecef26
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-02-17 15:52:47 +0000

    CN: coq lib: more use of generic list ops
    Similar setup to before, but with more use of List.firstn, List.nth
    etc. Actually requires more proofs, but probably more useful in the
    future.

--- tests/cn/tree16/as_auto_mutual_dt/coq_lemmas/theories/CN_Lib.v
+++ tests/cn/tree16/as_auto_mutual_dt/coq_lemmas/theories/CN_Lib.v
@@ -4,47 +4,54 @@ Open Scope Z.
 Require Import Lia.
 Require NArith.
 
-Fixpoint nth_list_enc {A L_A : Type}
-    (dest : L_A -> option (A * L_A)) (i : nat) (xs : L_A) (d : A) :=
-  match i with
-    | 0%nat => match dest xs with
-      | None => d
-      | Some (y, _) => y
+Fixpoint dec_list {A L_A : Type}
+    (dest : L_A -> option (A * L_A)) (xs : L_A) max_len :=
+  match max_len with
+    | 0%nat => List.nil
+    | S i => match dest xs with
+      | None => List.nil
+      | Some (y, ys) => List.cons y (dec_list dest ys i)
     end
-    | S j => match dest xs with
-      | None => d
-      | Some (_, ys) => nth_list_enc dest j ys d
-    end
+  end.
+
+Fixpoint enc_list {A L_A : Type}
+    (nil_c : L_A) (cons_c : A -> L_A -> L_A) xs :=
+  match xs with
+    | List.nil => nil_c
+    | List.cons y ys => cons_c y (enc_list nil_c cons_c ys)
   end.
 
 Definition nth_list_z {A L_A : Type}
-    (dest : L_A -> option (A * L_A)) (i : Z) (xs : L_A) (d : A) :=
+    dest (i : Z) (xs : L_A) (d : A) :=
   if (i <? 0)%Z then d
-  else nth_list_enc dest (Z.to_nat i) xs d.
-
-Lemma nth_list_enc_nil:
-  forall {A L_A} (nil_c : L_A) dest ix (d : A),
-  (dest nil_c = None) ->
-  nth_list_enc dest ix nil_c d = d.
-Proof.
-  intros.
-  destruct ix; simpl; rewrite H; auto.
-Qed.
-
-Fixpoint array_to_list_enc {A L_A : Type}
-    (nil_c : L_A) (cons_c : A -> L_A -> L_A)
-    (arr : Z -> A) (i : Z) (n : nat) : L_A :=
-  match n with
-  | 0%nat => nil_c
-  | S n2 => cons_c (arr i)
-    (array_to_list_enc nil_c cons_c arr (i + 1) n2)
-  end.
+  else List.nth (Z.to_nat i) (dec_list dest xs (S (Z.to_nat i))) d.
 
 Definition array_to_list {A L_A : Type}
     (nil_c : L_A) (cons_c : A -> L_A -> L_A)
     arr i (n : Z) :=
-    if (n <? 0)%Z then nil_c
-    else array_to_list_enc nil_c cons_c arr i (Z.to_nat n).
+  if (n <? 0)%Z then nil_c
+  else enc_list nil_c cons_c (List.map (fun j => arr (i + (Z.of_nat j))) (List.seq 0 (Z.to_nat n))).
+
+Lemma enc_list_dec_list:
+  forall {A L_A} nil_c (cons_c : A -> L_A -> L_A) dest,
+  (dest nil_c = None) ->
+  (forall y ys, dest (cons_c y ys) = Some (y, ys)) ->
+  forall xs max_len,
+  dec_list dest (enc_list nil_c cons_c xs) max_len = List.firstn max_len xs.
+Proof.
+  intros until xs.
+  induction xs; intros; destruct max_len.
+  - auto.
+  - simpl.
+    rewrite H.
+    reflexivity.
+  - auto.
+  - simpl.
+    rewrite H0.
+    simpl.
+    rewrite IHxs.
+    auto.
+Qed.
 
 Lemma succ_ltb_mono:
   forall i j,
@@ -60,34 +67,42 @@ Proof.
   - auto.
 Qed.
 
-Lemma nth_list_enc_array_to_list_enc:
-  forall {A L_A} (arr : Z -> A) nil_c (cons_c : A -> L_A -> L_A) dest,
-  (dest nil_c = None) ->
-  (forall y ys, dest (cons_c y ys) = Some (y, ys)) ->
-  forall (i : Z) n ix d,
-  nth_list_enc dest ix (array_to_list_enc nil_c cons_c arr i n) d =
-  if (ix <? n)%nat
-  then arr (i + Z.of_nat ix)
-  else d.
+Lemma nth_firstn:
+  forall {A : Type} i j (xs : list A) d,
+  List.nth i (List.firstn j xs) d =
+  if (i <? j)%nat then List.nth i xs d else d.
 Proof.
-  intros until n.
-  generalize i.
-  induction n.
+  intros until xs.
+  generalize i, j.
+  induction xs.
   - intros.
-    auto using nth_list_enc_nil.
+    destruct i0; destruct j0; simpl; try reflexivity.
+    rewrite Tauto.if_same.
+    reflexivity.
   - intros.
-    simpl.
-    destruct ix; simpl; rewrite H0.
-    + f_equal.
-      lia.
-    + rewrite IHn.
-      rewrite succ_ltb_mono.
-      destruct (ix <? n)%nat; try reflexivity.
-      f_equal.
-      lia.
+    destruct i0; destruct j0; simpl; try reflexivity.
+    rewrite IHxs.
+    rewrite succ_ltb_mono.
+    reflexivity.
 Qed.
 
-Lemma array_to_list_nth_z:
+Lemma nth_map_if_d2:
+  forall {A B : Type} (d2 : A) i (f : A -> B) xs d,
+  List.nth i (List.map f xs) d =
+  if (i <? List.length xs)%nat then f (List.nth i xs d2) else d.
+Proof.
+  intros until i.
+  induction i.
+  - intros.
+    destruct xs; auto.
+  - intros.
+    destruct xs; simpl; try reflexivity.
+    rewrite IHi.
+    rewrite succ_ltb_mono.
+    reflexivity.
+Qed.
+
+Lemma array_to_list_nth_list:
   forall {A L_A} (arr : Z -> A) nil_c (cons_c : A -> L_A -> L_A) dest,
   (dest nil_c = None) ->
   (forall y ys, dest (cons_c y ys) = Some (y, ys)) ->
@@ -104,18 +119,32 @@ Proof.
       destruct (n <? 0) eqn: n_lt0.
   - auto.
   - auto.
-  - assert ((ix <? n) = false) as lt by lia.
-    rewrite lt.
-    auto using nth_list_enc_nil.
-  - rewrite nth_list_enc_array_to_list_enc by auto.
-    destruct (ix <? n) eqn: ix_lt; simpl.
-    + assert ((Z.to_nat ix <? Z.to_nat n)%nat = true) as ix_lt2
+  - simpl.
+    rewrite H.
+    assert ((ix <? n) = false) by lia.
+    rewrite H1.
+    assert (forall j, List.nth j nil d = d)
+      by (intros; destruct j; auto).
+    rewrite H2.
+    auto.
+  - rewrite enc_list_dec_list by auto.
+    rewrite nth_firstn.
+    assert (forall j, (j <? S j)%nat = true)
+      by (intros; rewrite Nat.ltb_lt; lia).
+    rewrite H1.
+    rewrite (nth_map_if_d2 0%nat).
+    rewrite List.seq_length.
+    destruct (ix <? n) eqn: ix_lt_n.
+    + assert ((Z.to_nat ix <? Z.to_nat n)%nat = true)
         by (rewrite Nat.ltb_lt; lia).
-      rewrite ix_lt2.
+      rewrite H2.
+      simpl.
       f_equal.
+      rewrite List.seq_nth by lia.
       lia.
-    + assert ((Z.to_nat ix <? Z.to_nat n)%nat = false) as ix_lt2
+    + assert ((Z.to_nat ix <? Z.to_nat n)%nat = false)
         by (rewrite Nat.ltb_ge; lia).
-      rewrite ix_lt2.
+      rewrite H2.
       auto.
 Qed.
+
