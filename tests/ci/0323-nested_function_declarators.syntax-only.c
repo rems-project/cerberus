rems-project/cerberus

commit 4432fe7b9ecfd14168fae06d9c60ec133eb3ecf3

fixing the desugaring of function declarators (this addresses #44  #45 and #102)

frontend/model/cabs_to_ail.lem
@@ -1786,7 +1786,7 @@ and desugar_array_declarator under_array (ADecl loc tquals is_static a_decltor_s
   (* NOTE: if we are in prototype scope, then we are dealing with
      a function parameter *)
   E.get_scope >>= function
-     | E.Scope_prototype ->
+     | E.Scope_prototype _ ->
          if under_array = IsUnderArray && (tquals <> [] || is_static) then
 STD_ "§6.7.6.2#1, sentence 5, not outermost" $
            E.constraint_violation loc ArrayDeclarationQsAndStaticOnlyOutmost
@@ -1846,7 +1846,7 @@ STD_ "§6.7.6.2#1, sentence 5, not function parameter" $
     
     | Just ADeclSize_asterisk ->
         E.get_scope >>= function
-          | E.Scope_prototype ->
+          | E.Scope_prototype _ ->
               (* STD §6.7.6.2#4, sentence 2 *)
               E.fail loc (Errors.Desugar_NotYetSupported "variable length array type")
           | _ ->
@@ -1922,7 +1922,8 @@ and desugar_direct_abstract_declarator under_array = function
       (* TODO: documentation (the code the same as for desugar_direct_declarator, DDecl_function *)
       maybe (E.return (fun qs ty -> (qs, ty)))
         (desugar_direct_abstract_declarator under_array) dabs_decltor_opt >>= fun mk_qs_ctype ->
-      E.under_scope E.Scope_prototype
+      E.fresh_prototype_scope >>= fun proto_scope ->
+      E.under_scope proto_scope
         (desugar_parameter_type_list NotFunDef param_tys) >>= fun (params_opt, isVariadic) ->
       match params_opt with
         (* TODO: isn't this dead code ? *)
@@ -1983,14 +1984,22 @@ and desugar_direct_declarator fundef under_array = function
                    mk_outer_qs_ctype array_qs array_ty )
   
   | DDecl_function ddecltor param_tys ->
+      let fundef' =
+        match (fundef, ddecltor) with
+          | (IsFunDef, DDecl_identifier _ _) ->
+              IsFunDef
+          | _ ->
+              NotFunDef
+        end in
       (* TODO: check the NotFunDef *)
-      desugar_direct_declarator NotFunDef under_array ddecltor >>= fun (attrs_ident, mk_qs_ctype) ->
-      match fundef with
+      desugar_direct_declarator fundef under_array ddecltor >>= fun (attrs_ident, mk_qs_ctype) ->
+      match fundef' with
         | IsFunDef ->
-            desugar_parameter_type_list fundef param_tys
+            desugar_parameter_type_list fundef' param_tys
         | NotFunDef ->
-            E.under_scope E.Scope_prototype
-              (desugar_parameter_type_list fundef param_tys)
+            E.fresh_prototype_scope >>= fun proto_scope ->
+            E.under_scope proto_scope
+              (desugar_parameter_type_list fundef' param_tys)
       end >>= fun (_params_opt, isVariadic) ->
       let params_opt =
         (* NOTE2: but our current parser doesn't support identifier-list (this is an old syntax) *)

tests/tests.sh
@@ -187,6 +187,10 @@ citests=(
   0316-declaration_struct_pointer.error.c
   0317-compound-literal-lifetime.c
   0318-compound-interal-in_global.c
+  0319-valid_function_declarators.syntax-only.c
+  0320-invalid_function_declarators.error.c
+  0321-invalid_function_declarators.error.c
+  0322-invalid_function_declarators.error.c
 )
 
 # TESTS THAT ARE KNOW TO FAIL (for example .error test for which we need to improve the message)

frontend/model/cabs_to_ail_effect.lem
@@ -95,12 +95,12 @@ end
 
 
 (* STD §6.2.1#2 *)
-type block_id = nat
+type scope_id = nat
 type scope =
   | Scope_function
   | Scope_file
-  | Scope_block of block_id
-  | Scope_prototype
+  | Scope_block of scope_id
+  | Scope_prototype of scope_id
 
 let scopeEqual sc1 sc2 =
   match (sc1, sc2) with
@@ -110,8 +110,8 @@ let scopeEqual sc1 sc2 =
         true
     | (Scope_block id1, Scope_block id2) ->
         id1 = id2
-    | (Scope_prototype, Scope_prototype) ->
-        true
+    | (Scope_prototype id1, Scope_prototype id2) ->
+        id1 = id2
     | _ ->
         false
   end
@@ -130,8 +130,8 @@ let string_of_scope = function
       "file"
   | Scope_block n ->
       "block(" ^ show n ^ ")"
-  | Scope_prototype ->
-      "prototype"
+  | Scope_prototype n ->
+      "prototype(" ^ show n ^ ")"
 end
 
 let is_block_scope = function
@@ -221,6 +221,7 @@ type switch_info = <|
 type state = <|
   (* Counter used to generate fresh cabs label names (see the desugaring of do-while statements) *)
   aux_supply: nat;
+  scope_id_supply: nat;
   (* table (with stacked scopes) associating C identifiers to symbols *)
   
   registered_external_identifiers: map (Symbol.identifier * namespace) (scope * (A.ail_identifier * identifier_kind * maybe linkage));
@@ -541,7 +542,8 @@ let empty_switch_info =
 val initial_state: (map string Symbol.sym) * Core.fun_map unit * Core.impl -> state_with_markers
 let initial_state core_eval_stuff = <|
   inner= <|
-    aux_supply=    0;
+    aux_supply=      0;
+    scope_id_supply= 0;
     
     registered_external_identifiers= Map.empty;
     registered_identifiers=      Scope_table.empty;
@@ -607,10 +609,16 @@ let freshify (Symbol.Identifier loc str) =
 val fresh_block_scope: desugM scope
 let fresh_block_scope =
   modify_inner (fun st ->
-    let n = st.aux_supply in
-    (Scope_block n, <| st with aux_supply= n+1 |>)
+    let n = st.scope_id_supply in
+    (Scope_block n, <| st with scope_id_supply= n+1 |>)
   )
 
+val fresh_prototype_scope: desugM scope
+let fresh_prototype_scope =
+  modify_inner (fun st ->
+    let n = st.scope_id_supply in
+    (Scope_prototype n, <| st with scope_id_supply= n+1 |>)
+  )
 
 
 (* ========================================================================== *)

tests/ci/0319-valid_function_declarators.syntax-only.c
@@ -1,0 +1,27 @@
+// VALID: the two declarations of 'x' are in difference "function prototype scopes"
+struct S {
+  int (*f)(int x, int (*g)(int x));
+};
+
+// VALID: same a before
+int fun1(void)
+{
+  return sizeof(int (*)(int x, int (*g)(int x)));
+}
+// VALID: same a before, PLUS "arg" is in scope of function body statement
+int (*fun2(int arg, int x))(int x, int not_arg) {
+  arg + x;
+  return 0;
+}
+
+struct S {
+  void* (*f)(struct S* s);
+};
+void* (*fun3(struct S *p))(struct S*){
+  return p->f;
+}
+
+void (*fun4(int sig, void (*func)(int)))(int)
+{
+  return func;
+}

tests/ci/0320-invalid_function_declarators.error.c
@@ -1,0 +1,4 @@
+// the last two declarations of 'x' are in the same scope
+struct S {
+  int (*f)(int x, int (*g)(int x, int x));
+};

tests/ci/0321-invalid_function_declarators.error.c
@@ -1,0 +1,4 @@
+// the first and last declarations of 'x' are in the same scope
+struct S {
+  int (*f)(int x, int (*g)(int x), int x);
+};

tests/ci/0322-invalid_function_declarators.error.c
@@ -1,0 +1,3 @@
+int (*fun2(int arg, int x))(int x, int not_arg) {
+  not_arg; // this is NOT in scope
+}

tests/ci/expected/0237-array-star-illegal-scope.error.c.expected
@@ -1,0 +1,11 @@
+ci/0237-array-star-illegal-scope.error.c:1:6: error: star modifier used outside of function prototype
+char a[*];
+     ^~~~ 
+§6.7.6.2#4, 2nd sentence: 
+4   If the size is not present, the array type is an incomplete type. If the size is * instead of
+    being an expression, the array type is a variable length array type of unspecified size,
+    which can only be used in declarations or type names with function prototype scope;143)
+    such arrays are nonetheless complete types. If the size is an integer constant expression
+    and the element type has a known constant size, the array type is not a variable length
+    array type; otherwise, the array type is a variable length array type. (Variable length
+    arrays are a conditional feature that implementations need not support; see 6.10.8.3.)

commit d42c548375a06b9bbab21f79d8729b2a3caa67ed

fixing regression introduced by 4432fe7b9ecfd14168fae06d9c60ec133eb3ecf3

frontend/model/cabs_to_ail.lem
@@ -1445,10 +1445,19 @@ let arrayFromStringLiteral size_opt (pref_opt, strs) =
 
 
 
-
-
-
-
+let rec reduces_to_ddecl_identifier ddecltor =
+  match ddecltor with
+    | DDecl_identifier _ _ ->
+        true
+    | DDecl_declarator (Declarator Nothing ddecltor') ->
+        reduces_to_ddecl_identifier ddecltor'
+    | DDecl_declarator (Declarator (Just _) _) ->
+        false
+    | DDecl_array _ _ ->
+        false
+    | DDecl_function _ _ ->
+        false
+end
 
 
 
@@ -1985,9 +1994,12 @@ and desugar_direct_declarator fundef under_array = function
   
   | DDecl_function ddecltor param_tys ->
       let fundef' =
-        match (fundef, ddecltor) with
-          | (IsFunDef, DDecl_identifier _ _) ->
-              IsFunDef
+        match fundef with
+          | IsFunDef ->
+              if reduces_to_ddecl_identifier ddecltor then
+                IsFunDef
+              else
+                NotFunDef
           | _ ->
               NotFunDef
         end in

tests/ci/0323-nested_function_declarators.syntax-only.c
@@ -1,0 +1,9 @@
+int (fun1)(int arg)
+{
+  return arg;
+}
+
+int ((((fun2))))(int arg1, int arg2)
+{
+  return arg1 + arg2;
+}

tests/tests.sh
@@ -191,6 +191,7 @@ citests=(
   0320-invalid_function_declarators.error.c
   0321-invalid_function_declarators.error.c
   0322-invalid_function_declarators.error.c
+  0323-nested_function_declarators.syntax-only.c
 )
 
 # TESTS THAT ARE KNOW TO FAIL (for example .error test for which we need to improve the message)