51867348efd9dac0c7f764bedd45b0ae3490b68c
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-03-22 17:03:35 +0000

    CN: fix most of the remaining tests

--- tests/cn/pred_def03.error.c
+++ tests/cn/pred_def03.error.c
@@ -7,8 +7,8 @@ predicate {integer len} IntList(pointer l) {
   if ( l == NULL ) {
     return { len = 0 } ;
   } else {
-    let head_item = Owned<struct int_list_items>(l) ;
-    let tail = IntList(head_item.value.next) ;
+    take head_item = Owned<struct int_list_items>(l) ;
+    take tail = IntList(head_item.value.next) ;
 
     assert( tail.wrong ) ;
 
