ede97841500ef0538507d41451a37d25f1cdc8d0
Rini Banerjee <rb2018@cam.ac.uk>
2023-03-24 15:14:09 +0000

    Pattern matching based on cn_statement type working

--- backend/cn/main.ml
+++ backend/cn/main.ml
@@ -130,11 +130,17 @@ let _empty_executable_spec = {
 }
 
 
-let [@warning "-27"] generate_c_statements cn_statements = []
-  (* let generate_c_statement cn_statement = 
-    (Location_ocaml.Loc_other "hello", "Unknown")
+let [@warning "-27"] generate_c_statements cn_statements =
+  let open CF.Cn in
+  let generate_c_statement (CN_statement (loc, stmt_)) = 
+    let pp_statement =
+      match stmt_ with
+      | CN_assert_stmt e -> "hello"
+      | _ -> ""
+    in 
+  (loc, pp_statement)
   in
-  List.map generate_c_statement cn_statements *)
+  List.map generate_c_statement cn_statements
 
 
 (* Core_to_mucore.instrumentation list -> executable_spec *)
