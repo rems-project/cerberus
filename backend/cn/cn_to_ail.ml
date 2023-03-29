1fb4aec357323d5e9ab45df4273fef61f3cab769
Rini Banerjee <rbanerjee20@gmail.com>
2023-03-29 16:48:30 +0200

    Added basic cn to ail and ail to string functions

---
+++ backend/cn/cn_to_ail.ml
@@ -0,0 +1,31 @@
+[@@@warning "-27"]
+module CF=Cerb_frontend
+(* module CB=Cerb_backend
+open CB.Pipeline
+open Setup *)
+open CF.Cn
+module A=CF.AilSyntax
+
+let mk_expr expr_ = 
+  A.AnnotatedExpression ((), [], Cerb_location.unknown, expr_)
+
+
+let cn_to_ail_const = function
+| CNConst_integer n -> mk_expr (A.(AilEconst (ConstantInteger (IConstant (n, Decimal, None)))))
+| _ -> failwith "TODO"
+
+(* frontend/model/ail/ail_syntax.ml *)
+let cn_to_ail_expr (CNExpr (loc, expr_)) =
+  match expr_ with
+    | CNExpr_const cn_cst -> cn_to_ail_const cn_cst
+    | CNExpr_value_of_c_atom (sym, _) -> 
+      Printf.printf ">> \x1b[31m%s\x1b0m]\n" (CF.Pp_symbol.to_string_pretty sym);
+      mk_expr (A.(AilEident sym))
+    | _ -> failwith "TODO"
+
+
+
+
+let pp_ail = CF.String_ail.string_of_expression
+
+(* frontend/model/symbol.lem - fresh_pretty function for generating Sym with unimportant digest and nat *)
