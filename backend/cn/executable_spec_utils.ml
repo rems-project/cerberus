f3a67186b4e7a69c08041df579bf9ba755902db1
Rini Banerjee <rb2018@cam.ac.uk>
2023-04-25 17:13:52 +0100

    Set up basic pre and postcondition printing

--- backend/cn/executable_spec_utils.ml
+++ backend/cn/executable_spec_utils.ml
@@ -8,6 +8,8 @@ let mk_expr expr_ =
 
 let rm_expr (A.AnnotatedExpression (_, _, _, expr_)) = expr_
 
+let rm_stmt (A.AnnotatedStatement (_, _, stmt_)) = stmt_
+
 let mk_stmt stmt_ = 
   A.AnnotatedStatement(Cerb_location.unknown, CF.Annot.Attrs [], stmt_)
 
