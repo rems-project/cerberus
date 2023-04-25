51783eb5e9ec66aac6cd7dedfddb792c6160e8f6
Rini Banerjee <rb2018@cam.ac.uk>
2023-04-25 16:48:44 +0100

    Add Ail translation stage for CN assertions

--- backend/cn/executable_spec_utils.ml
+++ backend/cn/executable_spec_utils.ml
@@ -11,4 +11,7 @@ let rm_expr (A.AnnotatedExpression (_, _, _, expr_)) = expr_
 let mk_stmt stmt_ = 
   A.AnnotatedStatement(Cerb_location.unknown, CF.Annot.Attrs [], stmt_)
 
+let mk_ctype ctype_ =
+  C.Ctype ([], ctype_)
+
 let empty_qualifiers : C.qualifiers = {const = false; restrict = false; volatile = false}
