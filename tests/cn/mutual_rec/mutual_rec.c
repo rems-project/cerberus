560f7dbf8b7b2e2dadf1f7e1604d805a20e75703
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-04-25 15:38:44 +0100

    CN: support Let in lemmata, debugging, fix test
    This is needed to get the mutual_rec test working.
    
    The debugging amounts to tracing more stuff sometimes, which
    may occasionally be necessary.

--- tests/cn/mutual_rec/mutual_rec.c
+++ tests/cn/mutual_rec/mutual_rec.c
@@ -155,33 +155,31 @@ function (datatype key_list) merge (datatype key_list xs, datatype key_list ys)
 
 /* Lemmas that boil down to the definitions of the above. */
 
-void
-a_tree_keys_leaf_lemma (void)
-/*@ trusted @*/
-/*@ ensures (a_tree_keys (A_Leaf {})) == (K_Nil {}) @*/
-{
-  return;
-}
+lemma inc_list_lemma (datatype key_list xs)
+  requires true
+  ensures (inc_list (xs)) == (match xs {
+    K_Nil {} => {K_Nil {}}
+    K_Cons {k: k, tail: ys} => {K_Cons {k: k + 1, tail: inc_list (ys)}}
+  })
 
-void
-b_tree_keys_leaf_lemma (void)
-/*@ trusted @*/
-/*@ ensures (b_tree_keys (B_Leaf {})) == (K_Nil {}) @*/
-{
-  return;
-}
+lemma a_tree_keys_lemma (datatype a_tree atree)
+  requires true
+  ensures (a_tree_keys (atree)) == (match atree {
+    A_Leaf {} => {K_Nil {}}
+    A_Node {k: k, v: v, left: l, right: r} => {
+        concat (b_tree_keys(l), K_Cons {k: k, tail: b_tree_keys(r)})}
+  })
 
-void
-inc_list_nil_lemma (void)
-/*@ trusted @*/
-/*@ ensures (inc_list (K_Nil {})) == (K_Nil {}) @*/
-{
-  return;
-}
+lemma b_tree_keys_lemma (datatype b_tree btree)
+  requires true
+  ensures (b_tree_keys (btree)) == (match btree {
+    B_Leaf {} => {K_Nil {}}
+    B_Node {even: e, odd: o} => {
+        merge (double_list (a_tree_keys (e)), inc_list (double_list (a_tree_keys (o))))}
+  })
 
 void
 a_tree_keys_node_lemma (int k, int v, struct b_node *left, struct b_node *right)
-/*@ trusted @*/
 /*@ requires take L = B_Tree (left) @*/
 /*@ requires take R = B_Tree (right) @*/
 /*@ ensures take L2 = B_Tree (left) @*/
@@ -191,12 +189,12 @@ a_tree_keys_node_lemma (int k, int v, struct b_node *left, struct b_node *right)
 /*@ ensures (a_tree_keys (A_Node {k: k, v: v, left: L2.t, right: R2.t}))
   == (concat (b_tree_keys(L2.t), K_Cons {k: k, tail: (b_tree_keys(R2.t))})) @*/
 {
+  /*@ apply a_tree_keys_lemma (A_Node {k: k, v: v, left: L.t, right: R.t}); @*/
   return;
 }
 
 void
 b_tree_keys_node_lemma (struct a_node *even, struct a_node *odd)
-/*@ trusted @*/
 /*@ requires take E = A_Tree (even) @*/
 /*@ requires take O = A_Tree (odd) @*/
 /*@ ensures take E2 = A_Tree (even) @*/
@@ -206,13 +204,17 @@ b_tree_keys_node_lemma (struct a_node *even, struct a_node *odd)
 /*@ ensures (b_tree_keys (B_Node {even: E2.t, odd: O2.t}))
   == (merge (double_list (a_tree_keys (E2.t)), inc_list (double_list (a_tree_keys (O2.t))))) @*/
 {
+  /*@ apply b_tree_keys_lemma (B_Node {even: E.t, odd: O.t}); @*/
   return;
 }
 
 /* A lemma about increment of concat. */
+lemma inc_concat_lemma (datatype key_list xs, datatype key_list ys)
+  requires true
+  ensures inc_list (concat (xs, ys)) == concat (inc_list (xs), inc_list (ys))
+
 void
 a_tree_keys_node_concat_inc_lemma (int k, struct b_node *left, struct b_node *right)
-/*@ trusted @*/
 /*@ requires take L = B_Tree (left) @*/
 /*@ requires take R = B_Tree (right) @*/
 /*@ ensures take L2 = B_Tree (left) @*/
@@ -223,26 +225,30 @@ a_tree_keys_node_concat_inc_lemma (int k, struct b_node *left, struct b_node *ri
   == (concat (inc_list (b_tree_keys(L2.t)), inc_list (K_Cons {k: k, tail: (b_tree_keys(R2.t))})))
 @*/
 {
+  /*@ apply inc_concat_lemma (b_tree_keys(L.t), K_Cons {k: k, tail: (b_tree_keys(R.t))}); @*/
   return;
 }
 
-/* A lemma about increment of cons. */
 void
 a_tree_keys_node_concat_cons_inc_lemma (int k, struct b_node *right)
-/*@ trusted @*/
 /*@ requires take R = B_Tree (right) @*/
 /*@ ensures take R2 = B_Tree (right) @*/
 /*@ ensures R2.t == R.t @*/
 /*@ ensures (inc_list (K_Cons {k: k, tail: (b_tree_keys(R2.t))}))
   == (K_Cons {k: k + 1, tail: inc_list (b_tree_keys(R2.t))}) @*/
 {
+  /*@ apply inc_list_lemma (K_Cons {k: k, tail: b_tree_keys(R.t)}); @*/
   return;
 }
 
-/* A lemma about increment of merge. */
+/* A lemma about increment of cons. */
+lemma inc_merge_double_lemma (datatype key_list xs, datatype key_list ys)
+  requires true
+  ensures inc_list (merge (double_list (xs), inc_list (double_list (ys))))
+    == merge (inc_list (double_list (xs)), inc_list (inc_list (double_list (ys))))
+
 void
 b_tree_keys_node_merge_inc_lemma (struct a_node *even, struct a_node *odd)
-/*@ trusted @*/
 /*@ requires take E = A_Tree (even) @*/
 /*@ requires take O = A_Tree (odd) @*/
 /*@ ensures take E2 = A_Tree (even) @*/
@@ -255,14 +261,19 @@ b_tree_keys_node_merge_inc_lemma (struct a_node *even, struct a_node *odd)
         inc_list (inc_list (double_list (a_tree_keys (O2.t)))))) @*/
 
 {
+  /*@ apply inc_merge_double_lemma (a_tree_keys (E.t), a_tree_keys (O.t)); @*/
   return;
 }
 
 /* A lemma about flipping a merge. The merge has to sort, to some degree, to
    allow us to prove list equality rather than weaker multiset equality. */
+lemma flip_merge_lemma (datatype key_list xs, datatype key_list ys)
+  requires true
+  ensures merge (inc_list (double_list (xs)), inc_list (inc_list (double_list (ys))))
+    == merge (inc_list (inc_list (double_list (ys))), inc_list (double_list (xs)))
+
 void
 b_tree_keys_node_merge_flip_lemma (struct a_node *even, struct a_node *odd)
-/*@ trusted @*/
 /*@ requires take E = A_Tree (even) @*/
 /*@ requires take O = A_Tree (odd) @*/
 /*@ ensures take E2 = A_Tree (even) @*/
@@ -274,23 +285,26 @@ b_tree_keys_node_merge_flip_lemma (struct a_node *even, struct a_node *odd)
   == (merge (inc_list (inc_list (double_list (a_tree_keys (O2.t)))),
         inc_list (double_list (a_tree_keys (E2.t))))) @*/
 {
+  /*@ apply flip_merge_lemma (a_tree_keys (E.t), a_tree_keys (O.t)); @*/
   return;
 }
 
 /* A lemma about composing increments and multiplies. */
+lemma inc_double_lemma (datatype key_list xs)
+  requires true
+  ensures double_list (inc_list (xs)) == inc_list (inc_list (double_list (xs)))
+
 void
 b_tree_keys_node_inc_inc_double_lemma (struct a_node *odd)
-/*@ trusted @*/
 /*@ requires take O = A_Tree (odd) @*/
 /*@ ensures take O2 = A_Tree (odd) @*/
 /*@ ensures O2.t == O.t @*/
 /*@ ensures (inc_list (inc_list (double_list (a_tree_keys (O2.t)))))
   == (double_list (inc_list (a_tree_keys (O2.t)))) @*/
 {
+  /*@ apply inc_double_lemma (a_tree_keys (O.t)); @*/
   return;
 }
-
-
 
 #endif
 
@@ -330,16 +344,15 @@ inc_a_tree (struct a_node *p)
 /*@ ensures (return == 0) || ((a_tree_keys(T2.t)) == (inc_list(a_tree_keys(T.t)))) @*/
 {
   int r = 0;
+  /*@ apply a_tree_keys_lemma(T.t); @*/
   if (! p) {
     /*@ unpack A_Tree(p); @*/
-    a_tree_keys_leaf_lemma();
-    inc_list_nil_lemma();
+    /*@ apply inc_list_lemma(K_Nil {}); @*/
     return 1;
   }
   if (p->k >= INT_MAX) {
     return 0;
   }
-  a_tree_keys_node_lemma(p->k, p->v, p->left, p->right);
   a_tree_keys_node_concat_inc_lemma(p->k, p->left, p->right);
   a_tree_keys_node_concat_cons_inc_lemma(p->k, p->right);
   p->k ++;
@@ -356,10 +369,10 @@ inc_b_tree (struct b_node *p)
 {
   struct a_node *tmp = NULL;
   int r = 0;
+  /*@ apply b_tree_keys_lemma(T.t); @*/
   if (! p) {
     /*@ unpack B_Tree(p); @*/
-    b_tree_keys_leaf_lemma();
-    inc_list_nil_lemma();
+    /*@ apply inc_list_lemma(K_Nil {}); @*/
     return 1;
   }
   b_tree_keys_node_lemma(p->even, p->odd);
