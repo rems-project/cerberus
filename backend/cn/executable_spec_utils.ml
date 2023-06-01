97433c7011c8ab64fb4e15ea9368054902984605
Rini Banerjee <rb2018@cam.ac.uk>
2023-06-01 18:59:38 +0100

    Pattern matching translation progress using Ail statement attributes to carry info for pprinting

--- backend/cn/executable_spec_utils.ml
+++ backend/cn/executable_spec_utils.ml
@@ -7,12 +7,14 @@ module Cn=CF.Cn
 let mk_expr expr_ = 
   A.AnnotatedExpression ((), [], Cerb_location.unknown, expr_)
 
+
+let mk_stmt stmt_ = 
+  A.AnnotatedStatement (Cerb_location.unknown, CF.Annot.Attrs [], stmt_)
+
 let rm_expr (A.AnnotatedExpression (_, _, _, expr_)) = expr_
 
 let rm_stmt (A.AnnotatedStatement (_, _, stmt_)) = stmt_
 
-let mk_stmt stmt_ = 
-  A.AnnotatedStatement(Cerb_location.unknown, CF.Annot.Attrs [], stmt_)
 
 let mk_ctype ctype_ =
   C.Ctype ([], ctype_)
