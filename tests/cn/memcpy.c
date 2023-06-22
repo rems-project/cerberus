a3e785d765b1d9f63ac3c12cc0b09a7489c80207
Mete Polat <mete.polat@cl.cam.ac.uk>
2023-06-22 15:47:52 +0100

    CN: cleanup memcpy test spec

--- tests/cn/memcpy.c
+++ tests/cn/memcpy.c
@@ -1,32 +1,25 @@
-
-
-
 void
-naive_memcpy (char *in_arr, char *out_arr, int i)
-/*@ requires take IA = each (integer j; 0 <= j && j < i) {Owned(in_arr + (j * sizeof<char>))} @*/
-/*@ requires take OA = each (integer j; 0 <= j && j < i) {Owned(out_arr + (j * sizeof<char>))} @*/
-/*@ requires 0 <= i @*/
-/*@ ensures take IA2 = each (integer j; 0 <= j && j < i) {Owned(in_arr + (j * sizeof<char>))} @*/
-/*@ ensures take OA2 = each (integer j; 0 <= j && j < i) {Owned(out_arr + (j * sizeof<char>))} @*/
-/*@ ensures IA2 == IA @*/
-/*@ ensures each (integer k; 0 <= k && k < i) {OA2[k] == IA[k]} @*/
+memcpy (char *dst, char *src, int n)
+/*@ requires take dstStart = each (integer j; 0 <= j && j < n) {Owned(dst + (j * sizeof<char>))} @*/
+/*@ requires take srcStart = each (integer j; 0 <= j && j < n) {Owned(src + (j * sizeof<char>))} @*/
+/*@ ensures take dstEnd = each (integer j; 0 <= j && j < n) {Owned(dst + (j * sizeof<char>))} @*/
+/*@ ensures take srcEnd = each (integer j; 0 <= j && j < n) {Owned(src + (j * sizeof<char>))} @*/
+/*@ ensures srcEnd == srcStart @*/
+/*@ ensures each (integer k; 0 <= k && k < n) {dstEnd[k] == srcStart[k]} @*/
 {
-  int j;
-
-  for (j = 0; j < i; j++)
-  /*@ inv take IA3 = each (integer k; 0 <= k && k < i) {Owned(in_arr + (k * sizeof<char>))} @*/
-  /*@ inv take OA3 = each (integer k; 0 <= k && k < i) {Owned(out_arr + (k * sizeof<char>))} @*/
-  /*@ inv IA3 == IA @*/
-  /*@ inv each (integer k; 0 <= k && k < j) {OA3[k] == IA[k]} @*/
-  /*@ inv 0 <= j && j <= i @*/
-  /*@ inv i == {i}@start @*/
-  /*@ inv {in_arr} unchanged @*/
-  /*@ inv {out_arr} unchanged @*/
+  int i;
+  for (i = 0; i < n; i++)
+  /*@ inv take dstInv = each (integer j; 0 <= j && j < n) {Owned(dst + (j * sizeof<char>))} @*/
+  /*@ inv take srcInv = each (integer j; 0 <= j && j < n) {Owned(src + (j * sizeof<char>))} @*/
+  /*@ inv srcInv == srcStart @*/
+  /*@ inv each (integer j; 0 <= j && j < i) {dstInv[j] == srcStart[j]} @*/
+  /*@ inv 0 <= i @*/
+  /*@ inv {dst} unchanged @*/
+  /*@ inv {src} unchanged @*/
+  /*@ inv {n} unchanged @*/
   {
-    /*@ extract Owned<char>, j; @*/
-    /*@ instantiate good<char>, j; @*/
-    out_arr[j] = in_arr[j];
+    /*@ extract Owned<char>, i; @*/
+    /*@ instantiate good<char>, i; @*/
+    dst[i] = src[i];
   }
-
 }
-
