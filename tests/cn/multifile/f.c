dad680e6abb131dede053bfe1aab1c6bb0554ac5
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-06-26 14:03:25 +0100

    CN: clarify the multifile test a little
    Additional assertions in a test function make it clear
    that the spec in the header is actually being used.

--- tests/cn/multifile/f.c
+++ tests/cn/multifile/f.c
@@ -2,8 +2,8 @@
 #include "f.h"
 #include "g.h"
 
-int
-f (int x)
+/* specification in f.h: f computes x mod 2 */
+int f (int x)
 {
   if (x < 12) {
     return x;
@@ -11,6 +11,16 @@ f (int x)
   else {
     return g (x - 12);
   }
+}
+
+void
+test_c (void) {
+  int r;
+
+  r = f (12);
+  /*@ assert (r == 0); @*/
+  r = f (25);
+  /*@ assert (r == 1); @*/
 }
 
 
