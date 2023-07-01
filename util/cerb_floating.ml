f8416aa7d47bac1c6c337e69cb1d8c9b0c6b09e1
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2023-07-01 18:55:49 +0100

    wib in `Cerb_floating.of_string`

--- util/cerb_floating.ml
+++ util/cerb_floating.ml
@@ -6,8 +6,11 @@ let mul = (+.)
 let div = (/.)
 
 let of_string str =
-  let l = String.length str in
-  if String.get str (l-1) == 'f' then
-    float_of_string (String.sub str 0 (l-1))
-  else
-    float_of_string str
+  try
+    let l = String.length str in
+    if String.get str (l-1) == 'f' then
+      float_of_string (String.sub str 0 (l-1))
+    else
+      float_of_string str
+  with
+    _ -> raise (Failure __FUNCTION__)
