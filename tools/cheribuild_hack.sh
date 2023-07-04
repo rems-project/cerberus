f4ba74d1377cd8101c58f0880f544fc5a2568cf1
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2023-07-04 19:49:51 +0100

    improving cheribuild_hack script for compat with macos

--- tools/cheribuild_hack.sh
+++ tools/cheribuild_hack.sh
@@ -3,18 +3,34 @@
 trap 'repair_dune' SIGINT
 
 # GNU vs BSD
-if sed --version 2>/dev/null 1>/dev/null; then
-  MYSEDI="sed -i"
-else
-  MYSEDI="sed -i ''"
-fi
+# function sedi(){
+# if sed --version 2>/dev/null 1>/dev/null; then
+#   echo "sed -i -e '$1' $2"
+# else
+#   echo "sed -i '' -e '$1' $2"
+# fi
+# }
 
 function repair_dune() {
-  $MYSEDI '/(dirs/s/ coq//' dune
+  case "$(uname -s)" in
+  Darwin)
+    sed -i '' -e '/(dirs/s/ coq//' dune
+    ;;
+  *)
+    sed -i -e '/(dirs/s/ coq//' dune
+    ;;
+  esac
 }
 
 function add_coq() {
-  $MYSEDI 's/(dirs \([^)]*\))/(dirs \1 coq)/' dune
+    case "$(uname -s)" in
+  Darwin)
+    sed -i '' -e 's/(dirs \([^)]*\))/(dirs \1 coq)/' dune
+    ;;
+  *)
+    sed -i -e 's/(dirs \([^)]*\))/(dirs \1 coq)/' dune
+    ;;
+  esac
 }
 
 add_coq
