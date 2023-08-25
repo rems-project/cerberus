7b8441b9ea4bef4afa8f627c0718ad5fd7bea556
Rini Banerjee <rb2018@cam.ac.uk>
2023-08-25 18:35:51 +0100

    Update to account for changes from master (incl. patterns carrying basetypes)

--- backend/cn/cn_internal_to_ail.ml
+++ backend/cn/cn_internal_to_ail.ml
@@ -474,7 +474,7 @@ let rec cn_to_ail_expr_aux_internal
     dest d ([], [], ail_expr_)
 
   | ArrayShift _ -> failwith "TODO7"
-  | Nil _ -> failwith "TODO8"
+  | Nil bt -> failwith "TODO8"
   | Cons (x, xs) -> failwith "TODO9"
   | Head xs -> 
     let b, s, e_ = cn_to_ail_expr_aux_internal const_prop pred_name dts xs PassBack in
@@ -519,25 +519,29 @@ let rec cn_to_ail_expr_aux_internal
 
   | Match (t, ps) -> 
       Printf.printf "Reached pattern matching case\n";
+
       (* PATTERN COMPILER *)
+
+      let mk_pattern pattern_ bt = T.(Pat (pattern_, bt)) in
+
       let simplify_leading_variable t1 (ps, t2) =
         match ps with 
-        | T.PSym sym' :: ps' -> (T.PWild :: ps', T.(IT (Let ((sym', t1), t2), IT.basetype t2)))
+        | T.(Pat (PSym sym', p_bt)) :: ps' -> ((mk_pattern T.PWild p_bt):: ps', T.(IT (Let ((sym', t1), t2), IT.basetype t2)))
         | p :: ps' -> (p :: ps', t2)
         | [] -> assert false
       in
 
       let leading_wildcard (ps, _) =
         match ps with
-          | T.PWild :: ps' -> true
+          | T.(Pat (PWild, _)) :: ps' -> true
           | _ :: ps' -> false
           | [] -> failwith "Empty patterns not allowed"
       in
 
       let expand_datatype c (ps, e) = 
         match ps with 
-        | T.PWild :: ps' -> Some (T.PWild :: ps', e)
-        | T.PConstructor (c_nm, members) :: ps' ->
+        | T.(Pat (PWild, p_bt) as pat) :: ps' -> Some (pat :: ps', e)
+        | T.(Pat (PConstructor (c_nm, members), _)) :: ps' ->
           if Sym.equal_sym c c_nm then
             let member_patterns = List.map snd members in
             Some (member_patterns @ ps', e)
@@ -553,7 +557,7 @@ let rec cn_to_ail_expr_aux_internal
 
       (* TODO: Incorporate destination passing recursively into this. Might need PassBack throughout, like in cn_to_ail_expr_aux function *)
       (* Matrix algorithm for pattern compilation *)
-      let rec translate : int -> (BT.basetype Terms.term) list -> (Terms.pattern list * BT.basetype Terms.term) list -> (A.bindings * (_ A.statement_) list) =
+      let rec translate : int -> (BT.basetype Terms.term) list -> (BT.basetype Terms.pattern list * BT.basetype Terms.term) list -> (A.bindings * (_ A.statement_) list) =
         fun count vars cases -> 
           match vars with 
             | [] ->
@@ -624,7 +628,7 @@ let rec cn_to_ail_expr_aux_internal
                     failwith "Unexpected pattern"
       in
 
-      let translate_real : type a. (BT.basetype Terms.term) list -> (Terms.pattern list * BT.basetype Terms.term) list -> a dest -> a =
+      let translate_real : type a. (BT.basetype Terms.term) list -> (BT.basetype Terms.pattern list * BT.basetype Terms.term) list -> a dest -> a =
         fun vars cases d ->
           let (bs, ss) = translate 1 vars cases in
           match d with 
@@ -1061,6 +1065,8 @@ let cn_to_ail_cnstatement_internal : type a. (_ Cn.cn_datatype) list -> a dest -
 
   | Cnprog.M_CN_assert lc -> 
     cn_to_ail_logical_constraint_internal dts d lc
+
+  | Cnprog.M_CN_inline _ -> failwith "TODO M_CN_inline"
 
 
 let rec cn_to_ail_cnprog_internal dts = function
