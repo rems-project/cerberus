b661519f8636ba0cad1823d1f87ec1b733767c53
Mete Polat <mete.polat@cl.cam.ac.uk>
2023-09-12 15:56:01 +0200

    CN: Add swap example to tests

---
+++ tests/cn/swap_pair.c
@@ -0,0 +1,14 @@
+void swap_pair(unsigned long int *pair_p)
+/*@ requires take pairStart = each (integer j; 0 <= j && j < 2) {Owned(pair_p + (j * sizeof<unsigned long int>))} @*/
+/*@ ensures take pairEnd = each (integer j; 0 <= j && j < 2) {Owned(pair_p + (j * sizeof<unsigned long int>))} @*/
+/*@ ensures pairEnd[0] == pairStart[1] @*/
+/*@ ensures pairEnd[1] == pairStart[0] @*/
+{
+    /*@ extract Owned<unsigned long int>, 0; @*/
+    unsigned long int tmp = pair_p[0];
+    /*@ extract Owned<unsigned long int>, 1; @*/
+    /*@ instantiate good<unsigned long int>, 0; @*/
+    pair_p[0] = pair_p[1];
+    /*@ instantiate good<unsigned long int>, 1; @*/
+    pair_p[1] = tmp;
+}
