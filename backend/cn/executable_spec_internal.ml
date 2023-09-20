af5d3e2599b7997b4af11b85337384c5eda540f8
Rini Banerjee <rb2018@cam.ac.uk>
2023-09-20 11:20:35 +0100

    Implement simple assertions in preconditions with filtering of assertions containing only a variable

--- backend/cn/executable_spec_internal.ml
+++ backend/cn/executable_spec_internal.ml
@@ -44,37 +44,6 @@ let generate_c_statements_internal (loc, statements) dts =
 
 
 let generate_c_pres_and_posts_internal (instrumentation : Core_to_mucore.instrumentation) type_map (ail_prog: _ CF.AilSyntax.sigma) (prog5: unit Mucore.mu_file) =
-  (* let sym_equality = fun (loc, _) -> CF.Symbol.equal_sym loc instrumentation.fn in *)
-  (* let fn_decl = List.filter sym_equality ail_prog.A.declarations in *)
-  (* let fn_def = List.filter sym_equality ail_prog.A.function_definitions in *)
-  (* let (arg_types, arg_syms) = 
-  match (fn_decl, fn_def) with 
-    | ((_, (_, _, A.(Decl_function (_, _, arg_types, _, _, _)))) :: _), ((_, (_, _, _, arg_syms, _)) :: _) -> 
-      let arg_types = List.map (fun (_, ctype, _) -> ctype) arg_types in
-      (arg_types, arg_syms)
-    | _ -> ([], [])
-  in
-  let gen_old_var_fn = (fun sym -> (CF.Pp_symbol.to_string_pretty sym) ^ "_old") in
-  let empty_qualifiers : CF.Ctype.qualifiers = {const = false; restrict = false; volatile = false} in
-  let pp_ctype ctype = CF.Pp_utils.to_plain_pretty_string (CF.Pp_ail.pp_ctype empty_qualifiers ctype) in
-  let arg_str_fn (ctype, sym) =
-    pp_ctype ctype ^
-    " " ^
-    gen_old_var_fn sym ^
-    " = " ^
-    CF.Pp_symbol.to_string_pretty sym ^
-    ";\n"
-  in *)
-  (* let arg_names = List.map CF.Pp_symbol.to_string_pretty arg_syms in
-  let arg_strs = List.map arg_str_fn (List.combine arg_types arg_syms) in
-  let generate_condition_str cn_condition arg_names_opt =
-    (let (ail_stats, type_info) = Cn_to_ail.cn_to_ail_condition cn_condition type_map ail_prog.cn_datatypes in
-    let strs = List.map (fun s -> Ail_to_c.pp_ail_stmt (s, type_info) arg_names_opt) ail_stats in
-    (List.fold_left (^) "" strs) ^ ";\n")
-  in *)
-
-
-
   let dts = ail_prog.cn_datatypes in
   let preds = prog5.mu_resource_predicates in
   let pre_stats_ = Cn_internal_to_ail.cn_to_ail_arguments_internal dts preds instrumentation.internal.pre in
@@ -92,22 +61,21 @@ let generate_c_specs_internal instrumentation_list type_map (statement_locs : Ce
 (prog5: unit Mucore.mu_file)
 =
   let generate_c_spec (instrumentation : Core_to_mucore.instrumentation) =
-    let c_pres_and_posts = generate_c_pres_and_posts_internal instrumentation type_map ail_prog prog5 in 
     let rec remove_duplicates locs stats = 
-      match stats with 
+      (match stats with 
         | [] -> []
         | (loc, s) :: ss ->
           let loc_equality x y = 
             String.equal (Cerb_location.location_to_string x) (Cerb_location.location_to_string y) in 
           if (List.mem loc_equality loc locs) then 
-            (Printf.printf "Found duplicate at loc %s!\n" (Cerb_location.simple_location loc);
-            remove_duplicates locs ss)
+            remove_duplicates locs ss
           else 
-            (loc, s) :: (remove_duplicates (loc :: locs) ss)
+            (loc, s) :: (remove_duplicates (loc :: locs) ss))
     in
-    let internal_statements = List.filter (fun (_, ss) ->  List.length ss != 0) instrumentation.internal.statements in
-    let internal_statements = remove_duplicates [] internal_statements in
+    (* let internal_statements = List.filter (fun (_, ss) ->  List.length ss != 0) instrumentation.internal.statements in *)
+    let internal_statements = remove_duplicates [] instrumentation.internal.statements in
     let c_statements = List.map (fun s -> generate_c_statements_internal s ail_prog.cn_datatypes) internal_statements in
+    let c_pres_and_posts = generate_c_pres_and_posts_internal instrumentation type_map ail_prog prog5 in 
     (c_pres_and_posts, c_statements)
   in
   let specs = List.map generate_c_spec instrumentation_list in 
