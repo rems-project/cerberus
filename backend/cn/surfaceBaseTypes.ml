83d505d695c9236b0b6a05d533ac56e03159961d
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-09-26 10:26:09 +0100

    CN: likewise for surface-basetypes

--- backend/cn/surfaceBaseTypes.ml
+++ backend/cn/surfaceBaseTypes.ml
@@ -104,7 +104,7 @@ let make_map_bt abt rbt = Map (abt, rbt)
 
 let rec of_sct is_signed size_of = function
   | Sctypes.Void -> Unit
-  | Integer ity -> Bits ((if is_signed ity then Signed else Unsigned), size_of ity)
+  | Integer ity -> Bits ((if is_signed ity then Signed else Unsigned), size_of ity * 8)
   | Array (sct, _) -> Map (Integer, of_sct is_signed size_of sct)
   | Pointer ct -> Loc (Some ct)
   | Struct tag -> Struct tag
