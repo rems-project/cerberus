34657c784b1dbfc3d57a9afe0915a8c80ba6fa78
Rini Banerjee <rb2018@cam.ac.uk>
2023-09-27 20:33:30 +0100

    Add CN pointer wrapper

--- executable-spec/cn_utils.c
+++ executable-spec/cn_utils.c
@@ -4,10 +4,25 @@
 #include <stdio.h>
 
 
-
 /* Wrappers for C types */
 
-typedef unsigned int cn_integer;
+// typedef unsigned int cn_integer_2;
+
+// cn_integer_2 *convert_to_cn_integer_2(unsigned int i) {
+//     cn_integer_2 *cn_i = alloc(sizeof(cn_integer_2));
+//     *cn_i = i;
+//     return cn_i;
+// }
+
+
+typedef struct cn_integer {
+    unsigned int val;
+} cn_integer;
+
+typedef struct cn_pointer {
+    void *ptr;
+} cn_pointer;
+
 typedef _Bool cn_bool;
 
 _Bool cn_char_equality(void *a, void *b) {
@@ -23,31 +38,46 @@ cn_map *map_create(void) {
 
 void *cn_map_get(cn_map *m, cn_integer *key) {
     // const char key_arr[1] = {key};
-    void *res = ht_get(m, key);
-    if (!res) printf("NULL being returned for key %d\n", *key);
+    void *res = ht_get(m, &key->val);
+    if (!res) printf("NULL being returned for key %d\n", (key->val));
     return res;
 }
 
 void cn_map_set(cn_map *m, cn_integer *key, void *value) {
-    ht_set(m, key, value);
+    ht_set(m, &key->val, value);
 }
+
+// void *cn_map_get_2(cn_map *m, cn_integer_2 *key) {
+//     // const char key_arr[1] = {key};
+//     void *res = ht_get(m, key);
+//     if (!res) printf("NULL being returned for key %d\n", *(key));
+//     return res;
+// }
+
+// void cn_map_set_2(cn_map *m, cn_integer_2 *key, void *value) {
+//     ht_set(m, key, value);
+// }
 
 /* Every equality function needs to take two void pointers for this to work */
 _Bool cn_integer_equality(void *i1, void *i2) {
-    return *((cn_integer *) i1) == *((cn_integer *) i2);
+    return (((cn_integer *) i1)->val) == (((cn_integer *) i2)->val);
 }
 
-_Bool cn_map_equality(cn_map *m1, cn_map *m2, _Bool (value_equality_fun)(void *, void *)) {
+// _Bool cn_integer_2_equality(void *i1, void *i2) {
+//     return *((cn_integer_2 *) i1) == *((cn_integer_2 *) i2);
+// }
+
+
+// Check if m2 is a subset of m1
+_Bool cn_map_subset(cn_map *m1, cn_map *m2, _Bool (value_equality_fun)(void *, void *)) {
     if (ht_size(m1) != ht_size(m2)) return 0;
     
     hash_table_iterator hti1 = ht_iterator(m1);
-    hash_table_iterator hti2 = ht_iterator(m2);
-
 
     while (ht_next(&hti1)) {
         unsigned int* curr_key = hti1.key;
-        void *val1 = cn_map_get(m1, curr_key);
-        void *val2 = cn_map_get(m2, curr_key);
+        void *val1 = ht_get(m1, curr_key);
+        void *val2 = ht_get(m2, curr_key);
 
         /* Check if other map has this key at all */
         if (!val2) return 0;
@@ -59,6 +89,10 @@ _Bool cn_map_equality(cn_map *m1, cn_map *m2, _Bool (value_equality_fun)(void *,
     }
 
     return 1;
+}
+
+_Bool cn_map_equality(cn_map *m1, cn_map *m2, _Bool (value_equality_fun)(void *, void *)) {
+    return cn_map_subset(m1, m2, value_equality_fun) && cn_map_subset(m2, m1, value_equality_fun);
 }
 
 
@@ -110,8 +144,47 @@ long max(long a, long b) {
 
 /* Conversion functions */
 
-cn_integer *convert_to_cn_integer(int i) {
+cn_integer *convert_to_cn_integer(unsigned int i) {
     cn_integer *ret = alloc(sizeof(cn_integer));
-    *ret = (cn_integer) i;
+    // unsigned int *i_ptr = alloc(sizeof(unsigned int));
+    // *i_ptr = i;
+    // ret->val = i_ptr;
+    ret->val = i;
     return ret;
+}
+
+cn_pointer *convert_to_cn_pointer(void *ptr) {
+    cn_pointer *res = alloc(sizeof(cn_pointer));
+    res->ptr = ptr; // Carries around an address
+    return res;
+}
+
+/* These should be produced automatically based on binops used in source CN annotations */
+_Bool cn_integer_lt(cn_integer *i1, cn_integer *i2) {
+    return (i1->val) < (i2->val);
+}
+
+cn_integer *cn_integer_add(cn_integer *i1, cn_integer *i2) {
+    cn_integer *res = alloc(sizeof(cn_integer));
+    // res->val = alloc(sizeof(unsigned int));
+    (res->val) = (i1->val) + (i2->val);
+    return res;
+}
+
+cn_integer *cn_integer_increment(cn_integer *i) {
+    (i->val)++;
+    return i;
+}
+
+cn_integer *cn_integer_multiply(cn_integer *i1, cn_integer *i2) {
+    cn_integer *res = alloc(sizeof(cn_integer));
+    // res->val = alloc(sizeof(unsigned int));
+    res->val = i1->val * i2->val;
+    return res;
+}
+
+cn_pointer *cn_pointer_add(cn_pointer *ptr, cn_integer *i) {
+    cn_pointer *res = alloc(sizeof(cn_pointer));
+    res->ptr = (char *) ptr->ptr + i->val;
+    return res;
 }
