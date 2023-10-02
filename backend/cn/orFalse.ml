8a469274ba1e0ef642292b382930afa671de8690
Christopher Pulte <cp526@cam.ac.uk>
2023-10-03 00:27:40 +0100

    remove now-unused module

--- backend/cn/orFalse.ml
+++
@@ -1,7 +0,0 @@
-(* `orFalse` is used for the type of Run/Goto: Goto has no return type
-   (because the control flow does not return there), but instead
-   returns `False`. Type inference of impure expressions returns
-   either a return type and a typing context or `False` *)
-type 'a orFalse = 
-  | Normal of 'a
-  | False
