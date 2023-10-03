daad359c4d266d0b59037d39df8a0c8cfcfea2ce
Vadim Zaliva <lord@crocodile.org>
2023-10-02 17:42:37 -0700

    more stateless proofs, mem_state equality, model compatiblity

--- coq/Common/SimpleError.v
+++ coq/Common/SimpleError.v
@@ -7,6 +7,8 @@ Require Import ExtLib.Structures.Monad.
 Require Import ExtLib.Structures.MonadExc.
 Require Import ExtLib.Data.Monads.EitherMonad.
 
+Require Import Coq.Relations.Relation_Definitions.
+
 Notation serr := (sum string).
 
 Local Instance Exception_serr : MonadExc string (serr) :=
@@ -25,3 +27,14 @@ Definition option2serr {A: Type} (msg:string) (o:option A): (serr A)
 
 Definition sassert (b:bool) (msg:string) : serr unit
   := if b then ret tt else raise msg.
+
+(* 'serr' type comparision using given predicate.
+     This version is strict and requires error messages
+     to be the same *)
+Definition serr_eq {A:Type}: relation(A) -> relation (serr A)
+  := fun pred a b =>
+       match a,b with
+       | inr av, inr bv => pred av bv
+       | inl ae, inl be => ae = be
+       | _, _ => False
+       end.
