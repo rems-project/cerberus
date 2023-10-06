1036a88a3f2b61fa0d9ee117f2e8527ddd85573f
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-10-06 16:20:33 +0100

    CN: add a test from a while back

---
+++ tests/cn/swap.c
@@ -0,0 +1,18 @@
+
+void swap_pair(unsigned long int *pair)
+/*@ requires take pairStart = each (i32 j; 0i32 <= j && j < 2i32)
+    {Owned(pair + (j * ((i32) (sizeof<unsigned long int>))))} @*/
+/*@ ensures take pairEnd = each (i32 j; 0i32 <= j && j < 2i32)
+    {Owned(pair + (j * ((i32) (sizeof<unsigned long int>))))} @*/
+/*@ ensures pairEnd[0i32] == pairStart[1i32] @*/
+/*@ ensures pairEnd[1i32] == pairStart[0i32] @*/
+{
+    /*@ extract Owned<unsigned long int>, 0i32; @*/
+    /*@ extract Owned<unsigned long int>, 1i32; @*/
+    /*@ instantiate good<unsigned long int>, 0i32; @*/
+    /*@ instantiate good<unsigned long int>, 1i32; @*/
+    unsigned long int tmp = pair[0];
+    pair[0] = pair[1];
+    pair[1] = tmp;
+}
+
