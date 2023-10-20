fefa0b2090c00da1a63ec4e423501556e71123f5
Vadim Zaliva <lord@crocodile.org>
2023-10-19 18:11:37 -0700

    section for `allocate_region proofs`

--- coq/Proofs/Revocation.v
+++ coq/Proofs/Revocation.v
@@ -885,139 +885,143 @@ Module RevocationProofs.
     auto.
   Qed.
 
-  Lemma allocate_region_same_result:
-    forall mem_state1 mem_state2 tid pref align_int size_int,
+  Section allocate_region_proofs.
+    Parameter mem_state1 : CheriMemoryWithPNVI.mem_state.
+    Parameter mem_state2 : CheriMemoryWithoutPNVI.mem_state.
+    Parameter  tid : MemCommonExe.thread_id.
+    Parameter  pref : CoqSymbol.prefix.
+    Parameter  align_int: CheriMemoryWithPNVI.integer_value.
+    Parameter  size_int : CheriMemoryWithPNVI.integer_value.
+
+    Opaque CheriMemoryWithPNVI.allocator CheriMemoryWithoutPNVI.allocator.
+
+    Lemma allocate_region_same_result:
       mem_state_same mem_state1 mem_state2 ->
 
       lift_sum eq pointer_value_eq False
         (evalErrS (CheriMemoryWithPNVI.allocate_region tid pref align_int size_int) mem_state1)
         (evalErrS (CheriMemoryWithoutPNVI.allocate_region tid pref align_int size_int) mem_state2).
-  Proof.
-    Opaque CheriMemoryWithPNVI.allocator CheriMemoryWithoutPNVI.allocator.
-    intros mem_state1 mem_state2 tid pref align_int size_int M.
-    destruct_mem_state_same M.
-    unfold lift_sum.
-    unfold CheriMemoryWithPNVI.mem_state in *.
-    unfold evalErrS.
-    repeat break_let.
-    repeat break_match;invc Heqs1;invc Heqs0;
-      cbn in Heqp, Heqp0; repeat break_let;
-      rewrite num_of_int_eq in *;
+    Proof.
+      intros M.
+      destruct_mem_state_same M.
+      unfold lift_sum.
+      unfold CheriMemoryWithPNVI.mem_state in *.
+      unfold evalErrS.
+      repeat break_let.
+      repeat break_match;invc Heqs1;invc Heqs0;
+        cbn in Heqp, Heqp0; repeat break_let;
+        rewrite num_of_int_eq in *;
 
 
 
-      remember (Capability_GS.representable_length
-                  (CheriMemoryWithoutPNVI.num_of_int size_int)) as size;
-      clear Heqsize;
-      rewrite num_of_int_eq in *;
-      remember (Z.max (CheriMemoryWithoutPNVI.num_of_int align_int)
-                  (Z.succ
-                     (AddressValue.to_Z
-                        (AddressValue.bitwise_complement
-                           (AddressValue.of_Z
-                              (Z.of_N
-                                 (MachineWord.MachineWord.word_to_N
-                                    (Values.get_word
-                                       (CapFns.CapGetRepresentableMask
-                                          (MachineWord.MachineWord.Z_to_word
-                                             (Pos.to_nat 64)
-                                             (CheriMemoryWithoutPNVI.num_of_int
-                                                size_int))))))))))) as align;
-      clear Heqalign.
+        remember (Capability_GS.representable_length
+                    (CheriMemoryWithoutPNVI.num_of_int size_int)) as size;
+        clear Heqsize;
+        rewrite num_of_int_eq in *;
+        remember (Z.max (CheriMemoryWithoutPNVI.num_of_int align_int)
+                    (Z.succ
+                       (AddressValue.to_Z
+                          (AddressValue.bitwise_complement
+                             (AddressValue.of_Z
+                                (Z.of_N
+                                   (MachineWord.MachineWord.word_to_N
+                                      (Values.get_word
+                                         (CapFns.CapGetRepresentableMask
+                                            (MachineWord.MachineWord.Z_to_word
+                                               (Pos.to_nat 64)
+                                               (CheriMemoryWithoutPNVI.num_of_int
+                                                  size_int))))))))))) as align;
+        clear Heqalign.
 
-    all: assert(E:evalErrS (CheriMemoryWithPNVI.allocator size align) mem_state1 =
-                    evalErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2) by (try  apply allocator_same;
-                                                                                           repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
-                                                                                           try apply Mvarargs1; try apply Mvarargs2);
-      unfold evalErrS in E;
-      repeat break_let;
-      repeat tuple_inversion;
-      destruct s0,s; inv E;repeat tuple_inversion;
-      try reflexivity;
-      repeat break_let;
-      repeat tuple_inversion.
+      all: assert(E:evalErrS (CheriMemoryWithPNVI.allocator size align) mem_state1 =
+                      evalErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2) by (try  apply allocator_same;
+                                                                                             repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
+                                                                                             try apply Mvarargs1; try apply Mvarargs2);
+        unfold evalErrS in E;
+        repeat break_let;
+        repeat tuple_inversion;
+        destruct s0,s; inv E;repeat tuple_inversion;
+        try reflexivity;
+        repeat break_let;
+        repeat tuple_inversion.
 
-    constructor.
-    reflexivity.
-    Transparent CheriMemoryWithPNVI.allocator CheriMemoryWithoutPNVI.allocator.
-  Admitted.
+      constructor.
+      reflexivity.
+      (* Bug: Hangs on Qed here! *)
+    Admitted.
 
-  Lemma allocate_region_same_state:
-    forall mem_state1 mem_state2 tid pref align_int size_int,
+    Lemma allocate_region_same_state:
       mem_state_same mem_state1 mem_state2 ->
       lift_sum eq mem_state_same False
         (execErrS (CheriMemoryWithPNVI.allocate_region tid pref align_int size_int) mem_state1)
         (execErrS (CheriMemoryWithoutPNVI.allocate_region tid pref align_int size_int) mem_state2).
-  Proof.
-    intros mem_state1 mem_state2 tid pref align_int size_int M.
-    destruct_mem_state_same M.
-    Opaque CheriMemoryWithPNVI.allocator CheriMemoryWithoutPNVI.allocator.
-    unfold lift_sum.
-    unfold CheriMemoryWithPNVI.mem_state in *.
-    unfold execErrS.
-    repeat break_let.
-    repeat break_match;invc Heqs1;invc Heqs0;
-      cbn in Heqp, Heqp0; repeat break_let;
-      rewrite num_of_int_eq in *;
+    Proof.
+      intros M.
+      destruct_mem_state_same M.
+      unfold lift_sum.
+      unfold CheriMemoryWithPNVI.mem_state in *.
+      unfold execErrS.
+      repeat break_let.
+      repeat break_match;invc Heqs1;invc Heqs0;
+        cbn in Heqp, Heqp0; repeat break_let;
+        rewrite num_of_int_eq in *;
 
 
 
-      remember (Capability_GS.representable_length
-                  (CheriMemoryWithoutPNVI.num_of_int size_int)) as size;
-      clear Heqsize;
-      rewrite num_of_int_eq in *;
-      remember (Z.max (CheriMemoryWithoutPNVI.num_of_int align_int)
-                  (Z.succ
-                     (AddressValue.to_Z
-                        (AddressValue.bitwise_complement
-                           (AddressValue.of_Z
-                              (Z.of_N
-                                 (MachineWord.MachineWord.word_to_N
-                                    (Values.get_word
-                                       (CapFns.CapGetRepresentableMask
-                                          (MachineWord.MachineWord.Z_to_word
-                                             (Pos.to_nat 64)
-                                             (CheriMemoryWithoutPNVI.num_of_int
-                                                size_int))))))))))) as align;
-      clear Heqalign;
+        remember (Capability_GS.representable_length
+                    (CheriMemoryWithoutPNVI.num_of_int size_int)) as size;
+        clear Heqsize;
+        rewrite num_of_int_eq in *;
+        remember (Z.max (CheriMemoryWithoutPNVI.num_of_int align_int)
+                    (Z.succ
+                       (AddressValue.to_Z
+                          (AddressValue.bitwise_complement
+                             (AddressValue.of_Z
+                                (Z.of_N
+                                   (MachineWord.MachineWord.word_to_N
+                                      (Values.get_word
+                                         (CapFns.CapGetRepresentableMask
+                                            (MachineWord.MachineWord.Z_to_word
+                                               (Pos.to_nat 64)
+                                               (CheriMemoryWithoutPNVI.num_of_int
+                                                  size_int))))))))))) as align;
+        clear Heqalign;
 
-      assert(E0:lift_sum eq mem_state_same False
-                  (execErrS (CheriMemoryWithPNVI.allocator size align) mem_state1)
-                  (execErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2)) by
-      (try apply allocator_same;
-       repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
-       try apply Mvarargs1; try apply Mvarargs2);
-      assert(E1:evalErrS (CheriMemoryWithPNVI.allocator size align) mem_state1 =
-                  evalErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2) by (try  apply allocator_same;
-                                                                                         repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
-                                                                                         try apply Mvarargs1; try apply Mvarargs2);
+        assert(E0:lift_sum eq mem_state_same False
+                    (execErrS (CheriMemoryWithPNVI.allocator size align) mem_state1)
+                    (execErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2)) by
+        (try apply allocator_same;
+         repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
+         try apply Mvarargs1; try apply Mvarargs2);
+        assert(E1:evalErrS (CheriMemoryWithPNVI.allocator size align) mem_state1 =
+                    evalErrS (CheriMemoryWithoutPNVI.allocator size align) mem_state2) by (try  apply allocator_same;
+                                                                                           repeat split;try assumption;destruct Mvarargs as [Mvarargs1 Mvarargs2];
+                                                                                           try apply Mvarargs1; try apply Mvarargs2);
 
-      unfold lift_sum, execErrS in E0;
-      repeat break_let;
-      repeat tuple_inversion;
-      destruct s0,s; inv E0;repeat tuple_inversion;
-      try reflexivity;repeat break_let;
-      repeat tuple_inversion;
-      repeat break_match; invc Heqs1; invc Heqs0.
+        unfold lift_sum, execErrS in E0;
+        repeat break_let;
+        repeat tuple_inversion;
+        destruct s0,s; inv E0;repeat tuple_inversion;
+        try reflexivity;repeat break_let;
+        repeat tuple_inversion;
+        repeat break_match; invc Heqs1; invc Heqs0.
 
-    unfold evalErrS in E1;
-      repeat break_let;
-      repeat tuple_inversion;
-      destruct s0,s; invc E1;repeat tuple_inversion;
+      unfold evalErrS in E1;
+        repeat break_let;
+        repeat tuple_inversion;
+        destruct s0,s; invc E1;repeat tuple_inversion;
 
-      destruct_mem_state_same E0;
-      repeat split;cbn;try assumption;
-      (try setoid_rewrite Mallocs0; try reflexivity);
-      destruct Mvarargs0 as [Mvarargs01 Mvarargs02];
-      try apply Mvarargs01;
-      try apply Mvarargs02.
-    Transparent CheriMemoryWithPNVI.allocator CheriMemoryWithoutPNVI.allocator.
-  Admitted.
+        destruct_mem_state_same E0;
+        repeat split;cbn;try assumption;
+        (try setoid_rewrite Mallocs0; try reflexivity);
+        destruct Mvarargs0 as [Mvarargs01 Mvarargs02];
+        try apply Mvarargs01;
+        try apply Mvarargs02.
+      (* Bug: Hangs on Qed here! *)
+    Admitted.
 
-  Theorem allocate_region_same:
-    forall mem_state1 mem_state2 tid pref align_int size_int,
+    Theorem allocate_region_same:
       mem_state_same mem_state1 mem_state2 ->
-
       lift_sum eq pointer_value_eq False
         (evalErrS (CheriMemoryWithPNVI.allocate_region tid pref align_int size_int) mem_state1)
         (evalErrS (CheriMemoryWithoutPNVI.allocate_region tid pref align_int size_int) mem_state2)
@@ -1025,10 +1029,12 @@ Module RevocationProofs.
         lift_sum eq mem_state_same False
           (execErrS (CheriMemoryWithPNVI.allocate_region tid pref align_int size_int) mem_state1)
           (execErrS (CheriMemoryWithoutPNVI.allocate_region tid pref align_int size_int) mem_state2).
-  Proof.
-    split.
-    - apply allocate_region_same_result; assumption.
-    - apply allocate_region_same_state; assumption.
-  Qed.
+    Proof.
+      split.
+      - apply allocate_region_same_result; assumption.
+      - apply allocate_region_same_state; assumption.
+    Qed.
+
+  End allocate_region_proofs.
 
 End RevocationProofs.
