dd35e24e1202bae3481ace2d703a77b45e133ec1
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2023-11-17 14:57:22 +0000

    CN: test-case involving mask/mod and pointers
    This test case happens to hang Z3 with our current mapping. To be investigated
    why that might be and what we can do about it.

---
+++ tests/cn/mask_ptr.c
@@ -0,0 +1,40 @@
+
+/* A test case about modulus and masking, in both integers and pointers.
+   Originally introduced as a test case because the pointer variant was causing
+   issues for the SMT solver for unclear reasons.
+*/
+
+typedef unsigned long long u64;
+
+enum {
+  SHIFT_AMOUNT = 5
+};
+
+u64
+foo_integer (u64 y)
+/*@ requires mod(y, power(2u64, ((u64) SHIFT_AMOUNT))) == 0u64 @*/
+{
+  u64 x = y;
+  x &= ~ ((1UL << SHIFT_AMOUNT) - 1);
+  /*@ assert (x == y); @*/
+  return x;
+}
+
+
+
+int *
+foo (int *p)
+/*@ requires let p_u64 = (u64) p @*/
+/*@ requires mod(p_u64, power(2u64, ((u64) SHIFT_AMOUNT))) == 0u64 @*/
+{
+  u64 x = ((u64) p);
+  int *p2;
+
+  x &= ~ ((1UL << SHIFT_AMOUNT) - 1);
+
+  p2 = ((int *) x);
+  /*@ assert (((u64) p2) == ((u64) p)); @*/
+  return p2;
+}
+
+
