1a2aba12dece3335733a3ed76db3eea8ee5f8f5c
Vadim Zaliva <lord@crocodile.org>
2024-01-24 14:29:09 -0800

    `Avalue` constructor for `annot` added. `CIntegerType` module intoduced.

--- coq/CheriMemory/CoqAilTypesAux.v
+++ coq/CheriMemory/CoqAilTypesAux.v
@@ -1,4 +1,5 @@
 Require Import CoqImplementation.
+Require Import CoqIntegerType.
 Require Import CoqCtype.
 
 Module AilTypesAux(IMP: Implementation).
--- coq/CheriMemory/CoqCtype.v
+++ coq/CheriMemory/CoqCtype.v
@@ -12,6 +12,7 @@ From Common Require Import Utils SimpleError.
 Require Import CoqLocation.
 Require Import CoqSymbol.
 Require Import CoqAnnot.
+Require Import CoqIntegerType.
 
 Import MonadNotation.
 
@@ -19,32 +20,6 @@ Open Scope monad_scope.
 Open Scope nat_scope.
 Open Scope string_scope.
 
-Inductive integerBaseType : Type :=
- | Ichar: integerBaseType
- | Short: integerBaseType
- | Int_: integerBaseType
- | Long: integerBaseType
- | LongLong: integerBaseType
-   (* Things defined in the standard libraries *)
- | IntN_t:  nat  -> integerBaseType
- | Int_leastN_t:  nat  -> integerBaseType
- | Int_fastN_t:  nat  -> integerBaseType
- | Intmax_t: integerBaseType
- | Intptr_t: integerBaseType.
-
-(* STD Â§6.2.5#17, sentence 1 *)
-Inductive integerType : Type := (* [name = "^\\(\\|\\([a-z A-Z]+_\\)\\)ity[0-9]*'?$"] *)
- | Char: integerType
- | Bool: integerType
- | Signed:  integerBaseType  -> integerType
- | Unsigned:  integerBaseType  -> integerType
- | Enum:  sym  -> integerType
-   (* Things defined in the standard libraries *)
- | Wchar_t: integerType
- | Wint_t: integerType
- | Size_t: integerType
- | Ptrdiff_t: integerType
- | Ptraddr_t: integerType .
 
 (* STD Â§6.2.5#10, sentence 1 *)
 Inductive realFloatingType : Type :=
