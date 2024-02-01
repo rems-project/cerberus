5147c37fe4902d3497b7fb547321df5a35635a66
Vadim Zaliva <lord@crocodile.org>
2024-02-01 15:53:55 -0800

    using correct opam switch

--- tests/run-cheri.sh
+++ tests/run-cheri.sh
@@ -61,7 +61,7 @@ fi
 # CERB="${WITH_CERB:=dune exec --no-print-directory cerberus --no-build -- }"
 CERB="${WITH_CERB:=dune exec --no-print-directory cerberus-cheri --no-build --}"
 if [[ ! -z "${USE_OPAM+x}" ]]; then
-  echo -e "\033[1m\033[33mUsing opam installed cerberus\033[0m";
+  echo -e "\033[1m\033[33mUsing opam installed cerberus-cheri\033[0m";
   CERB=$OPAM_SWITCH_PREFIX/bin/cerberus-cheri
   export CERB_RUNTIME=$OPAM_SWITCH_PREFIX/lib/cerberus/runtime/
 else
