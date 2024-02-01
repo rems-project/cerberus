d544237008f84dd317e1cb055b068d6cc9ca76ee
Vadim Zaliva <lord@crocodile.org>
2024-02-01 10:08:44 -0800

    bugfix. tests pass now.

--- memory/concrete/impl_mem.ml
+++ memory/concrete/impl_mem.ml
@@ -408,10 +408,9 @@ module Concrete : Memory = struct
                 | _ ->
                     `OtherBytes in
               (prov_acc', b.value :: val_acc, offset_acc')
-              ) (`VALID b.prov, [], `PtrBytes 0) bl in
-          let values = List.rev rev_values in
+              ) (`VALID b.prov, [], `PtrBytes 0) (List.rev bl) in
           match _prov, offset_status with
-          | `VALID z, `PtrBytes _ -> (z, `ValidPtrProv, values)
+          | `VALID z, `PtrBytes _ -> (z, `ValidPtrProv, rev_values)
           | `VALID z, _ -> (z, `NotValidPtrProv, rev_values)
           | `INVALID, _ -> (Prov_none, `NotValidPtrProv, rev_values)
 
