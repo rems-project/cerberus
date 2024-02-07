9f31bbe8544eb1eb1ad379898b0ddf81514128ea
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2024-02-07 12:40:44 +0000

    CN: add a test for type_synonym mechanism
    It serves as a reminder what the syntax is.

---
+++ tests/cn/type_synonym.c
@@ -0,0 +1,18 @@
+
+
+/*@
+type_synonym xy_tup = ({u32 x, u32 y})
+
+function (xy_tup) mk_tup (u32 x, u32 y)
+  { {x : x, y : y} }
+@*/
+
+void
+f (unsigned int x, unsigned int y)
+/*@ requires let tup = mk_tup(x, y) @*/ 
+/*@ ensures tup == tup @*/
+{
+  return;
+}
+
+
