c0bc1335ceb4fc03cab15711360730eca13e5bc3
Thomas Sewell <Thomas.Sewell@cl.cam.ac.uk>
2024-03-08 17:39:05 +0000

    Add promotion/annotation to ++/-- elaboration
    The postfix operators x++ and x-- elaborate to arithmetic that introduces a '1'
    constant. Ensure that the '1' constant is annotated with the promoted type of
    the op, and that the main argument is first promoted to this type as well.

---
+++ tests/cn/increments.c
@@ -0,0 +1,34 @@
+
+
+void
+direct (void)
+{
+  unsigned char x = 1;
+  char y = 2;
+
+  x ++;
+  y ++;
+
+  y --;
+  y --;
+
+}
+
+struct has_short {
+  unsigned short x;
+};
+
+void
+indirect (unsigned char *p, struct has_short *q)
+/*@ requires take C = Owned(p) @*/
+/*@ requires take S = Owned(q) @*/
+/*@ ensures take C2 = Owned(p) @*/
+/*@ ensures take S2 = Owned(q) @*/
+{
+  char x = 1;
+  char *r = &x;
+
+  *p++;
+  q->x++;
+  *r++;
+}
