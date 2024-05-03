347aacb836cec2a2648813822b091a1d69697ab3
Rini Banerjee <rb2018@cam.ac.uk>
2024-05-03 16:26:46 +0100

    Fix for record name generation by adding extra pass through CN funs and preds

--- backend/cn/executable_spec_internal.ml
+++ backend/cn/executable_spec_internal.ml
@@ -31,6 +31,19 @@ let generate_ail_stat_strs (bs, (ail_stats_ : CF.GenTypes.genTypeCategory A.stat
   List.map CF.Pp_utils.to_plain_pretty_string doc
 
 
+let populate_record_map_aux (sym, bt_ret_type) = 
+  match Cn_internal_to_ail.bt_to_cn_base_type bt_ret_type with 
+    | Cn.CN_record members -> 
+      let sym' = Cn_internal_to_ail.generate_sym_with_suffix ~suffix:"_record" sym in
+      Cn_internal_to_ail.records := Cn_internal_to_ail.RecordMap.add members sym' !(Cn_internal_to_ail.records);
+    | _ -> ()
+
+
+let populate_record_map (prog5: unit Mucore.mu_file) = 
+  let fun_syms_and_ret_types = List.map (fun (sym, (def : LogicalFunctions.definition)) -> (sym, def.return_bt)) prog5.mu_logical_predicates in
+  let pred_syms_and_ret_types = List.map (fun (sym, (def : ResourcePredicates.definition)) -> (sym, def.oarg_bt)) prog5.mu_resource_predicates in
+  let _ = List.map populate_record_map_aux (fun_syms_and_ret_types @ pred_syms_and_ret_types) in 
+  ()
 
 let rec extract_global_variables = function
   | [] -> []
