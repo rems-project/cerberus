30a3accb568b9ac29c96982e1fa32b16af22bb3c
Dhruv Makwana <dc_mak@outlook.com>
2024-05-13 18:12:37 +0100

    CN VIP: Specify and factor out mem{cpy,cmp}
    This makes it clearer what functionality is missing.

--- tests/cn_vip_testsuite/pointer_copy_memcpy.c
+++ tests/cn_vip_testsuite/pointer_copy_memcpy.c
@@ -1,10 +1,21 @@
 //CN_VIP #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
+#include "cn_lemmas.h"
 int x=1;
-int main() {
+int main()
+/*@ accesses x; @*/
+{
   int *p = &x;
   int *q;
-  memcpy (&q, &p, sizeof p);
+  /*CN_VIP*/if (&p == &q) return 0;                                         // CN used to derive disjointness and non-null
+  /*CN_VIP*/if ((uintptr_t)&p + sizeof(int*) < (uintptr_t)&p) return 0;     // constraints from resource ownership, but this
+  /*CN_VIP*/if ((uintptr_t)&q + sizeof(int*) < (uintptr_t)&q) return 0;     // was removed for performance reasons.
+  /*CN_VIP*/unsigned char *q_bytes = block_int_ptr_to_block_uchar_arr(&q);
+  /*CN_VIP*/unsigned char *p_bytes = owned_int_ptr_to_owned_uchar_arr(&p);
+  _memcpy (q_bytes, p_bytes, sizeof p);
+  /*CN_VIP*//*@ apply byte_ptr_to_int_ptr_ptr(q_bytes); @*/
+  /*CN_VIP*//*@ apply byte_ptr_to_int_ptr_ptr(p_bytes); @*/
   *q = 11; // is this free of undefined behaviour?
   //CN_VIP printf("*p=%d  *q=%d\n",*p,*q);
   /*CN_VIP*//*@ assert(*p == 11i32 && *q == 11i32); @*/
