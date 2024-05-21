73081d5c4d11a662987e77fd3cddbd7f846c9541
Rini Banerjee <rb2018@cam.ac.uk>
2024-05-21 14:38:44 +0100

    Add new script for generating executable spec files, compiling, linking and running them

---
+++ tests/compile-and-run-executable-spec.sh
@@ -0,0 +1,109 @@
+#! /bin/bash
+
+# tests for the CN executable spec tool attached to cerberus
+
+DIRNAME=$1
+
+SUCC=$(find $DIRNAME -maxdepth 1 -name '*.c' | grep -v '\.error\.c' | grep -v '\.unknown\.c' | grep -v '\-exec\.c')
+
+NUM_GENERATION_FAILED=0
+GENERATION_FAILED=''
+
+NUM_COMPILATION_FAILED=0
+COMPILATION_FAILED=''
+
+NUM_LINKING_FAILED=0
+LINKING_FAILED=''
+
+NUM_RUNNING_BINARY_FAILED=0
+RUNNING_BINARY_FAILED=''
+
+EXECUTABLE_SPEC_DIR='../executable-spec'
+
+mkdir -p $DIRNAME/exec
+clang -fno-lto -c $EXECUTABLE_SPEC_DIR/hash_table.c $EXECUTABLE_SPEC_DIR/alloc.c $EXECUTABLE_SPEC_DIR/cn_utils.c
+
+for TEST in $SUCC
+do
+  TEST_BASENAME=$(basename $TEST .c)
+  mkdir -p $DIRNAME/exec/$TEST_BASENAME
+  echo Generating $DIRNAME/exec/$TEST_BASENAME/$TEST_BASENAME-exec.c ...
+  if ! ~/.opam/4.14.0/bin/cn $TEST --output_decorated=$TEST_BASENAME-exec.c --output_decorated_dir=$DIRNAME/exec/$TEST_BASENAME/
+  then
+    echo Generation failed.
+    NUM_GENERATION_FAILED=$(( $NUM_GENERATION_FAILED + 1 ))
+    GENERATION_FAILED="$GENERATION_FAILED $TEST"
+  else 
+    echo Generation succeeded!
+    echo Compiling $DIRNAME/exec/$TEST_BASENAME/$TEST_BASENAME-exec.c ...
+    if ! clang -fno-lto -c $DIRNAME/exec/$TEST_BASENAME/$TEST_BASENAME-exec.c $DIRNAME/exec/$TEST_BASENAME/cn.c   
+    then
+      echo Compilation failed.
+      NUM_COMPILATION_FAILED=$(( $NUM_COMPILATION_FAILED + 1 ))
+      COMPILATION_FAILED="$COMPILATION_FAILED $TEST"
+    else 
+      echo Compilation succeeded!
+      echo Linking $TEST_BASENAME-exec.o with other files ...
+      if ! clang -o $TEST_BASENAME-output $TEST_BASENAME-exec.o cn.o hash_table.o alloc.o cn_utils.o
+      then 
+        echo Linking failed.
+        NUM_LINKING_FAILED=$(( $NUM_LINKING_FAILED + 1 ))
+        LINKING_FAILED="$LINKING_FAILED $TEST"
+      else 
+        echo Linking succeeded!
+        echo Running the $TEST_BASENAME-output binary ...
+        if ! ./$TEST_BASENAME-output
+        then 
+            echo Running binary failed.
+            NUM_RUNNING_BINARY_FAILED=$(( $NUM_RUNNING_BINARY_FAILED + 1 ))
+            RUNNING_BINARY_FAILED="$RUNNING_BINARY_FAILED $TEST"
+        else 
+            echo Running binary succeeded!
+        fi
+      fi
+    fi
+  fi
+  
+done
+
+
+# FAIL=$(find $DIRNAME -name '*.error.c')
+
+# for TEST in $FAIL
+# do
+#   echo cn $TEST
+#   if ! cn $TEST
+#   then
+#     NUM_FAILED=$(( $NUM_FAILED + 1 ))
+#     FAILED="$FAILED $TEST"
+#   fi
+# done
+
+# UNKNOWN=$(find $DIRNAME -name '*.unknown.c')
+
+# echo $UNKNOWN | xargs -n 1 cn
+
+echo
+echo 'Done running tests.'
+echo
+
+if [ -z "$GENERATION_FAILED$COMPILATION_FAILED$LINKING_FAILED$RUNNING_BINARY_FAILED" ]
+then
+  echo "All tests passed."
+  exit 0
+else
+  echo "$NUM_GENERATION_FAILED tests failed to have executable specs generated:"
+  echo "  $GENERATION_FAILED"
+  echo " "
+  echo "$NUM_COMPILATION_FAILED tests failed to be compiled:"
+  echo "  $COMPILATION_FAILED"
+  echo " "
+  echo "$NUM_LINKING_FAILED tests failed to be linked:"
+  echo "  $LINKING_FAILED"
+  echo " "
+  echo "$NUM_RUNNING_BINARY_FAILED tests failed to be run as binaries:"
+  echo "  $RUNNING_BINARY_FAILED"
+  exit 1
+fi
+
+
