1fced18ca419de7be4a07a2b4fd5c5e008472f19
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2024-07-05 10:18:14 +0100

    Adding a new string to `Version` that always returns the git commit version and using it for CN
    This is to prevent getting a Cerberus version number from CN if we ever start using
    the watermark %%VERSION%% when running `tools/gen_version.ml`.

--- tools/gen_version.ml
+++ tools/gen_version.ml
@@ -1,8 +1,5 @@
-let version =
-  (* Trick to check whether the watermark has been substituted. *)
-  if "%%VERSION%%" <> "%%" ^ "VERSION%%" then "%%VERSION%%" else
-  (* If not, we fallback to git version. *)
-  let cmd = "git describe --dirty --always" in 
+let git_version =
+  let cmd = "git describe --dirty --always" in
   let (oc, ic, ec) = Unix.open_process_full cmd (Unix.environment ()) in
   let version =
     try Printf.sprintf "git-%s" (input_line oc)
@@ -12,9 +9,18 @@ let version =
   | Unix.WEXITED(0) -> version
   | _               -> "unknown"
 
+let version =
+  (* Trick to check whether the watermark has been substituted. *)
+  if "%%VERSION%%" <> "%%" ^ "VERSION%%" then "%%VERSION%%" else
+  (* If not, we fallback to git version. *)
+  git_version
+
 let _ =
   let line fmt = Printf.printf (fmt ^^ "\n%!") in
   line "(** Version informations. *)";
+  line "";
+  line "(** [git_version] gives a git commit version description. *)";
+  line "let git_version : string = \"%s\"" git_version;
   line "";
   line "(** [version] gives a version description. *)";
   line "let version : string = \"%s\"" version
