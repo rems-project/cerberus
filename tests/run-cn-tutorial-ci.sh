84916ff4d3c3cb9ec49d980ba490ee4874655977
septract <miked@galois.com>
2024-07-09 17:44:29 -0700

    Tidy up the tutorial repo CI by using the new `make check` target

--- tests/run-cn-tutorial-ci.sh
+++ tests/run-cn-tutorial-ci.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 TUTORIAL_PATH=$1
 
 if [ -n "$TUTORIAL_PATH" ] 
@@ -11,8 +10,6 @@ else
     exit 1
 fi
 
-
-
 # copying from run-ci.sh
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:`ocamlfind query z3`
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`ocamlfind query z3`
@@ -20,38 +17,15 @@ CN=$OPAM_SWITCH_PREFIX/bin/cn
 
 HERE=$(pwd)
 
-cd "$TUTORIAL_PATH"/src/example-archive/
+cd "$TUTORIAL_PATH"
 
 FAILURE=0
 
-# Check the example archive 
-./check-all.sh "$CN --solver-type=cvc5"
-if [ $? != 0 ] 
-then
-   FAILURE=1
-fi
+make check CN_PATH="$CN --solver-type=cvc5"
+FAILURE+=$?
 
-./check-all.sh "$CN --solver-type=z3"
-if [ $? != 0 ] 
-then
-   FAILURE=1
-fi
-
-
-# Check the tutorial examples 
-cd ../..
-
-./check.sh "$CN --solver-type=cvc5"
-if [ $? != 0 ] 
-then
-   FAILURE=1
-fi
-
-./check.sh "$CN --solver-type=z3"
-if [ $? != 0 ] 
-then
-   FAILURE=1
-fi
+make check CN_PATH="$CN --solver-type=z3"
+FAILURE+=$?
 
 cd $HERE
 
