92d3432afdb5fe7cce33e1a0224846197b84d317
Zain Aamer <ZippeyKeys12@gmail.com>
2024-07-22 15:45:48 -0400

    [CN-Test-Gen] Pull test frameworks into modules

--- backend/cn/testGeneration/codify.ml
+++ backend/cn/testGeneration/codify.ml
@@ -7,15 +7,21 @@ open Dsl
 
 type test_framework = GTest
 
-let codify_sym = codify_sym
+module type TF_Codify = sig
+  val codify_header : string -> string -> string
+end
+
+module GTest_Codify = struct
+  let codify_header (suite : string) (test : string) : string =
+    "RC_GTEST_PROP(Test" ^ String.capitalize_ascii suite ^ ", " ^ test ^ ", ())\n"
+end
 
 let rec codify_it_ (e : BT.t IT.term_) : string option =
   let exception Unsupported_codify_it in
   try
     Some
       (match e with
-       | Const Null ->
-         "nullptr"
+       | Const Null -> "nullptr"
        | Const (Bits ((Signed, bits), n)) when bits <= 16 ->
          Int64.to_string (Z.to_int64 n)
        | Const (Bits ((Unsigned, bits), n)) when bits <= 16 ->
@@ -28,75 +34,49 @@ let rec codify_it_ (e : BT.t IT.term_) : string option =
          Int64.to_string (Z.to_int64 n) ^ "LL"
        | Const (Bits ((Unsigned, bits), n)) when bits <= 64 ->
          Int64.to_string (Z.to_int64 n) ^ "ULL"
-       | Const (Bool b) ->
-         string_of_bool b
-       | Sym x ->
-         codify_sym x
-       | Unop (Not, e') ->
-         "(!" ^ codify_it e' ^ ")"
-       | Unop (Negate, e') ->
-         "(-" ^ codify_it e' ^ ")"
-       | Binop (And, e1, e2) ->
-         "(" ^ codify_it e1 ^ " && " ^ codify_it e2 ^ ")"
-       | Binop (Or, e1, e2) ->
-         codify_it e1 ^ " || " ^ codify_it e2 ^ ")"
-       | Binop (Implies, e1, e2) ->
-         "((!" ^ codify_it e1 ^ ") || " ^ codify_it e2 ^ ")"
-       | Binop (Add, e1, e2) ->
-         "(" ^ codify_it e1 ^ " + " ^ codify_it e2 ^ ")"
-       | Binop (Sub, e1, e2) ->
-         "(" ^ codify_it e1 ^ " - " ^ codify_it e2 ^ ")"
-       | Binop (Mul, e1, e2)
-       | Binop (MulNoSMT, e1, e2) ->
+       | Const (Bool b) -> string_of_bool b
+       | Sym x -> codify_sym x
+       | Unop (Not, e') -> "(!" ^ codify_it e' ^ ")"
+       | Unop (Negate, e') -> "(-" ^ codify_it e' ^ ")"
+       | Binop (And, e1, e2) -> "(" ^ codify_it e1 ^ " && " ^ codify_it e2 ^ ")"
+       | Binop (Or, e1, e2) -> codify_it e1 ^ " || " ^ codify_it e2 ^ ")"
+       | Binop (Implies, e1, e2) -> "((!" ^ codify_it e1 ^ ") || " ^ codify_it e2 ^ ")"
+       | Binop (Add, e1, e2) -> "(" ^ codify_it e1 ^ " + " ^ codify_it e2 ^ ")"
+       | Binop (Sub, e1, e2) -> "(" ^ codify_it e1 ^ " - " ^ codify_it e2 ^ ")"
+       | Binop (Mul, e1, e2) | Binop (MulNoSMT, e1, e2) ->
          "(" ^ codify_it e1 ^ " * " ^ codify_it e2 ^ ")"
-       | Binop (Div, e1, e2)
-       | Binop (DivNoSMT, e1, e2) ->
+       | Binop (Div, e1, e2) | Binop (DivNoSMT, e1, e2) ->
          "(" ^ codify_it e1 ^ " / " ^ codify_it e2 ^ ")"
-       | Binop (XORNoSMT, e1, e2) ->
-         "(" ^ codify_it e1 ^ " ^ " ^ codify_it e2 ^ ")"
-       | Binop (BWAndNoSMT, e1, e2) ->
-         "(" ^ codify_it e1 ^ " & " ^ codify_it e2 ^ ")"
-       | Binop (BWOrNoSMT, e1, e2) ->
-         "(" ^ codify_it e1 ^ " | " ^ codify_it e2 ^ ")"
-       | Binop (ShiftLeft, e1, e2) ->
-         "(" ^ codify_it e1 ^ " << " ^ codify_it e2 ^ ")"
-       | Binop (ShiftRight, e1, e2) ->
-         "(" ^ codify_it e1 ^ " >> " ^ codify_it e2 ^ ")"
-       | Binop (LT, e1, e2)
-       | Binop (LTPointer, e1, e2) ->
+       | Binop (XORNoSMT, e1, e2) -> "(" ^ codify_it e1 ^ " ^ " ^ codify_it e2 ^ ")"
+       | Binop (BWAndNoSMT, e1, e2) -> "(" ^ codify_it e1 ^ " & " ^ codify_it e2 ^ ")"
+       | Binop (BWOrNoSMT, e1, e2) -> "(" ^ codify_it e1 ^ " | " ^ codify_it e2 ^ ")"
+       | Binop (ShiftLeft, e1, e2) -> "(" ^ codify_it e1 ^ " << " ^ codify_it e2 ^ ")"
+       | Binop (ShiftRight, e1, e2) -> "(" ^ codify_it e1 ^ " >> " ^ codify_it e2 ^ ")"
+       | Binop (LT, e1, e2) | Binop (LTPointer, e1, e2) ->
          "(" ^ codify_it e1 ^ " < " ^ codify_it e2 ^ ")"
-       | Binop (LE, e1, e2)
-       | Binop (LEPointer, e1, e2) ->
+       | Binop (LE, e1, e2) | Binop (LEPointer, e1, e2) ->
          "(" ^ codify_it e1 ^ " <= " ^ codify_it e2 ^ ")"
-       | Binop (EQ, e1, e2) ->
-         "(" ^ codify_it e1 ^ " == " ^ codify_it e2 ^ ")"
-       | Binop (Mod, e1, e2) ->
-         "(" ^ codify_it e1 ^ " % " ^ codify_it e2 ^ ")"
+       | Binop (EQ, e1, e2) -> "(" ^ codify_it e1 ^ " == " ^ codify_it e2 ^ ")"
+       | Binop (Mod, e1, e2) -> "(" ^ codify_it e1 ^ " % " ^ codify_it e2 ^ ")"
        | ITE (e1, e2, e3) ->
          "(" ^ codify_it e1 ^ " ? " ^ codify_it e2 ^ " : " ^ codify_it e3 ^ ")"
        (* *)
-       | _ ->
-         raise Unsupported_codify_it)
+       | _ -> raise Unsupported_codify_it)
   with
-  | Unsupported_codify_it ->
-    None
+  | Unsupported_codify_it -> None
 
 
 and codify_it (e : IT.t) : string =
   let (IT (e_, _, _)) = e in
   match codify_it_ e_ with
-  | Some str ->
-    str
-  | None ->
-    failwith ("unsupported operation " ^ Pp_utils.to_plain_pretty_string (IT.pp e))
+  | Some str -> str
+  | None -> failwith ("unsupported operation " ^ Pp_utils.to_plain_pretty_string (IT.pp e))
 
 
 let rec codify_gen' (g : gen) : string =
   match g with
-  | Arbitrary ty ->
-    "rc::gen::arbitrary<" ^ string_of_ctype ty ^ ">()"
-  | Return (ty, e) ->
-    "rc::gen::just<" ^ string_of_ctype ty ^ ">(" ^ codify_it e ^ ")"
+  | Arbitrary ty -> "rc::gen::arbitrary<" ^ string_of_ctype ty ^ ">()"
+  | Return (ty, e) -> "rc::gen::just<" ^ string_of_ctype ty ^ ">(" ^ codify_it e ^ ")"
   | Filter (x', ty, e, g') ->
     let gen = codify_gen' g' in
     "rc::gen::suchThat("
@@ -119,8 +99,7 @@ let rec codify_gen' (g : gen) : string =
     ^ "){ return "
     ^ codify_it e
     ^ "; })"
-  | Alloc (ty, x) ->
-    "rc::gen::just<" ^ string_of_ctype ty ^ ">(&" ^ codify_sym x ^ ")"
+  | Alloc (ty, x) -> "rc::gen::just<" ^ string_of_ctype ty ^ ">(&" ^ codify_sym x ^ ")"
   | Struct (ty, ms) ->
     "rc::gen::just<"
     ^ string_of_ctype ty
@@ -141,25 +120,11 @@ let codify_gen (x : Sym.sym) (g : gen) : string =
 
 
 let rec codify_gen_context (gtx : gen_context) : string =
-  match gtx with
-  | (x, g) :: gtx' ->
-    codify_gen x g ^ codify_gen_context gtx'
-  | [] ->
-    ""
+  match gtx with (x, g) :: gtx' -> codify_gen x g ^ codify_gen_context gtx' | [] -> ""
 
 
-let codify_pbt_header
-  (tf : test_framework)
-  (suite : string)
-  (test : string)
-  (oc : out_channel)
-  : unit
-  =
-  match tf with
-  | GTest ->
-    output_string
-      oc
-      ("\nRC_GTEST_PROP(Test" ^ String.capitalize_ascii suite ^ ", " ^ test ^ ", ()){\n")
+let codify_header (tf : test_framework) (suite : string) (test : string) : string =
+  match tf with GTest -> GTest_Codify.codify_header suite test
 
 
 let codify_pbt
@@ -171,12 +136,13 @@ let codify_pbt
   (gtx : gen_context)
   : unit
   =
-  codify_pbt_header tf (codify_sym instrumentation.fn) ("Test" ^ string_of_int index) oc;
+  output_string
+    oc
+    (codify_header tf (codify_sym instrumentation.fn) ("Test" ^ string_of_int index));
+  output_string oc "{\n";
   output_string oc (codify_gen_context gtx);
   output_string oc (codify_sym instrumentation.fn);
   output_string oc "(";
   output_string oc (args |> List.map fst |> List.map codify_sym |> String.concat ", ");
   output_string oc ");\n";
-  match tf with
-  | GTest ->
-    output_string oc "}\n\n"
+  output_string oc "}\n\n"
