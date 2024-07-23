c77d64541983966a50cc00e349b6627756d18024
Zain Aamer <ZippeyKeys12@gmail.com>
2024-07-23 04:51:04 -0400

    [CN-Test-Gen] Basic CI tests (#410)

---
+++ tests/cn-test-gen/src/abs_mem.c
@@ -0,0 +1,16 @@
+int abs_mem(int* p)
+/*@ requires take x = Owned<int>(p);
+             MINi32() < x;
+    ensures take x2 = Owned<int>(p);
+            x == x2;
+            return == ((x >= 0i32) ? x : (0i32-x));
+@*/
+{
+  int x = *p;
+  if (x >= 0) {
+    return x;
+  }
+  else {
+    return -x;
+  }
+}
