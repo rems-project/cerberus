587f441f17c36f59b3b0aa59aa2a725bc21b6a24
Zain Aamer <ZippeyKeys12@gmail.com>
2024-07-23 02:26:59 -0400

    [CN-Test-Gen] Force `output_dir` to be a directory
    Make sure path always ends with a separator

--- backend/cn/main.ml
+++ backend/cn/main.ml
@@ -343,6 +343,12 @@ let generate_tests
   =
   (* flags *)
   Cerb_debug.debug_level := debug_level;
+  let output_dir =
+    if String.ends_with ~suffix:Filename.dir_sep output_dir then
+      output_dir
+    else
+      output_dir ^ Filename.dir_sep
+  in
   let handle_error (e : TypeErrors.type_error) =
     let report = TypeErrors.pp_message e.msg in
     Pp.error e.loc report.short (Option.to_list report.descr);
