7ae69655485d8cf9cee34ad9211786d3fd256a7d
Benjamin Pierce <bcpierce00t@gmail.com>
2024-07-25 10:08:46 -0400

    Tidy solver.mli

--- backend/cn/solver.mli
+++ backend/cn/solver.mli
@@ -1,16 +1,22 @@
 (* Module Solver -- Interface to the SMT solver via SMTLIB *)
+
 type solver
 
 type model
 
-(* (TODO: BCP: The "with quantifiers" part will be the instantiations that the solver
-   found -- is that right?) *)
-type model_with_q = model * (Sym.t * LogicalSorts.t) list
-
 val empty_model : model
 
-(* Global flags to pass to the solver (TODO: BCP: Could use a bit more documentation,
-   maybe) *)
+type model_with_q = model * (Sym.t * LogicalSorts.t) list
+(* The "with quantifiers" part will be the instantiations that the
+   solver found.  To implement provable (Forall ((s,bt), t)), CN picks
+   a fresh variable s' and checks that not t[s'/s] is
+   unsatisfiable ("there exists no choice of s such that t doesn't
+   hold"). If provable returns false, i.e. the SMT solver found an s'
+   for which t[s'/s] doesn't hold, then model_with_q has that s' for
+   its second component. *)
+
+(* Global flags to pass to the solver *)
+
 val random_seed : int ref
 
 val log_to_temp : bool ref
@@ -34,11 +40,14 @@ val push : solver -> unit
 
 val pop : solver -> int -> unit
 
-(* TODO: BCP: What is this? *)
 val num_scopes : solver -> int
 
-(* Run the solver. Note that we pass the assumptions explicitly even though they are also
-   available in the solver context, because CN is going some simplification on its own. *)
+(* Run the solver.  Note that we pass the assumptions explicitly even
+   though they are also available in the solver context, because CN is
+   going some simplification on its own.  Plus we have a default
+   quantifier instantiation heuristic, which adds additional
+   constraints derived from assumptions based on the to-be-proved
+   constraint. *)
 val provable
   :  loc:Locations.t ->
   solver:solver ->
@@ -51,18 +60,23 @@ val provable
 (* Ask the solver for the model that it found in a call to [provable] *)
 val model : unit -> model_with_q
 
-(* Ask the solver to evaluate a CN term in the context of a model. (Might return None in
-   case we ask for the value of a "don't care" value in the (minimal) model.) *)
-(* TODO: BCP: I don't understand how this could ever be called -- how do we get a model to
-   pass it??? *)
+(* Ask the solver to evaluate a CN term in the context of a
+   model.  (Might return None in case we ask for the value of a "don't
+   care" value in the (minimal) model.) *)
 val eval
   :  Global.t ->
   (* TODO: BCP: IIUC Christopher thinks this is not needed? *)
   model ->
   IndexTerms.t ->
   IndexTerms.t option
+(* TODO: CP: Here and in other places in the solver module, I think,
+   the global argument can be removed: the solver is initialised with
+   a global typing context at some point earlier on, and then this is
+   expected not to change any longer. *)
 
-(* TODO: BCP: What is this? *)
+(* For logging slow-running SMT queries (TODO: BCP: CP says maybe this
+   is not needed any more... I see that it is called from Main, but
+   possibly it could be removed?) *)
 val set_slow_smt_settings : float option -> string option -> unit
 
 (* Debugging *)
