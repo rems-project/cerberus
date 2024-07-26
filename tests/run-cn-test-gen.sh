a84563c879e28f6a1aae72c143cf0f4caf7593c1
Zain Aamer <ZippeyKeys12@gmail.com>
2024-07-26 19:08:47 -0400

    [CN-Test-Gen/CN-Exec] Make CI tests use executable spec

--- tests/run-cn-test-gen.sh
+++ tests/run-cn-test-gen.sh
@@ -5,68 +5,112 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(ocamlfind query z3)
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(ocamlfind query z3)
 CN=$OPAM_SWITCH_PREFIX/bin/cn
 
-DIRNAME=$(dirname $0)
+DIRNAME=$(dirname "$0")
 
 # Clean directory
-cd $DIRNAME/cn-test-gen
-rm -rf test build
-mkdir test build
+cd "$DIRNAME"/cn-test-gen || exit
+rm -rf build decorated test
+mkdir build decorated test
 
 # Get `*.c` files
-SUCC=$(find $DIRNAME/src -name '*.c')
+FILES=$(find "$DIRNAME"/src -name '*.c')
 
 # Track failures
 NUM_FAILED=0
 FAILED=''
 
 # Test each `*.c` file
-for TEST in $SUCC; do
+for TEST in $FILES; do
+  CLEANUP="rm -rf decorated/*"
+
+  # Instrument file
+  $CN verify "$TEST" --output-decorated="$(basename "$TEST")" --output-decorated-dir=decorated/
+  RET=$?
+  if [[ "$RET" != 0 ]]; then
+    echo "$TEST -- Error during executable spec decoration"
+    NUM_FAILED=$(($NUM_FAILED + 1))
+    FAILED="$FAILED $TEST"
+    eval "$CLEANUP"
+    continue
+  else
+    echo "$TEST -- Executable spec decorated successfully"
+  fi
+
+  CLEANUP="rm -rf test/*;$CLEANUP"
+
   # Generate tests
-  $CN generate-tests $TEST --output-dir="test"
+  $CN generate-tests "$TEST" --output-dir="test"
   RET=$?
   if [[ "$RET" != 0 ]]; then
     echo "$TEST -- Error during test generation"
     NUM_FAILED=$(($NUM_FAILED + 1))
     FAILED="$FAILED $TEST"
+    eval "$CLEANUP"
+    continue
   else
     echo "$TEST -- Tests generated successfully"
+  fi
 
-    # Run CMake
-    cd build
-    cmake .. >/dev/null
+  CLEANUP="cd ..;$CLEANUP"
+
+  # Run CMake
+  cd build || exit
+  cmake .. >/dev/null
+  RET=$?
+  if [[ "$RET" != 0 ]]; then
+    echo "$TEST -- Error while running CMake"
+    NUM_FAILED=$(($NUM_FAILED + 1))
+    FAILED="$FAILED $TEST"
+    eval "$CLEANUP"
+    continue
+  else
+    echo "$TEST -- CMake run successfully"
+  fi
+
+  # Build tests
+  make >/dev/null
+  RET=$?
+  if [[ "$RET" != 0 ]]; then
+    echo "$TEST -- Error while building tests"
+    NUM_FAILED=$(($NUM_FAILED + 1))
+    FAILED="$FAILED $TEST"
+    eval "$CLEANUP"
+    continue
+  else
+    echo "$TEST -- Tests built successfully"
+  fi
+
+  # Run passing tests
+  if [[ $TEST == *.pass.c ]]; then
+    ./ci_tests >/dev/null
     RET=$?
     if [[ "$RET" != 0 ]]; then
-      echo "$TEST -- Error while running CMake"
+      echo "$TEST -- Tests failed unexpectedly"
       NUM_FAILED=$(($NUM_FAILED + 1))
       FAILED="$FAILED $TEST"
+      eval "$CLEANUP"
+      continue
     else
-      echo "$TEST -- CMake run successfully"
-
-      # Build tests
-      make >/dev/null
-      RET=$?
-      if [[ "$RET" != 0 ]]; then
-        echo "$TEST -- Error while building tests"
-        NUM_FAILED=$(($NUM_FAILED + 1))
-        FAILED="$FAILED $TEST"
-      else
-        echo "$TEST -- Tests built successfully"
-
-        # Run tests
-        ./ci_tests >/dev/null
-        RET=$?
-        if [[ "$RET" != 0 ]]; then
-          echo "$TEST -- Error while running tests"
-          NUM_FAILED=$(($NUM_FAILED + 1))
-          FAILED="$FAILED $TEST"
-        else
-          echo "$TEST -- Tests run successfully"
-        fi
-      fi
+      echo "$TEST -- Tests passed successfully"
     fi
-    cd ..
   fi
-  rm -rf test/*
+
+  # Run failing tests
+  if [[ $TEST == *.fail.c ]]; then
+    ./ci_tests 2>/dev/null
+    RET=$?
+    if [[ "$RET" = 0 ]]; then
+      echo "$TEST -- Tests passed unexpectedly"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED $TEST"
+      eval "$CLEANUP"
+      continue
+    else
+      echo "$TEST -- Tests failed successfully"
+    fi
+  fi
+
+  eval "$CLEANUP"
 done
 
 echo
