31ad5db6b658536cafd2ecb603d7f14d2706100c
Christopher Pulte <cp526@cam.ac.uk>
2024-07-26 11:31:58 +0100

    [CN] improve CN performance (#436)
    CN derives constraints from resource ownership; this fixes (some of
    the) duplication in what constraints are derived

--- backend/cn/lib/typing.ml
+++ backend/cn/lib/typing.ml
@@ -445,7 +445,11 @@ let add_r_internal loc (r, RE.O oargs) =
   let@ simp_ctxt = simp_ctxt () in
   let r = Simplify.ResourceTypes.simp simp_ctxt r in
   let oargs = Simplify.IndexTerms.simp simp_ctxt oargs in
-  let pointer_facts = Resources.pointer_facts ((r, RE.O oargs) :: Context.get_rs s) in
+  let pointer_facts =
+    Resources.pointer_facts
+      ~new_resource:(r, RE.O oargs)
+      ~old_resources:(Context.get_rs s)
+  in
   let@ () = set_typing_context (Context.add_r loc (r, O oargs) s) in
   iterM (fun x -> add_c_internal (LC.T x)) pointer_facts
 
