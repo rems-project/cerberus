rems-project/cerberus

commit c77d64541983966a50cc00e349b6627756d18024

[CN-Test-Gen] Basic CI tests (#410)

.github/workflows/ci.yml
@@ -27,7 +27,7 @@ jobs:
 
     - name: System dependencies (ubuntu)
       run: |
-        sudo apt install build-essential libgmp-dev z3 opam
+        sudo apt install build-essential libgmp-dev z3 opam cmake
     
     - name: Restore cached opam
       id: cache-opam-restore
@@ -112,11 +112,17 @@ jobs:
         eval $(opam env --switch=${{ matrix.version }})
         USE_OPAM='' tests/run-cn-tutorial-ci.sh cn-tutorial
 
+    - name: Run CN-Test-Gen CI tests
+      run: |
+        opam switch ${{ matrix.version }}
+        eval $(opam env --switch=${{ matrix.version }})
+        cd tests; USE_OPAM='' ./run-cn-test-gen.sh
+        cd ..
+
     - name: Check CN code formatting
       run: |
         opam switch ${{ matrix.version }}
         eval $(opam env --switch=${{ matrix.version }})
-        USE_OPAM='' cd backend/cn && dune build @fmt
 
     - name: Install Cerberus-CHERI
       if: ${{ matrix.version == '4.14.1' }}

tests/cn-test-gen/.gitignore
@@ -1,0 +1,2 @@
+build/
+test/

tests/cn-test-gen/CMakeLists.txt
@@ -1,0 +1,37 @@
+cmake_minimum_required(VERSION 3.20)
+project(ci_tests)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set (RC_ENABLE_GTEST ON)
+
+file(GLOB_RECURSE SRC src/*.c test/*.cpp)
+
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+  DOWNLOAD_EXTRACT_TIMESTAMP true
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+enable_testing()
+
+include(GoogleTest)
+
+FetchContent_Declare(
+  rapidcheck
+  GIT_REPOSITORY https://github.com/emil-e/rapidcheck.git
+)
+FetchContent_MakeAvailable(rapidcheck)
+
+add_executable(ci_tests ${SRC})
+
+target_link_libraries(ci_tests GTest::gtest_main rapidcheck rapidcheck_gtest)
+
+target_include_directories(ci_tests PRIVATE)
+
+gtest_discover_tests(ci_tests)

tests/cn-test-gen/src/abs.c
@@ -1,0 +1,12 @@
+int abs(int x)
+/*@ requires MINi32() < x;
+    ensures return == ((x >= 0i32) ? x : (0i32-x));
+@*/
+{
+  if (x >= 0) {
+    return x;
+  }
+  else {
+    return -x;
+  }
+}

tests/cn-test-gen/src/abs_mem.c
@@ -1,0 +1,16 @@
+int abs_mem(int* p)
+/*@ requires take x = Owned<int>(p);
+             MINi32() < x;
+    ensures take x2 = Owned<int>(p);
+            x == x2;
+            return == ((x >= 0i32) ? x : (0i32-x));
+@*/
+{
+  int x = *p;
+  if (x >= 0) {
+    return x;
+  }
+  else {
+    return -x;
+  }
+}

tests/cn-test-gen/src/list_rev.c
@@ -1,0 +1,78 @@
+struct int_list {
+  int head;
+  struct int_list* tail;
+};
+
+/*@
+datatype seq {
+  Seq_Nil {},
+  Seq_Cons {i32 head, datatype seq tail}
+}
+
+predicate (datatype seq) IntList(pointer p) {
+  if (is_null(p)) {
+    return Seq_Nil{};
+  } else {
+    take H = Owned<struct int_list>(p);
+    take tl = IntList(H.tail);
+    return (Seq_Cons { head: H.head, tail: tl });
+  }
+}
+
+function [rec] (datatype seq) append(datatype seq xs, datatype seq ys) {
+  match xs {
+    Seq_Nil {} => {
+      ys
+    }
+    Seq_Cons {head : h, tail : zs}  => {
+      Seq_Cons {head: h, tail: append(zs, ys)}
+    }
+  }
+}
+
+function [rec] (datatype seq) snoc(datatype seq xs, i32 y) {
+  match xs {
+    Seq_Nil {} => {
+      Seq_Cons {head: y, tail: Seq_Nil{}}
+    }
+    Seq_Cons {head: x, tail: zs}  => {
+      Seq_Cons{head: x, tail: snoc (zs, y)}
+    }
+  }
+}
+
+function [rec] (datatype seq) rev(datatype seq xs) {
+  match xs {
+    Seq_Nil {} => {
+      Seq_Nil {}
+    }
+    Seq_Cons {head : h, tail : zs}  => {
+      snoc (rev(zs), h)
+    }
+  }
+}
+@*/
+
+struct int_list* IntList_rev_aux(struct int_list* xs, struct int_list* ys)
+  /*@ requires take L1 = IntList(xs); @*/
+  /*@ requires take L2 = IntList(ys); @*/
+  /*@ ensures take R = IntList(return); @*/
+  /*@ ensures R == append(rev(L2), L1); @*/
+{
+  if (ys == 0) {
+    return xs;
+  }
+  else {
+    struct int_list* tmp = ys->tail;
+    ys->tail = xs;
+    return IntList_rev_aux(ys, tmp);
+  }
+}
+
+struct int_list* IntList_rev(struct int_list* xs)
+  /*@ requires take L1 = IntList(xs); @*/
+  /*@ ensures take L1_rev = IntList(return); @*/
+  /*@ ensures L1_rev == rev(L1); @*/
+{
+  return IntList_rev_aux(0, xs);
+}

tests/run-cn-test-gen.sh
@@ -1,0 +1,83 @@
+#!/bin/bash
+
+# copying from run-ci.sh
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$(ocamlfind query z3)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(ocamlfind query z3)
+CN=$OPAM_SWITCH_PREFIX/bin/cn
+
+DIRNAME=$(dirname $0)
+
+# Clean directory
+cd $DIRNAME/cn-test-gen
+rm -rf test build
+mkdir test build
+
+# Get `*.c` files
+SUCC=$(find $DIRNAME/src -name '*.c')
+
+# Track failures
+NUM_FAILED=0
+FAILED=''
+
+# Test each `*.c` file
+for TEST in $SUCC; do
+  # Generate tests
+  $CN generate-tests $TEST --output-dir="test"
+  RET=$?
+  if [[ "$RET" != 0 ]]; then
+    echo "$TEST -- Error during test generation"
+    NUM_FAILED=$(($NUM_FAILED + 1))
+    FAILED="$FAILED $TEST"
+  else
+    echo "$TEST -- Tests generated successfully"
+
+    # Run CMake
+    cd build
+    cmake .. >/dev/null
+    RET=$?
+    if [[ "$RET" != 0 ]]; then
+      echo "$TEST -- Error while running CMake"
+      NUM_FAILED=$(($NUM_FAILED + 1))
+      FAILED="$FAILED $TEST"
+    else
+      echo "$TEST -- CMake run successfully"
+
+      # Build tests
+      make >/dev/null
+      RET=$?
+      if [[ "$RET" != 0 ]]; then
+        echo "$TEST -- Error while building tests"
+        NUM_FAILED=$(($NUM_FAILED + 1))
+        FAILED="$FAILED $TEST"
+      else
+        echo "$TEST -- Tests built successfully"
+
+        # Run tests
+        ./ci_tests >/dev/null
+        RET=$?
+        if [[ "$RET" != 0 ]]; then
+          echo "$TEST -- Error while running tests"
+          NUM_FAILED=$(($NUM_FAILED + 1))
+          FAILED="$FAILED $TEST"
+        else
+          echo "$TEST -- Tests run successfully"
+        fi
+      fi
+    fi
+    cd ..
+  fi
+  rm -rf test/*
+done
+
+echo
+echo 'Done running tests.'
+echo
+
+if [ -z "$FAILED" ]; then
+  echo "All tests passed."
+  exit 0
+else
+  echo "$NUM_FAILED tests failed:"
+  echo "  $FAILED"
+  exit 1
+fi

commit 461ce532b8419d0891e2d24d994705e978a46f72

CN: Fix and re-enable formatting
https://github.com/rems-project/cerberus/commit/c77d64541983966a50cc00e349b6627756d18024
accidentally deleted the formatting CI check and https://github.com/rems-project/cerberus/commit/d7b47b7417ab78af0d9e0dbbc590453b83bc5870
committed badly formatted code.

This commit fixes both of the above.

.github/workflows/ci.yml
@@ -126,6 +126,7 @@ jobs:
       run: |
         opam switch ${{ matrix.version }}
         eval $(opam env --switch=${{ matrix.version }})
+        USE_OPAM='' cd backend/cn && dune build @fmt
 
     - name: Install Cerberus-CHERI
       if: ${{ matrix.version == '4.14.1' }}

backend/cn/lib/check.ml
@@ -559,21 +559,21 @@ let rec check_pexpr (pe : BT.t mu_pexpr) (k : IT.t -> unit m) : unit m =
                 let@ model = model () in
                 let ub = CF.Undefined.UB045a_division_by_zero in
                 fail (fun ctxt -> { loc; msg = Undefined_behaviour { ub; ctxt; model } })))
-          | OpRem_t ->
-             let@ () = WellTyped.ensure_base_type loc ~expect (bt_of_pexpr pe1) in
-             let@ () = WellTyped.ensure_bits_type loc expect in
-             let@ () = WellTyped.ensure_bits_type loc (bt_of_pexpr pe2) in
-             check_pexpr pe1 (fun v1 ->
-              check_pexpr pe2 (fun v2 ->
-                let@ provable = provable loc in
-                let v2_bt = bt_of_pexpr pe2 in
-                let here = Locations.other __FUNCTION__ in
-                match provable (t_ (ne_ (v2, int_lit_ 0 v2_bt here) here)) with
-                | `True -> k (rem_ (v1, v2) loc)
-                | `False -> 
-                  let@ model = model () in
-                  let ub = CF.Undefined.UB045b_modulo_by_zero in
-                  fail (fun ctxt -> {loc; msg = Undefined_behaviour {ub; ctxt; model}})))
+        | OpRem_t ->
+          let@ () = WellTyped.ensure_base_type loc ~expect (bt_of_pexpr pe1) in
+          let@ () = WellTyped.ensure_bits_type loc expect in
+          let@ () = WellTyped.ensure_bits_type loc (bt_of_pexpr pe2) in
+          check_pexpr pe1 (fun v1 ->
+            check_pexpr pe2 (fun v2 ->
+              let@ provable = provable loc in
+              let v2_bt = bt_of_pexpr pe2 in
+              let here = Locations.other __FUNCTION__ in
+              match provable (t_ (ne_ (v2, int_lit_ 0 v2_bt here) here)) with
+              | `True -> k (rem_ (v1, v2) loc)
+              | `False ->
+                let@ model = model () in
+                let ub = CF.Undefined.UB045b_modulo_by_zero in
+                fail (fun ctxt -> { loc; msg = Undefined_behaviour { ub; ctxt; model } })))
         | OpEq ->
           let@ () = WellTyped.ensure_base_type loc ~expect Bool in
           let@ () =