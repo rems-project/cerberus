ccdd59830ed1f13dd9131a78bb2ffbb57d86d5ee
Dhruv Makwana <dc_mak@outlook.com>
2024-07-30 13:26:01 +0100

    CN: Strengthen & exec existing ptr_diff test

--- tests/cn/ptr_diff.c
+++ tests/cn/ptr_diff.c
@@ -1,14 +1,12 @@
-
-int
-f (int *p, int *q)
-/*@ requires q == array_shift(p, 10i32); @*/
+int f(int *p, int *q)
+/*@ requires ptr_eq(q, array_shift(p, 10i32));
+    ensures return == -10i32; @*/
 {
-  int x;
-  x = p - q; // intentionally p - q = -10
-
-  return x;
+  return p - q; // intentionally p - q = -10;
 }
 
 int main(void)
 {
+    int arr[11] = { 0 };
+    f(&arr[0], &arr[10]);
 }
