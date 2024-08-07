b67a95eebe23d8efa70705cfe15588fd0dfea952
Iavor Diatchki <iavor.diatchki@gmail.com>
2024-08-07 15:55:21 -0700

    Add support for right shift on signed types.
    Checkpoint.

--- backend/cn/lib/check.ml
+++ backend/cn/lib/check.ml
@@ -543,6 +543,9 @@ let rec check_pexpr (pe : BT.t mu_pexpr) (k : IT.t -> unit m) : unit m =
            fail (fun _ ->
              { loc; msg = Mismatch { has = BT.pp ty; expect = !^"comparable type" } })
        in
+       let not_yet x =
+           Pp.debug 1 (lazy (Pp.item "not yet restored" (Pp_mucore_ast.pp_pexpr orig_pe)));
+           failwith ("todo: " ^ x) in
        (match op with
         | OpDiv ->
           let@ () = WellTyped.ensure_base_type loc ~expect (bt_of_pexpr pe1) in
@@ -618,9 +621,12 @@ let rec check_pexpr (pe : BT.t mu_pexpr) (k : IT.t -> unit m) : unit m =
           let@ () = WellTyped.ensure_base_type loc ~expect:Bool (bt_of_pexpr pe1) in
           let@ () = WellTyped.ensure_base_type loc ~expect:Bool (bt_of_pexpr pe2) in
           check_pexpr pe1 (fun v1 -> check_pexpr pe2 (fun v2 -> k (or_ [ v1; v2 ] loc)))
-        | _ ->
-          Pp.debug 1 (lazy (Pp.item "not yet restored" (Pp_mucore_ast.pp_pexpr orig_pe)));
-          failwith ("todo: " ^ __FUNCTION__))
+        | OpAdd -> not_yet "OpAdd"
+        | OpSub -> not_yet "OpSub"
+        | OpMul -> not_yet "OpMul"
+        | OpRem_f -> not_yet "OpRem_f"
+        | OpExp -> not_yet "OpExp"
+        )
      | M_PEapply_fun (fun_id, args) ->
        let@ () =
          match mu_fun_return_type fun_id args with
@@ -693,10 +699,11 @@ let rec check_pexpr (pe : BT.t mu_pexpr) (k : IT.t -> unit m) : unit m =
        (* in integers, perform this op and round. in bitvector types, just perform
           the op (for all the ops where wrapping is consistent) *)
        let@ () = WellTyped.WCT.is_ct act.loc act.ct in
+       (*
        assert (
          match act.ct with
          | Integer ity when Sctypes.is_unsigned_integer_type ity -> true
-         | _ -> false);
+         | _ -> false); *)
        let@ () = ensure_base_type loc ~expect (Memory.bt_of_sct act.ct) in
        let@ () = ensure_base_type loc ~expect (bt_of_pexpr pe1) in
        let@ () = WellTyped.ensure_bits_type loc expect in
