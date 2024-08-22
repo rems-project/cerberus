f6f185ffae11456002e139919a0777ab0d3a9c82
James Parker <james@galois.com>
2024-08-22 16:10:02 -0400

    .

--- tests/run-ci-benchmarks.sh
+++ tests/run-ci-benchmarks.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-cat << EOF
+JSON_FILE="benchmark-data.json"
+
+cat << EOF >> ${JSON_FILE}
 [
+EOF
+
+DIRNAME=$(dirname "$0")
+
+SUCC=$(find "${DIRNAME}"/cn -name '*.c' | grep -v '\.error\.c')
+
+for TEST in ${SUCC}; do
+  # Record wall clock time in seconds
+  /usr/bin/time -f "%e" -o /tmp/time cn verify ${TEST}
+  TIME=$(cat /tmp/time)
+
+  # Hack to output JSON.
+  cat << EOF >> ${JSON_FILE}
     {
-        "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+	"name": "${TEST}",
         "unit": "Seconds",
-        "value": 500
+	"value": ${TIME}
     },
-    {
-        "name": "My Custom Smaller Is Better Benchmark - Memory Used",
-        "unit": "Megabytes",
-        "value": 100,
-        "range": "3",
-        "extra": "Value for Tooltip: 25\nOptional Num #2: 100\nAnything Else!"
-    }
+EOF
+done
+
+cat << EOF >> ${JSON_FILE}
 ]
 EOF
+
+cat ${JSON_FILE}
