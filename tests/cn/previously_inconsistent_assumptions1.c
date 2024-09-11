5a3d166149d434e53d4b440ca31d5fbf62505a5f
Christopher Pulte <cp526@cam.ac.uk>
2024-09-11 23:50:26 +0100

    Record @septract's tests that previously exposed errors in CN's checks for inconsistent assumptions.

---
+++ tests/cn/previously_inconsistent_assumptions1.c
@@ -0,0 +1,13 @@
+/*@ https://github.com/rems-project/cerberus/issues/566 *@/
+
+struct in_addr {
+    int s_addr;  // load with inet_aton()
+};
+ 
+ 
+extern int test(struct in_addr* addr);
+/*@
+  spec test(pointer addr);
+  requires take x =Owned<struct in_addr>(addr);
+  ensures take x2 =Owned<struct in_addr>(addr);
+@*/
