c6d2b5d9938c6887cc73fcfd2b2a03126b6fa805
Rini Banerjee <rb2018@cam.ac.uk>
2024-09-25 00:33:35 +0100

    expand record test

--- tests/cn/record1.c
+++ tests/cn/record1.c
@@ -3,6 +3,10 @@ function ({i32 x, i32 y}) increment(i32 x, i32 y) {
     {x: x + 1i32, y: y + 1i32}
 }
 
+function ({i32 x, i32 y}) decrement(i32 x, i32 y) {
+    {x: x - 1i32, y: y - 1i32}
+}
+
 @*/
 
 signed int incr_one(signed int x, signed int y, int flag) 
@@ -14,6 +18,16 @@ signed int incr_one(signed int x, signed int y, int flag)
     return (flag == 1) ? x + 1 : y + 1;
 }
 
+signed int decr_one(signed int x, signed int y, int flag) 
+/*@ requires x >= 0i32 && x < power(2i32, 31i32) - 1i32; 
+    requires y >= 0i32 && y < power(2i32, 31i32) - 1i32; @*/
+/*@ ensures let pair_record = decrement(x, y);
+    return == ((flag == 1i32) ? pair_record.x : pair_record.y); @*/
+{
+    return (flag == 1) ? x - 1 : y - 1;
+}
+
+
 
 int main(void) 
 /*@ trusted; @*/
@@ -22,5 +36,8 @@ int main(void)
     signed int y = 2;
     signed int r1 = incr_one(x, y, 1);
     signed int r2 = incr_one(x, y, 0);
+
+    signed int r3 = decr_one(x, y, 1);
+    signed int r4 = decr_one(x, y, 0);
     return 0;
 }
