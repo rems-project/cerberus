850df921ad31ef0cf5baf50f0bf02d12fb670b64
Dhruv Makwana <dc_mak@outlook.com>
2024-09-24 12:58:07 +0100

    CN VIP: Add live and bounds check to copy_alloc_id

--- tests/cn/copy_alloc_id.c
+++ tests/cn/copy_alloc_id.c
@@ -1,9 +1,27 @@
-void f (int *p)
+void f1 (int *p)
 /*@
 requires
+    take B = Block(p);
+ensures
+    take B2 = Block(p);
+@*/
+{
+  unsigned long long p_int = (unsigned long long) p;
+  int* q = __cerbvar_copy_alloc_id(p_int + 0ULL, p);
+  /*@ assert (ptr_eq(p, q)); @*/
+}
+
+void f2 (int *p)
+/*@
+requires
+    take A = Alloc(p);
+    A.base <= (u64) p;
+    (u64) p <= (u64) p + sizeof<int>;
+    (u64)p + sizeof<int> <= A.base + A.size;
     has_alloc_id(p);
 ensures
-    true;
+    take A2 = Alloc(p);
+    A == A2;
 @*/
 {
   unsigned long long p_int = (unsigned long long) p;
@@ -14,5 +32,7 @@ ensures
 int main(void)
 {
   int p[1] = {1};
-  f(p);
+  /*@ extract Owned<int>, 0u64; @*/
+  f1(p);
+  f2(p);
 }
