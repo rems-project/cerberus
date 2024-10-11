c95139d864d28fef6799479d580663f3c600e22c
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-11 06:25:27 -0400

    [CN-Test-Gen] Disable name mangling for builtins

--- backend/cn/lib/testGeneration/genUtils.ml
+++ backend/cn/lib/testGeneration/genUtils.ml
@@ -8,10 +8,13 @@ let names = ref []
 
 let get_mangled_name (syms : Sym.t list) : Sym.t =
   let open Pp in
-  match List.assoc_opt (List.equal Sym.equal) syms !names with
-  | Some sym -> sym
-  | None ->
-    let doc = string "cn_gen_" ^^ separate_map underscore Sym.pp syms in
-    let res_sym = Sym.fresh_named (CF.Pp_utils.to_plain_string doc) in
-    names := (syms, res_sym) :: !names;
-    res_sym
+  if GenBuiltins.is_builtin (List.hd syms) then
+    List.hd syms
+  else (
+    match List.assoc_opt (List.equal Sym.equal) syms !names with
+    | Some sym -> sym
+    | None ->
+      let doc = string "cn_gen_" ^^ separate_map underscore Sym.pp syms in
+      let res_sym = Sym.fresh_named (CF.Pp_utils.to_plain_string doc) in
+      names := (syms, res_sym) :: !names;
+      res_sym)
