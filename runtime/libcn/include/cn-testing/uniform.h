1c733bef97bde62ae10b0dd173265d8946e2fa77
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-11 06:24:56 -0400

    [CN-Test-Gen] Cleanup inequality generators

--- runtime/libcn/include/cn-testing/uniform.h
+++ runtime/libcn/include/cn-testing/uniform.h
@@ -5,6 +5,7 @@
 
 #include <cn-executable/utils.h>
 
+
 cn_bits_u8* cn_gen_uniform_cn_bits_u8(uint64_t sz);
 cn_bits_i8* cn_gen_uniform_cn_bits_i8(uint64_t sz);
 
@@ -16,6 +17,33 @@ cn_bits_i32* cn_gen_uniform_cn_bits_i32(uint64_t sz);
 
 cn_bits_u64* cn_gen_uniform_cn_bits_u64(uint64_t sz);
 cn_bits_i64* cn_gen_uniform_cn_bits_i64(uint64_t sz);
+
+
+cn_bits_u8* cn_gen_lt_cn_bits_u8(cn_bits_u8* max);
+cn_bits_i8* cn_gen_lt_cn_bits_i8(cn_bits_i8* max);
+
+cn_bits_u16* cn_gen_lt_cn_bits_u16(cn_bits_u16* max);
+cn_bits_i16* cn_gen_lt_cn_bits_i16(cn_bits_i16* max);
+
+cn_bits_u32* cn_gen_lt_cn_bits_u32(cn_bits_u32* max);
+cn_bits_i32* cn_gen_lt_cn_bits_i32(cn_bits_i32* max);
+
+cn_bits_u64* cn_gen_lt_cn_bits_u64(cn_bits_u64* max);
+cn_bits_i64* cn_gen_lt_cn_bits_i64(cn_bits_i64* max);
+
+
+cn_bits_u8* cn_gen_ge_cn_bits_u8(cn_bits_u8* min);
+cn_bits_i8* cn_gen_ge_cn_bits_i8(cn_bits_i8* min);
+
+cn_bits_u16* cn_gen_ge_cn_bits_u16(cn_bits_u16* min);
+cn_bits_i16* cn_gen_ge_cn_bits_i16(cn_bits_i16* min);
+
+cn_bits_u32* cn_gen_ge_cn_bits_u32(cn_bits_u32* min);
+cn_bits_i32* cn_gen_ge_cn_bits_i32(cn_bits_i32* min);
+
+cn_bits_u64* cn_gen_ge_cn_bits_u64(cn_bits_u64* min);
+cn_bits_i64* cn_gen_ge_cn_bits_i64(cn_bits_i64* min);
+
 
 cn_bits_u8* cn_gen_range_cn_bits_u8(cn_bits_u8* min, cn_bits_u8* max);
 cn_bits_i8* cn_gen_range_cn_bits_i8(cn_bits_i8* min, cn_bits_i8* max);
@@ -29,6 +57,7 @@ cn_bits_i32* cn_gen_range_cn_bits_i32(cn_bits_i32* min, cn_bits_i32* max);
 cn_bits_u64* cn_gen_range_cn_bits_u64(cn_bits_u64* min, cn_bits_u64* max);
 cn_bits_i64* cn_gen_range_cn_bits_i64(cn_bits_i64* min, cn_bits_i64* max);
 
+
 cn_bits_u8* cn_gen_mult_range_cn_bits_u8(cn_bits_u8* mul, cn_bits_u8* min, cn_bits_u8* max);
 cn_bits_i8* cn_gen_mult_range_cn_bits_i8(cn_bits_i8* mul, cn_bits_i8* min, cn_bits_i8* max);
 
@@ -41,6 +70,7 @@ cn_bits_i32* cn_gen_mult_range_cn_bits_i32(cn_bits_i32* mul, cn_bits_i32* min, c
 cn_bits_u64* cn_gen_mult_range_cn_bits_u64(cn_bits_u64* mul, cn_bits_u64* min, cn_bits_u64* max);
 cn_bits_i64* cn_gen_mult_range_cn_bits_i64(cn_bits_i64* mul, cn_bits_i64* min, cn_bits_i64* max);
 
+
 cn_bits_u8* cn_gen_mult_cn_bits_u8(cn_bits_u8* mul);
 cn_bits_i8* cn_gen_mult_cn_bits_i8(cn_bits_i8* mul);
 
@@ -52,5 +82,6 @@ cn_bits_i32* cn_gen_mult_cn_bits_i32(cn_bits_i32* mul);
 
 cn_bits_u64* cn_gen_mult_cn_bits_u64(cn_bits_u64* mul);
 cn_bits_i64* cn_gen_mult_cn_bits_i64(cn_bits_i64* mul);
+
 
 #endif // CN_UNIFORM_H
