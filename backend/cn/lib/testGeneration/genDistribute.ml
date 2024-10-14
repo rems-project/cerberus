683df61c4ce95264759e3a7fe08986ff182f2de4
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-13 23:00:48 -0400

    [CN-Test-Gen] Fix `map` typing bug
    [CN-Test-Gen] More typing fixes for `map`

--- backend/cn/lib/testGeneration/genDistribute.ml
+++ backend/cn/lib/testGeneration/genDistribute.ml
@@ -110,14 +110,13 @@ let pull_out_inner_generators (gt : GT.t) : GT.t =
            loc_asgn
        | GT (Let (y_backtracks', (y, gt3), gt4), _, loc_let') ->
          let z = Sym.fresh () in
-         let rename =
+         let gt4 =
            GT.subst
              (IT.make_subst [ (y, IT.sym_ (z, GT.bt gt3, Locations.other __LOC__)) ])
+             gt4
          in
          GT.let_
-           ( y_backtracks',
-             (z, gt3),
-             rename (GT.let_ (x_backtracks, (x, gt4), gt2) loc_let) )
+           (y_backtracks', (z, gt3), GT.let_ (x_backtracks, (x, gt4), gt2) loc_let)
            loc_let'
        | GT (Assert (lc, gt3), _, loc_assert) ->
          GT.assert_ (lc, GT.let_ (x_backtracks, (x, gt3), gt2) loc_let) loc_assert
