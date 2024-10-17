ac071b0457a8e616c7425f60f3d191572859257b
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-17 00:08:51 -0400

    [CN-Test-Gen] Move member indirection to optimization
    Allows it to catch new `memberof` operations from inlined functions.
    
    Also added support for records

--- backend/cn/lib/testGeneration/genOptimize.ml
+++ backend/cn/lib/testGeneration/genOptimize.ml
@@ -16,6 +16,154 @@ type opt_pass =
     transform : GT.t -> GT.t
   }
 
+module MemberIndirection = struct
+  type kind =
+    | Struct
+    | Record
+
+  let rec replace_memberof_it
+    (k : kind)
+    (sym : Sym.t)
+    (dict : (Id.t * Sym.t) list)
+    (it : IT.t)
+    : IT.t
+    =
+    let repl = replace_memberof_it k sym dict in
+    let (IT (it_, bt, loc)) = it in
+    let it_ =
+      match it_ with
+      | Const _ | Sym _ | SizeOf _ | OffsetOf _ | Nil _ -> it_
+      | Unop (op, it') -> IT.Unop (op, repl it')
+      | Binop (op, it1, it2) -> IT.Binop (op, repl it1, repl it2)
+      | ITE (it1, it2, it3) -> IT.ITE (repl it1, repl it2, repl it3)
+      | EachI ((min, (i_sym, i_bt), max), it') ->
+        IT.EachI ((min, (i_sym, i_bt), max), repl it')
+      | Tuple its -> IT.Tuple (List.map repl its)
+      | NthTuple (n, it') -> IT.NthTuple (n, repl it')
+      | Struct (tag, xits) -> IT.Struct (tag, List.map_snd repl xits)
+      | StructMember (it', x) ->
+        (match (k, IT.is_sym it') with
+         | Struct, Some (y, _y_bt) when Sym.equal y sym ->
+           IT.Sym (List.assoc Id.equal x dict)
+         | _ -> IT.StructMember (repl it', x))
+      | StructUpdate ((it_struct, x), it_val) ->
+        IT.StructUpdate ((repl it_struct, x), repl it_val)
+      | Record xits -> IT.Record (List.map_snd repl xits)
+      | RecordMember (it', x) ->
+        (match (k, IT.is_sym it') with
+         | Record, Some (y, _y_bt) when Sym.equal y sym ->
+           IT.Sym (List.assoc Id.equal x dict)
+         | _ -> IT.RecordMember (repl it', x))
+      | RecordUpdate ((it_record, x), it_val) ->
+        IT.RecordUpdate ((repl it_record, x), repl it_val)
+      | Constructor (tag, xits) -> IT.Constructor (tag, List.map_snd repl xits)
+      | MemberShift (it', tag, member) -> IT.MemberShift (it', tag, member)
+      | ArrayShift { base; ct; index } ->
+        IT.ArrayShift { base = repl base; ct; index = repl index }
+      | CopyAllocId { addr; loc } -> IT.CopyAllocId { addr = repl addr; loc = repl loc }
+      | HasAllocId it' -> IT.HasAllocId (repl it')
+      | Cons (it1, it2) -> IT.Cons (repl it1, repl it2)
+      | Head it' -> IT.Head (repl it')
+      | Tail it' -> IT.Tail (repl it')
+      | NthList (it1, it2, it3) -> IT.NthList (repl it1, repl it2, repl it3)
+      | ArrayToList (it1, it2, it3) -> IT.ArrayToList (repl it1, repl it2, repl it3)
+      | Representable (sct, it') -> IT.Representable (sct, repl it')
+      | Good (sct, it') -> IT.Good (sct, repl it')
+      | Aligned { t; align } -> IT.Aligned { t = repl t; align = repl align }
+      | WrapI (sct, it') -> IT.WrapI (sct, repl it')
+      | MapConst (bt, it') -> IT.MapConst (bt, repl it')
+      | MapSet (it1, it2, it3) -> IT.MapSet (repl it1, repl it2, repl it3)
+      | MapGet (it1, it2) -> IT.MapGet (repl it1, repl it2)
+      | MapDef ((x, bt), it') -> IT.MapDef ((x, bt), repl it')
+      | Apply (fsym, its) -> IT.Apply (fsym, List.map repl its)
+      | Let ((x, it1), it2) -> IT.Let ((x, repl it1), it2)
+      | Match (it', pits) -> IT.Match (repl it', List.map_snd repl pits)
+      | Cast (bt, it') -> IT.Cast (bt, repl it')
+    in
+    IT (it_, bt, loc)
+
+
+  let replace_memberof_gt
+    (k : kind)
+    (sym : Sym.t)
+    (dict : (Id.t * Sym.t) list)
+    (gt : GT.t)
+    : GT.t
+    =
+    let repl = replace_memberof_it k sym dict in
+    let aux (gt : GT.t) : GT.t =
+      let (GT (gt_, bt, loc)) = gt in
+      let gt_ =
+        match gt_ with
+        | Alloc it -> GT.Alloc (repl it)
+        | Call (fsym, xits) -> GT.Call (fsym, List.map_snd repl xits)
+        | Asgn ((it_addr, sct), it_val, gt') ->
+          GT.Asgn ((repl it_addr, sct), repl it_val, gt')
+        | Return it -> GT.Return (repl it)
+        | Assert (T it, gt') -> GT.Assert (LC.T (repl it), gt')
+        | Assert (Forall ((i_sym, i_bt), it), gt') ->
+          GT.Assert (LC.Forall ((i_sym, i_bt), repl it), gt')
+        | ITE (it_if, gt_then, gt_else) -> GT.ITE (repl it_if, gt_then, gt_else)
+        | Map ((i_sym, i_bt, it_perm), gt_inner) ->
+          GT.Map ((i_sym, i_bt, repl it_perm), gt_inner)
+        | _ -> gt_
+      in
+      GT (gt_, bt, loc)
+    in
+    GT.map_gen_pre aux gt
+
+
+  let transform (gt : GT.t) : GT.t =
+    let aux (gt : GT.t) : GT.t =
+      match gt with
+      | GT
+          ( Let
+              ( backtracks,
+                (x, (GT (Return (IT (Struct (_, xits), bt, _)), _, _) as gt_inner)),
+                gt' ),
+            _,
+            loc )
+      | GT
+          ( Let
+              ( backtracks,
+                (x, (GT (Return (IT (Record xits, bt, _)), _, _) as gt_inner)),
+                gt' ),
+            _,
+            loc ) ->
+        let k =
+          match bt with Struct _ -> Struct | Record _ -> Record | _ -> failwith __LOC__
+        in
+        let open Either in
+        let members =
+          xits
+          |> List.map_snd (fun it ->
+            match IT.is_sym it with
+            | Some (x, _) -> (Left (), x)
+            | None -> (Right it, Sym.fresh ()))
+        in
+        let gt_main =
+          GT.let_
+            ( backtracks,
+              (x, gt_inner),
+              replace_memberof_gt k x (List.map_snd snd members) gt' )
+            loc
+        in
+        let here = Locations.other __LOC__ in
+        members
+        |> List.map snd
+        |> List.filter_map (fun (info, x) ->
+          match info with Right it -> Some (x, it) | Left () -> None)
+        |> List.fold_left
+             (fun gt'' (x, it) -> GT.let_ (0, (x, GT.return_ it here), gt'') here)
+             gt_main
+      | _ -> gt
+    in
+    GT.map_gen_post aux gt
+
+
+  let pass = { name = "member_indirection"; transform }
+end
+
 (** This pass performs makes pointer offsets consistent *)
 module PointerOffsets = struct
   let transform (gt : GT.t) : GT.t =
@@ -1216,7 +1364,7 @@ module Specialization = struct
 end
 
 let all_passes (prog5 : unit Mucore.file) =
-  Inline.passes
+  (MemberIndirection.pass :: Inline.passes)
   @ RemoveUnused.passes
   @ [ TermSimplification.pass prog5; PointerOffsets.pass ]
   @ SplitConstraints.passes
