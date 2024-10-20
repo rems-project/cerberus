c6e3cb078dd490ac615a12ab21337dc082896f38
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-20 19:19:54 -0400

    [CN-Test-Gen] Assorted fixes

--- backend/cn/lib/testGeneration/genRuntime.ml
+++ backend/cn/lib/testGeneration/genRuntime.ml
@@ -194,7 +194,7 @@ let rec pp_term (tm : term) : Pp.document =
     ^^ space
     ^^ parens (IT.pp cond)
     ^^ space
-    ^^ braces (c_comment (BT.pp bt) ^^ nest 2 (break 1 ^^ pp_term t) ^^ break 1)
+    ^^ braces (nest 2 (break 1 ^^ c_comment (BT.pp bt) ^^ break 1 ^^ pp_term t) ^^ break 1)
     ^^ space
     ^^ string "else"
     ^^ space
