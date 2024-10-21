4a611be811df911caacdbec27e8de3a4678c16aa
Zain K Aamer <ZippeyKeys12@gmail.com>
2024-10-21 17:39:14 -0400

    [CN-Test-Gen] Split implications in assertions

--- backend/cn/lib/testGeneration/genOptimize.ml
+++ backend/cn/lib/testGeneration/genOptimize.ml
@@ -1507,7 +1507,25 @@ module SplitConstraints = struct
     let pass = { name; transform }
   end
 
-  let passes = [ Conjunction.pass; Let.pass; ITE.pass; Disjunction.pass ]
+  module Implication = struct
+    let name = "split_implication"
+
+    let transform (gt : GT.t) : GT.t =
+      let aux (gt : GT.t) : GT.t =
+        let (GT (gt_, _bt, loc)) = gt in
+        match gt_ with
+        | Assert (T (IT (Binop (Implies, it_if, it_then), _, loc_ite)), gt') ->
+          GT.ite_ (it_if, GT.assert_ (T it_then, gt') loc, gt') loc_ite
+        | _ -> gt
+      in
+      GT.map_gen_pre aux gt
+
+
+    let pass = { name; transform }
+  end
+
+  let passes =
+    [ Conjunction.pass; Let.pass; ITE.pass; Disjunction.pass; Implication.pass ]
 end
 
 (** This pass infers how much allocation is needed
