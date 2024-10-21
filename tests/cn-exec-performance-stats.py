f72d1ef054ce3bb19362216e47868084191fe85c
Rini Banerjee <rb2018@cam.ac.uk>
2024-10-21 20:26:14 +0100

    add CLI args to script

--- tests/cn-exec-performance-stats.py
+++ tests/cn-exec-performance-stats.py
@@ -3,15 +3,28 @@
 import subprocess
 from os import listdir
 from os.path import isfile, join
-import sys
+import argparse, sys
 import pandas as pd
 
-if len(sys.argv) != 2:
-    print("Please provide directory of CN files")
+parser=argparse.ArgumentParser()
+
+parser.add_argument("--dir", help="Collect performance metrics for *directory* of CN files")
+parser.add_argument("--file", help="Collect performance metrics for a *single* CN file")
+
+args=parser.parse_args()
+
+if (not args.dir and not args.file) or (args.dir and args.file):
+    print("Please provide *either* a standalone CN file *or* a directory path for several CN files")
     exit()
 
-tests_path = sys.argv[1]
-cn_test_files = [f for f in listdir(tests_path) if (isfile(join(tests_path, f)) and ".error" not in f)]
+
+cn_test_files=[]
+tests_path=""
+if args.dir:
+    tests_path = args.dir
+    cn_test_files = [f for f in listdir(tests_path) if (isfile(join(tests_path, f)) and ".error" not in f)]
+else:
+    cn_test_files=[args.file]
 
 # print(cn_test_files)
 instr_cmd_prefix = "time cn instrument"
