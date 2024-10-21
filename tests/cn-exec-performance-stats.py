d37e185810f4bd99d74aa09cf9099381ab1a68fd
Rini Banerjee <rb2018@cam.ac.uk>
2024-10-21 20:39:26 +0100

    Write to csv and add input filename to dataframe

--- tests/cn-exec-performance-stats.py
+++ tests/cn-exec-performance-stats.py
@@ -22,7 +22,7 @@ cn_test_files=[]
 tests_path=""
 if args.dir:
     tests_path = args.dir
-    cn_test_files = [f for f in listdir(tests_path) if (isfile(join(tests_path, f)) and ".error" not in f)]
+    cn_test_files = [f for f in listdir(tests_path) if (isfile(join(tests_path, f)) and ".broken" not in f and ".c" in f)]
 else:
     cn_test_files=[args.file]
 
@@ -31,24 +31,31 @@ instr_cmd_prefix = "time cn instrument"
 num_error_files=0
 
 generation_times=[]
+non_error_cn_filenames=[]
+
+print("Collecting performance metrics...")
 
 for f in cn_test_files:
-    print(tests_path + "/" + f)
+    # print(tests_path + "/" + f)
     full_instr_cmd = instr_cmd_prefix + " " + tests_path + "/" + f
     result = subprocess.run(full_instr_cmd.split(), capture_output=True, text = True)
     output = result.stderr
     if "error" not in output:
         generation_time = output.split()[0]
-        print(generation_time)
+        # print(generation_time)
         generation_times.append(generation_time)
+        non_error_cn_filenames.append(f)
     else:
         print("ERROR")
         num_error_files+=1
 
-stats_dict = {'generation_time': generation_times}
+print("...done!")
+
+stats_dict = {'cn_filename': non_error_cn_filenames, 'generation_time': generation_times}
 df = pd.DataFrame.from_dict(stats_dict)
 
 print(df)
+df.to_csv('times.csv', index=False) 
 
 print("Number of error files:")
 print(num_error_files)
