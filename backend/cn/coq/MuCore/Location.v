3b5afadfa0cd4720ec6d08adcc11cb83f4991997
Vadim Zaliva <lord@crocodile.org>
2024-12-19 16:55:28 -0800

    More MuCore and relted Cerberus types

--- backend/cn/coq/MuCore/Location.v
+++ backend/cn/coq/MuCore/Location.v
@@ -12,10 +12,11 @@ Inductive location_cursor :=
 | PointCursor: lexing_position -> location_cursor
 | RegionCursor: lexing_position -> lexing_position -> location_cursor.
 
-Inductive Locations_t :=
-| Loc_unknown: Locations_t
-| Loc_other: string -> Locations_t
-| Loc_point: lexing_position -> Locations_t
+Inductive Location_t :=
+| Loc_unknown: Location_t
+| Loc_other: string -> Location_t
+| Loc_point: lexing_position -> Location_t
 (* start, end, cursor *)
-| Loc_region: lexing_position -> lexing_position -> location_cursor -> Locations_t
-| Loc_regions: list (lexing_position * lexing_position) -> location_cursor -> Locations_t.
+| Loc_region: lexing_position -> lexing_position -> location_cursor -> Location_t
+| Loc_regions: list (lexing_position * lexing_position) -> location_cursor -> Location_t.
+
--- backend/cn/coq/MuCore/MuCore.v
+++ backend/cn/coq/MuCore/MuCore.v
@@ -4,8 +4,12 @@
 Require Import String.
 Require Import List.
 Require Import ZArith.
+Require Import Coq.FSets.FMapInterface.
+Require Import Coq.FSets.FMapList.
+Require Import Coq.Structures.OrderedTypeEx.
 
-Require Import MuCore.CerbTypes.
+Require Import Symbol.
+Require Import Location.
 
 (* We'll need to declare some types that are imported from other modules *)
 Parameter Annot_t : Type.
@@ -19,11 +23,20 @@ Parameter mem_iv_constraint : Type.
 Parameter binop : Type.
 Parameter iop : Type.
 Parameter undefined_behaviour : Type.
+Parameter LogicalConstraints_t : Type.
+Parameter cn_condition : Type.
+Parameter ReturnTypes_t : Type.
+Parameter ArgumentTypes_ft : Type.
+Parameter Memory_struct_layout : Type.
 
+(* Memory orders - we'll need to declare these as parameters since they come from other modules *)
+Parameter memory_order : Type.
+Parameter linux_memory_order : Type.
+Parameter polarity : Type.
 
 (* Annotated C type *)
 Record act := {
-  loc : Locations_t;
+  loc : Location_t;
   annot : list Annot_t;
   ct : Sctypes_t
 }.
@@ -71,7 +84,7 @@ Inductive pattern_ (TY : Type) : Type :=
   | CaseCtor : ctor -> list (pattern TY) -> pattern_ TY
 
 with pattern (TY : Type) : Type :=
-  | Pattern : Locations_t -> list Annot_t -> TY -> pattern_ TY -> pattern TY.
+  | Pattern : Location_t -> list Annot_t -> TY -> pattern_ TY -> pattern TY.
 
 (* Function types *)
 Inductive mu_function : Type :=
@@ -131,10 +144,160 @@ Inductive pexpr_ (TY: Type) : Type :=
   | PEcatch_exceptional_condition : act -> pexpr TY -> pexpr_ TY
   | PEbounded_binop : bound_kind -> iop -> pexpr TY -> pexpr TY -> pexpr_ TY
   | PEis_representable_integer : pexpr TY -> act -> pexpr_ TY
-  | PEundef : Locations_t -> undefined_behaviour -> pexpr_ TY
+  | PEundef : Location_t -> undefined_behaviour -> pexpr_ TY
   | PEerror : string -> pexpr TY -> pexpr_ TY
   | PElet : pattern TY -> pexpr TY -> pexpr TY -> pexpr_ TY
   | PEif : pexpr TY -> pexpr TY -> pexpr TY -> pexpr_ TY
 
 with pexpr (TY: Type) : Type :=
-  | Pexpr : Locations_t -> list Annot_t -> TY -> pexpr_ TY -> pexpr TY.
+  | Pexpr : Location_t -> list Annot_t -> TY -> pexpr_ TY -> pexpr TY.
+
+(* Action types *)
+Inductive action_ (TY : Type) : Type :=
+  | Create : pexpr TY -> act -> Sym_t -> action_ TY
+  | CreateReadOnly : pexpr TY -> act -> pexpr TY -> Sym_t -> action_ TY
+  | Alloc : pexpr TY -> pexpr TY -> Sym_t -> action_ TY
+  | Kill : m_kill_kind -> pexpr TY -> action_ TY
+  | Store : bool -> act -> pexpr TY -> pexpr TY -> memory_order -> action_ TY
+  | Load : act -> pexpr TY -> memory_order -> action_ TY
+  | RMW : act -> pexpr TY -> pexpr TY -> pexpr TY -> memory_order -> memory_order -> action_ TY
+  | Fence : memory_order -> action_ TY
+  | CompareExchangeStrong : 
+      act -> pexpr TY -> pexpr TY -> pexpr TY -> 
+      memory_order -> memory_order -> action_ TY
+  | CompareExchangeWeak :
+      act -> pexpr TY -> pexpr TY -> pexpr TY ->
+      memory_order -> memory_order -> action_ TY
+  | LinuxFence : linux_memory_order -> action_ TY
+  | LinuxLoad : act -> pexpr TY -> linux_memory_order -> action_ TY
+  | LinuxStore : act -> pexpr TY -> pexpr TY -> linux_memory_order -> action_ TY
+  | LinuxRMW : act -> pexpr TY -> pexpr TY -> linux_memory_order -> action_ TY.
+
+Record action (TY : Type) := {
+  action_loc : Location_t;
+  action_content : action_ TY
+}.
+
+Record paction (TY : Type) := {
+  paction_polarity : polarity;
+  paction_action : action TY
+}.
+
+(* Memory operations *)
+Inductive memop (TY : Type) : Type :=
+  | PtrEq : pexpr TY * pexpr TY -> memop TY
+  | PtrNe : pexpr TY * pexpr TY -> memop TY
+  | PtrLt : pexpr TY * pexpr TY -> memop TY
+  | PtrGt : pexpr TY * pexpr TY -> memop TY
+  | PtrLe : pexpr TY * pexpr TY -> memop TY
+  | PtrGe : pexpr TY * pexpr TY -> memop TY
+  | Ptrdiff : act * pexpr TY * pexpr TY -> memop TY
+  | IntFromPtr : act * act * pexpr TY -> memop TY
+  | PtrFromInt : act * act * pexpr TY -> memop TY
+  | PtrValidForDeref : act * pexpr TY -> memop TY
+  | PtrWellAligned : act * pexpr TY -> memop TY
+  | PtrArrayShift : pexpr TY * act * pexpr TY -> memop TY
+  | PtrMemberShift : Sym_t * Id_t * pexpr TY -> memop TY
+  | Memcpy : pexpr TY * pexpr TY * pexpr TY -> memop TY
+  | Memcmp : pexpr TY * pexpr TY * pexpr TY -> memop TY
+  | Realloc : pexpr TY * pexpr TY * pexpr TY -> memop TY
+  | Va_start : pexpr TY * pexpr TY -> memop TY
+  | Va_copy : pexpr TY -> memop TY
+  | Va_arg : pexpr TY * act -> memop TY
+  | Va_end : pexpr TY -> memop TY
+  | CopyAllocId : pexpr TY * pexpr TY -> memop TY.
+
+(* Expressions *)
+Inductive expr_ (TY : Type) : Type :=
+  | Epure : pexpr TY -> expr_ TY
+  | Ememop : memop TY -> expr_ TY
+  | Eaction : paction TY -> expr_ TY
+  | Eskip : expr_ TY
+  | Eccall : act * pexpr TY * list (pexpr TY) -> expr_ TY
+  | Elet : pattern TY * pexpr TY * expr TY -> expr_ TY
+  | Eunseq : list (expr TY) -> expr_ TY
+  | Ewseq : pattern TY * expr TY * expr TY -> expr_ TY
+  | Esseq : pattern TY * expr TY * expr TY -> expr_ TY
+  | Eif : pexpr TY * expr TY * expr TY -> expr_ TY
+  | Ebound : expr TY -> expr_ TY
+  | End : list (expr TY) -> expr_ TY
+  | Erun : Sym_t * list (pexpr TY) -> expr_ TY
+  (* Note: CN_progs constructor omitted as it requires additional types *)
+
+with expr (TY : Type) : Type :=
+  | Expr : Location_t -> list Annot_t -> TY -> expr_ TY -> expr TY.
+
+(* Global declarations and definitions *)
+Inductive globs (TY : Type) : Type :=
+  | GlobalDef : Sctypes_t -> expr TY -> globs TY
+  | GlobalDecl : Sctypes_t -> globs TY.
+
+(* Arguments list with logical constraints *)
+Inductive arguments (i : Type) : Type :=
+  | Define : (Sym_t * IndexTerms_t) * Location_t * arguments i -> arguments i
+  | Resource : (Sym_t * (Request_t * BaseTypes_t)) * Location_t * arguments i -> arguments i
+  | Constraint : LogicalConstraints_t * Location_t * arguments i -> arguments i
+  | I : i -> arguments i.
+
+(* Label specification *)
+Record parse_ast_label_spec := {
+  label_spec : list cn_condition  (* Note: cn_condition needs to be defined *)
+}.
+
+(* Label definition *)
+Inductive label_def (TY : Type) : Type :=
+  | Return : Location_t -> label_def TY
+  | Label : Location_t -> 
+           arguments (expr TY) ->
+           list Annot_t ->
+           parse_ast_label_spec ->
+           (Location_t * Location_t) -> (* Loop locations *)
+           label_def TY.
+
+(* Trusted status *)
+Inductive trusted : Type :=
+  | Trusted : Location_t -> trusted
+  | Checked : trusted.
+
+(* Desugared specification *)
+Record desugared_spec := {
+  accesses : list (Sym_t * Type); (* simplified from ctype *)
+  requires : list cn_condition;
+  ensures : list cn_condition
+}.
+
+(* Arguments and body *)
+Definition args_and_body (TY : Type) := 
+  arguments (expr TY * (SymMap.t (label_def TY)) * ReturnTypes_t).
+
+(* Function map declaration *)
+Inductive fun_map_decl (TY : Type) : Type :=
+  | Proc : Location_t ->
+           args_and_body TY ->
+           trusted ->
+           desugared_spec ->
+           fun_map_decl TY
+  | ProcDecl : Location_t -> option ArgumentTypes_ft -> fun_map_decl TY.
+
+(* Tag definition *)
+Inductive tag_definition : Type :=
+  | StructDef : Memory_struct_layout -> tag_definition
+  | UnionDef : tag_definition.
+
+(* Function to convert *)
+Record function_to_convert := {
+  ftc_loc : Location_t;
+  c_fun_sym : Sym_t;
+  l_fun_sym : Sym_t
+}.
+
+(* Datatype *)
+Record datatype := {
+  dt_loc : Location_t;
+  cases : list (Sym_t * list (Id_t * BaseTypes_t))
+}.
+
+(* Note: Some types like cn_condition, ReturnTypes_t, ArgumentTypes_ft, 
+   Memory_struct_layout will need to be defined or imported *)
+
+
