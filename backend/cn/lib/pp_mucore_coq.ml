4339638e47fd1c10155099229631f9256d599c30
Vadim Zaliva <lord@crocodile.org>
2024-12-30 16:49:58 -0800

    expr, body

--- backend/cn/lib/pp_mucore_coq.ml
+++ backend/cn/lib/pp_mucore_coq.ml
@@ -1,4 +1,4 @@
-[@@@warning "-32"]  (* Temporary disable unused value warnings *)
+[@@@warning "-32-27-26"]  (* Disable unused value warnings and unused variable warnings *)
 
 open Cerb_pp_prelude
 open Printf
@@ -22,6 +22,14 @@ let pp_undefined_behaviour _ = !^"undefined_behaviour_placeholder"
 let pp_memory_order _ = !^"memory_order_placeholder"
 let pp_linux_memory_order _ = !^"linux_memory_order_placeholder"
 let pp_polarity _ = !^"polarity_placeholder"
+let pp_cn_condition _ = !^"cn_condition_placeholder"
+let pp_return_type _ = !^"return_type_placeholder" 
+let pp_label_map _ = !^"label_map_placeholder"
+let pp_memop _ = !^"memop_placeholder"
+
+let pp_option pp_elem = function
+  | None -> !^"None"
+  | Some x -> !^"(Some" ^^^ pp_elem x ^^ !^")"
 
 (* Basic type printers *)
 let pp_lexing_position {Lexing.pos_fname; pos_lnum; pos_bol; pos_cnum} =
@@ -39,8 +47,12 @@ let pp_location_cursor = function
       !^"(RegionCursor" ^^^ pp_lexing_position start_pos ^^^
       pp_lexing_position end_pos ^^ !^")"
 
+(* temporary debug option to disable noisy locations *)
+let debug_print_locations = false  (* Set to true to print actual locations *)
+
 let pp_location = function
   | Cerb_location.Loc_unknown -> !^"Loc_unknown"
+  | _ when not debug_print_locations -> !^"Loc_unknown"
   | Cerb_location.Loc_other s -> !^"(Loc_other" ^^^ !^(sprintf "%S" s) ^^ !^")"
   | Cerb_location.Loc_point pos -> !^"(Loc_point" ^^^ pp_lexing_position pos ^^ !^")"
   | Cerb_location.Loc_region (start_pos, end_pos, cursor) ->
@@ -154,7 +166,7 @@ and pp_core_object_type = function
   | Mucore.Carray -> !^"Carray"
 
 (* Operator printers *)
-let pp_binop = function
+let pp_core_binop = function
   | Core.OpAdd -> !^"Add"
   | Core.OpSub -> !^"Sub"
   | Core.OpMul -> !^"Mul"
@@ -169,6 +181,40 @@ let pp_binop = function
   | Core.OpLe -> !^"Le"
   | Core.OpAnd -> !^"And"
   | Core.OpOr -> !^"Or"
+
+let pp_binop = function
+  | Terms.And -> !^"And"
+  | Terms.Or -> !^"Or"
+  | Terms.Implies -> !^"Implies"
+  | Terms.Add -> !^"Add"
+  | Terms.Sub -> !^"Sub"
+  | Terms.Mul -> !^"Mul"
+  | Terms.MulNoSMT -> !^"MulNoSMT"
+  | Terms.Div -> !^"Div"
+  | Terms.DivNoSMT -> !^"DivNoSMT"
+  | Terms.Exp -> !^"Exp"
+  | Terms.ExpNoSMT -> !^"ExpNoSMT"
+  | Terms.Rem -> !^"Rem"
+  | Terms.RemNoSMT -> !^"RemNoSMT"
+  | Terms.Mod -> !^"Mod"
+  | Terms.ModNoSMT -> !^"ModNoSMT"
+  | Terms.BW_Xor -> !^"BW_Xor"
+  | Terms.BW_And -> !^"BW_And"
+  | Terms.BW_Or -> !^"BW_Or"
+  | Terms.ShiftLeft -> !^"ShiftLeft"
+  | Terms.ShiftRight -> !^"ShiftRight"
+  | Terms.LT -> !^"LT"
+  | Terms.LE -> !^"LE"
+  | Terms.Min -> !^"Min"
+  | Terms.Max -> !^"Max"
+  | Terms.EQ -> !^"EQ"
+  | Terms.LTPointer -> !^"LTPointer"
+  | Terms.LEPointer -> !^"LEPointer"
+  | Terms.SetUnion -> !^"SetUnion"
+  | Terms.SetIntersection -> !^"SetIntersection"
+  | Terms.SetDifference -> !^"SetDifference"
+  | Terms.SetMember -> !^"SetMember"
+  | Terms.Subset -> !^"Subset"
 
 (* Action printers *)
 let rec pp_paction (Paction (pol, act)) =
@@ -193,7 +239,7 @@ and pp_pexpr (Pexpr (loc, annots, ty, pe)) =
    | PEctor (c, es) -> 
        !^"(PEctor" ^^^ pp_ctor c ^^^ pp_list pp_pexpr es ^^ !^")"
    | PEop (op, e1, e2) ->
-       !^"(PEop" ^^^ pp_binop op ^^^ pp_pexpr e1 ^^^ pp_pexpr e2 ^^ !^")"
+       !^"(PEop" ^^^ pp_core_binop op ^^^ pp_pexpr e1 ^^^ pp_pexpr e2 ^^ !^")"
    | _ -> !^"(* TODO: other pexpr cases *)")
 
 and pp_action_content = function
@@ -285,23 +331,272 @@ and pp_object_value (OV (ty, ov)) =
 (* Function specification printers *)
 let pp_ft ft = !^"dummy_ft"  (* TODO *)
 
-let pp_args_and_body args = !^"dummy_args"  (* TODO *)
+let pp_location_info (loc, _) = pp_location loc
+
 
 let pp_trusted = function
   | Trusted loc -> !^"(Trusted" ^^^ pp_location loc ^^ !^")"
   | Checked -> !^"Checked"
 
+
+   let pp_unop = function
+  | Terms.Not -> !^"Not"
+  | Negate -> !^"Negate"
+  | BW_CLZ_NoSMT -> !^"BW_CLZ_NoSMT"
+  | BW_CTZ_NoSMT -> !^"BW_CTZ_NoSMT"
+  | BW_FFS_NoSMT -> !^"BW_FFS_NoSMT"
+  | BW_FLS_NoSMT -> !^"BW_FLS_NoSMT"
+  | BW_Compl -> !^"BW_Compl"
+
+
+let pp_sign = function
+| BaseTypes.Signed -> !^"Signed"
+| BaseTypes.Unsigned -> !^"Unsigned"
+
+let rec pp_terms_pattern (Terms.Pat (pat, bt, loc)) =
+  !^"(Pat" ^^^
+  pp_terms_pattern_ pat ^^^
+  pp_basetype bt ^^^
+  pp_location loc ^^
+  !^")"
+and pp_terms_pattern_ = function
+  | Terms.PSym s -> !^"(PSym" ^^^ pp_symbol s ^^ !^")"
+  | Terms.PWild -> !^"PWild"
+  | Terms.PConstructor (sym, args) -> 
+      !^"(PConstructor" ^^^ pp_symbol sym ^^^
+      pp_list (fun (id, pat) -> !^"(" ^^ pp_identifier id ^^ !^"," ^^^ pp_terms_pattern pat ^^ !^")") args ^^ !^")"
+
+
+let rec pp_pattern_ = function
+  | CaseBase (sym_opt, bt) ->
+      !^"(CaseBase" ^^^ 
+      pp_option pp_symbol sym_opt ^^^
+      pp_core_base_type bt ^^ 
+      !^")"
+  | CaseCtor (ctor, pats) ->
+      !^"(CaseCtor" ^^^ 
+      pp_ctor ctor ^^^
+      pp_list pp_pattern pats ^^
+      !^")"
+
+and pp_pattern (Pattern (loc, annots, ty, pat)) =
+  !^"(Pattern" ^^^
+  pp_location loc ^^^
+  pp_list pp_annot_t annots ^^^
+  pp_type ty ^^^
+  pp_pattern_ pat ^^
+  !^")"
+
+let pp_const = function
+  | Terms.Z z -> !^"(Z" ^^^ !^(Z.to_string z) ^^ !^")"
+  | Bits ((sign, sz), z) -> !^"(Bits" ^^^ !^"(" ^^ pp_sign sign ^^ !^"," ^^^ !^(string_of_int sz) ^^ !^")" ^^^ !^(Z.to_string z) ^^ !^")"
+  | Q q -> !^"(Q" ^^^ !^(Q.to_string q) ^^ !^")"
+  | MemByte {alloc_id; value} -> !^"(MemByte" ^^^ !^(Z.to_string alloc_id) ^^^ !^(Z.to_string value) ^^ !^")"
+  | Pointer {alloc_id; addr} -> !^"(Pointer" ^^^ !^(Z.to_string alloc_id) ^^^ !^(Z.to_string addr) ^^ !^")"
+  | Alloc_id z -> !^"(Alloc_id" ^^^ !^(Z.to_string z) ^^ !^")"
+  | Bool b -> !^"(Bool" ^^^ pp_bool b ^^ !^")"
+  | Unit -> !^"Unit"
+  | Null -> !^"Null"
+  | CType_const t -> !^"(CType_const" ^^^ pp_type t ^^ !^")"
+  | Default bt -> !^"(Default" ^^^ pp_basetype bt ^^ !^")"
+
+   let rec pp_index_term (IndexTerms.IT (term, bt, loc)) =
+    !^"(IT" ^^^
+    pp_index_term_content term ^^^
+    pp_basetype bt ^^^
+    pp_location loc ^^
+    !^")"
+  
+  and pp_index_term_content = function
+    | IndexTerms.Const c -> !^"(Const" ^^^ pp_const c ^^ !^")"
+    | Sym s -> !^"(Sym" ^^^ pp_symbol s ^^ !^")"
+    | Unop (op, t) -> !^"(Unop" ^^^ pp_unop op ^^^ pp_index_term t ^^ !^")"
+    | Binop (op, t1, t2) -> 
+        !^"(Binop" ^^^ pp_binop op ^^^ pp_index_term t1 ^^^ pp_index_term t2 ^^ !^")"
+    | ITE (c, t, e) ->
+        !^"(ITE" ^^^ pp_index_term c ^^^ pp_index_term t ^^^ pp_index_term e ^^ !^")"
+    | EachI ((n1, (sym, bt), n2), t) ->
+        !^"(EachI" ^^^ !^"(" ^^^
+        !^(string_of_int n1) ^^ !^"," ^^^
+        !^"(" ^^ pp_symbol sym ^^ !^"," ^^^ pp_basetype bt ^^ !^")" ^^ !^"," ^^^
+        !^(string_of_int n2) ^^ !^")" ^^^
+        pp_index_term t ^^ !^")"
+    | Tuple ts -> !^"(Tuple" ^^^ pp_list pp_index_term ts ^^ !^")"
+    | NthTuple (n, t) -> !^"(NthTuple" ^^^ !^(string_of_int n) ^^^ pp_index_term t ^^ !^")"
+    | Struct (tag, members) ->
+        !^"(Struct" ^^^ pp_symbol tag ^^^
+        pp_list (fun (id, t) -> !^"(" ^^ pp_identifier id ^^ !^"," ^^^ pp_index_term t ^^ !^")") members ^^ !^")"
+    | StructMember (t, member) ->
+        !^"(StructMember" ^^^ pp_index_term t ^^^ pp_identifier member ^^ !^")"
+    | StructUpdate ((t1, member), t2) ->
+        !^"(StructUpdate" ^^^ !^"(" ^^^ pp_index_term t1 ^^ !^"," ^^^ pp_identifier member ^^ !^")" ^^^
+        pp_index_term t2 ^^ !^")"
+    | Record members ->
+        !^"(Record" ^^^
+        pp_list (fun (id, t) -> !^"(" ^^ pp_identifier id ^^ !^"," ^^^ pp_index_term t ^^ !^")") members ^^ !^")"
+    | RecordMember (t, member) ->
+        !^"(RecordMember" ^^^ pp_index_term t ^^^ pp_identifier member ^^ !^")"
+    | RecordUpdate ((t1, member), t2) ->
+        !^"(RecordUpdate" ^^^ !^"(" ^^^ pp_index_term t1 ^^ !^"," ^^^ pp_identifier member ^^ !^")" ^^^
+        pp_index_term t2 ^^ !^")"
+    | Constructor (sym, args) ->
+        !^"(Constructor" ^^^ pp_symbol sym ^^^
+        pp_list (fun (id, t) -> !^"(" ^^ pp_identifier id ^^ !^"," ^^^ pp_index_term t ^^ !^")") args ^^ !^")"
+    | MemberShift (t, tag, id) ->
+        !^"(MemberShift" ^^^ pp_index_term t ^^^ pp_symbol tag ^^^ pp_identifier id ^^ !^")"
+    | ArrayShift {base; ct; index} ->
+        !^"(ArrayShift" ^^^ pp_index_term base ^^^ pp_sctypes_t ct ^^^ pp_index_term index ^^ !^")"
+    | CopyAllocId {addr; loc} ->
+        !^"(CopyAllocId" ^^^ pp_index_term addr ^^^ pp_index_term loc ^^ !^")"
+    | HasAllocId t -> !^"(HasAllocId" ^^^ pp_index_term t ^^ !^")"
+    | SizeOf ct -> !^"(SizeOf" ^^^ pp_sctypes_t ct ^^ !^")"
+    | OffsetOf (tag, member) ->
+        !^"(OffsetOf" ^^^ pp_symbol tag ^^^ pp_identifier member ^^ !^")"
+    | Nil bt -> !^"(Nil" ^^^ pp_basetype bt ^^ !^")"
+    | Cons (t1, t2) -> !^"(Cons" ^^^ pp_index_term t1 ^^^ pp_index_term t2 ^^ !^")"
+    | Head t -> !^"(Head" ^^^ pp_index_term t ^^ !^")"
+    | Tail t -> !^"(Tail" ^^^ pp_index_term t ^^ !^")"
+    | NthList (i, xs, d) ->
+        !^"(NthList" ^^^ pp_index_term i ^^^ pp_index_term xs ^^^ pp_index_term d ^^ !^")"
+    | ArrayToList (arr, i, len) ->
+        !^"(ArrayToList" ^^^ pp_index_term arr ^^^ pp_index_term i ^^^ pp_index_term len ^^ !^")"
+    | Representable (ct, t) ->
+        !^"(Representable" ^^^ pp_sctypes_t ct ^^^ pp_index_term t ^^ !^")"
+    | Good (ct, t) -> !^"(Good" ^^^ pp_sctypes_t ct ^^^ pp_index_term t ^^ !^")"
+    | Aligned {t; align} ->
+        !^"(Aligned" ^^^ pp_index_term t ^^^ pp_index_term align ^^ !^")"
+    | WrapI (ct, t) -> !^"(WrapI" ^^^ pp_sctypes_t ct ^^^ pp_index_term t ^^ !^")"
+    | MapConst (bt, t) -> !^"(MapConst" ^^^ pp_basetype bt ^^^ pp_index_term t ^^ !^")"
+    | MapSet (m, k, v) ->
+        !^"(MapSet" ^^^ pp_index_term m ^^^ pp_index_term k ^^^ pp_index_term v ^^ !^")"
+    | MapGet (m, k) -> !^"(MapGet" ^^^ pp_index_term m ^^^ pp_index_term k ^^ !^")"
+    | MapDef ((sym, bt), t) ->
+        !^"(MapDef" ^^^ !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^ pp_basetype bt ^^ !^")" ^^^
+        pp_index_term t ^^ !^")"
+    | Apply (sym, args) ->
+        !^"(Apply" ^^^ pp_symbol sym ^^^ pp_list pp_index_term args ^^ !^")"
+    | Let ((sym, t1), t2) ->
+        !^"(Let" ^^^ !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^ pp_index_term t1 ^^ !^")" ^^^
+        pp_index_term t2 ^^ !^")"
+    | Match (t, cases) ->
+        !^"(Match" ^^^ pp_index_term t ^^^
+        pp_list (fun (pat, body) -> !^"(" ^^ pp_terms_pattern pat ^^ !^"," ^^^ pp_index_term body ^^ !^")") cases ^^ !^")"
+    | Cast (bt, t) -> !^"(Cast" ^^^ pp_basetype bt ^^^ pp_index_term t ^^ !^")"
+
+
+    let pp_request_init = function
+    | Request.Init -> !^"Init"
+    | Request.Uninit -> !^"Uninit"
+  let rec pp_request = function
+    | Request.P pred ->
+        !^"(P" ^^^
+        !^"{|" ^^^
+        !^"name :=" ^^^ pp_request_name pred.Request.Predicate.name ^^ !^";" ^^^
+        !^"pointer :=" ^^^ pp_index_term pred.pointer ^^ !^";" ^^^
+        !^"iargs :=" ^^^ pp_list pp_index_term pred.iargs ^^^
+        !^"|}" ^^
+        !^")"
+    | Request.Q qpred ->
+        !^"(Q" ^^^
+        !^"{|" ^^^
+        !^"name :=" ^^^ pp_request_name qpred.Request.QPredicate.name ^^ !^";" ^^^
+        !^"pointer :=" ^^^ pp_index_term qpred.pointer ^^ !^";" ^^^
+        !^"q :=" ^^^ !^"(" ^^ pp_symbol (fst qpred.q) ^^ !^"," ^^^ pp_basetype (snd qpred.q) ^^ !^")" ^^ !^";" ^^^
+        !^"q_loc :=" ^^^ pp_location qpred.q_loc ^^ !^";" ^^^
+        !^"step :=" ^^^ pp_index_term qpred.step ^^ !^";" ^^^
+        !^"permission :=" ^^^ pp_index_term qpred.permission ^^ !^";" ^^^
+        !^"iargs :=" ^^^ pp_list pp_index_term qpred.iargs ^^^
+        !^"|}" ^^
+        !^")"
+  and pp_request_name = function
+    | Request.PName sym -> !^"(PName" ^^^ pp_symbol sym ^^ !^")"
+    | Request.Owned (ct, init) -> 
+      (* TODO
+        !^"(Owned" ^^^ pp_sctypes_t ct ^^^ pp_request_init init ^^ !^")"
+        *)  
+        P.empty
+  
 let rec pp_expr (Expr (loc, annots, ty, e)) =
-  !^"Expr" ^^^ pp_location loc ^^^
-  pp_list pp_annot_t annots ^^^ pp_type ty ^^^
-  (match e with
-   | Epure pe -> !^"(Epure" ^^^ pp_pexpr pe ^^ !^")"
-   | Eaction pa -> !^"(Eaction" ^^^ pp_paction pa ^^ !^")"
-   | Eskip -> !^"Eskip"
-   | Eif (c, t, e) ->
-       !^"(Eif" ^^^ pp_pexpr c ^^^ pp_expr t ^^^ pp_expr e ^^ !^")"
-   (* Add other cases following same pattern *)
-   | _ -> !^"(* TODO: other expr cases *)")
+          !^"Expr" ^^^ pp_location loc ^^^
+          pp_list pp_annot_t annots ^^^ pp_type ty ^^^
+          (match e with
+           | Epure pe -> !^"(Epure" ^^^ pp_pexpr pe ^^ !^")"
+           | Ememop m -> !^"(Ememop" ^^^ pp_memop m ^^ !^")"
+           | Eaction pa -> !^"(Eaction" ^^^ pp_paction pa ^^ !^")"
+           | Eskip -> !^"Eskip"
+           | Eccall (act, f, args) ->
+               !^"(Eccall" ^^^ pp_act act ^^^ pp_pexpr f ^^^ pp_list pp_pexpr args ^^ !^")"
+           | Elet (pat, e1, e2) ->
+               !^"(Elet" ^^^ pp_pattern pat ^^^ pp_pexpr e1 ^^^ pp_expr e2 ^^ !^")"
+           | Eunseq exprs ->
+               !^"(Eunseq" ^^^ pp_list pp_expr exprs ^^ !^")"
+           | Ewseq (pat, e1, e2) ->
+               !^"(Ewseq" ^^^ pp_pattern pat ^^^ pp_expr e1 ^^^ pp_expr e2 ^^ !^")"
+           | Esseq (pat, e1, e2) ->
+               !^"(Esseq" ^^^ pp_pattern pat ^^^ pp_expr e1 ^^^ pp_expr e2 ^^ !^")"
+           | Eif (c, t, e) ->
+               !^"(Eif" ^^^ pp_pexpr c ^^^ pp_expr t ^^^ pp_expr e ^^ !^")"
+           | Ebound e ->
+               !^"(Ebound" ^^^ pp_expr e ^^ !^")"
+           | End exprs ->
+               !^"(End" ^^^ pp_list pp_expr exprs ^^ !^")"
+           | Erun (sym, args) ->
+               !^"(Erun" ^^^ pp_symbol sym ^^^ pp_list pp_pexpr args ^^ !^")"
+           | CN_progs (stmts, progs) ->
+              (* TODO: this constructor was omitted from the original code *)
+              P.empty)
+            
+        
+let pp_logical_constraint = function
+        | LogicalConstraints.T term -> 
+            !^"(T" ^^^ pp_index_term term ^^ !^")"
+        | LogicalConstraints.Forall ((sym, bt), term) ->
+            !^"(Forall" ^^^ !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^ pp_basetype bt ^^ !^")" ^^^
+            pp_index_term term ^^ !^")"
+      
+   let pp_args_and_body args =
+    (* args is of type arguments (expr * label_map * return_type) *)
+    let rec pp_args = function
+      | Computational ((sym, bt), loc, rest) ->
+          !^"(Computational" ^^^
+          !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^ pp_basetype bt ^^ !^")" ^^^
+          pp_location_info loc ^^^
+          pp_args rest ^^ !^")"
+      | L logical_args -> pp_logical_args logical_args
+  
+    and pp_logical_args = function
+      | Define ((sym, term), info, rest) ->
+          !^"(Define" ^^^
+          !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^ pp_index_term term ^^ !^")" ^^^
+          pp_location_info info ^^^
+          pp_logical_args rest ^^ !^")"
+      | Resource ((sym, (req, bt)), info, rest) ->
+          !^"(Resource" ^^^
+          !^"(" ^^^ pp_symbol sym ^^ !^"," ^^^
+          !^"(" ^^^ pp_request req ^^ !^"," ^^^ pp_basetype bt ^^ !^"))" ^^^
+          pp_location_info info ^^^
+          pp_logical_args rest ^^ !^")"
+      | Constraint (lc, info, rest) ->
+          !^"(Constraint" ^^^
+          pp_logical_constraint lc ^^^
+          pp_location_info info ^^^
+          pp_logical_args rest ^^ !^")"
+      | I (body, labels, rt) ->
+          !^"(I" ^^^
+          !^"(" ^^^
+          pp_expr body ^^ !^"," ^^^
+          pp_label_map labels ^^ !^"," ^^^
+          pp_return_type rt ^^
+          !^"))"
+    in
+    pp_args args
+
+let pp_desugared_spec {accesses; requires; ensures} =
+  !^"{|" ^^^
+  !^"accesses :=" ^^^ pp_list (fun (sym, ty) -> !^"(" ^^ pp_symbol sym ^^ !^"," ^^^ pp_type ty ^^ !^")") accesses ^^ !^";" ^^^
+  !^"requires :=" ^^^ pp_list pp_cn_condition requires ^^ !^";" ^^^
+  !^"ensures :=" ^^^ pp_list pp_cn_condition ensures ^^^
+  !^"|}"
 
 (* Top-level file printer *)
 let pp_file file =
@@ -326,15 +621,21 @@ let pp_file file =
   Pmap.fold (fun sym decl acc ->
     acc ^^
     match decl with
+    (* TODO: handle ProcDecl *)
     | ProcDecl (loc, ft) ->
-        coq_def (Pp_symbol.to_string sym) P.empty
+      (*
+        coq_def (Pp_symbol.to_string_pretty_cn sym) P.empty
           (!^"ProcDecl" ^^^ pp_location loc ^^^ 
            match ft with None -> !^"None" | Some ft -> !^"(Some" ^^^ pp_ft ft ^^ !^")")
-    | Proc {loc; args_and_body; trusted} ->
-        coq_def (Pp_symbol.to_string sym) P.empty
+      *) 
+      P.empty
+    | Proc {loc; args_and_body; trusted; desugared_spec} ->
+        coq_def (Pp_symbol.to_string_pretty_cn sym) P.empty
           (!^"Proc" ^^^ pp_location loc ^^^ pp_args_and_body args_and_body ^^^
-           pp_trusted trusted)
+           pp_trusted trusted ^^^ pp_desugared_spec desugared_spec)
   ) file.funs P.empty
 
 let pp_file_string file =
   Pp_utils.to_plain_string (pp_file file) 
+
+
