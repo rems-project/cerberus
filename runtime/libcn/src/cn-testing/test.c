f62118800277b138a1c65a822116764bb85168fd
Zain K Aamer <ZippeyKeys12@gmail.com>
2025-01-30 01:36:22 -0500

    [CN-Test-Gen] Make trapping more portable

--- runtime/libcn/src/cn-testing/test.c
+++ runtime/libcn/src/cn-testing/test.c
@@ -52,13 +52,46 @@ void print_test_info(const char* suite, const char* name, int tests, int discard
     fflush(stdout);
 }
 
-#if defined(__has_builtin) && __has_builtin(__builtin_debugtrap)
-#define _cn_trap() __builtin_debugtrap()
-#elif defined(_MSC_VER) || (defined(__has_builtin) && __has_builtin(__debugbreak))
-#define _cn_trap() __debugbreak()
+#if defined(__has_builtin) && !defined(__ibmxl__) && __has_builtin(__builtin_debugtrap)
+#  define _cn_trap() __builtin_debugtrap()
+#elif defined(__has_builtin) && !defined(__ibmxl__) && __has_builtin(__debugbreak)
+#  define _cn_trap() __debugbreak()
+#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#  define _cn_trap() __debugbreak()
+#elif defined(__ARMCC_VERSION)
+#  define _cn_trap() __breakpoint(42)
+#elif defined(__ibmxl__) || defined(__xlC__)
+#  include <builtins.h>
+#  define _cn_trap() __trap(42)
+#elif defined(__DMC__) && defined(_M_IX86)
+static inline void _cn_trap(void) { __asm int 3h; }
+#elif defined(__i386__) || defined(__x86_64__)
+static inline void _cn_trap(void) { __asm__ __volatile__("int3"); }
+#elif defined(__thumb__)
+static inline void _cn_trap(void) { __asm__ __volatile__(".inst 0xde01"); }
+#elif defined(__aarch64__)
+static inline void _cn_trap(void) { __asm__ __volatile__(".inst 0xd4200000"); }
+#elif defined(__arm__)
+static inline void _cn_trap(void) { __asm__ __volatile__(".inst 0xe7f001f0"); }
+#elif defined (__alpha__) && !defined(__osf__)
+static inline void _cn_trap(void) { __asm__ __volatile__("bpt"); }
+#elif defined(_54_)
+static inline void _cn_trap(void) { __asm__ __volatile__("ESTOP"); }
+#elif defined(_55_)
+static inline void _cn_trap(void) { __asm__ __volatile__(";\n .if (.MNEMONIC)\n ESTOP_1\n .else\n ESTOP_1()\n .endif\n NOP"); }
+#elif defined(_64P_)
+static inline void _cn_trap(void) { __asm__ __volatile__("SWBP 0"); }
+#elif defined(_6x_)
+static inline void _cn_trap(void) { __asm__ __volatile__("NOP\n .word 0x10000000"); }
+#elif defined(__STDC_HOSTED__) && (__STDC_HOSTED__ == 0) && defined(__GNUC__)
+#    define _cn_trap() __builtin_trap()
 #else
-#include <signal.h>
-#define _cn_trap() raise(SIGTRAP)
+#  include <signal.h>
+#  if defined(SIGTRAP)
+#    define _cn_trap() raise(SIGTRAP)
+#  else
+#    define _cn_trap() raise(SIGABRT)
+#  endif
 #endif
 
 void cn_trap(void) { _cn_trap(); }
