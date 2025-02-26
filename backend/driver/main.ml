4ad89ad8e0cab16eb476dfc47079d24180b0642f
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2025-02-26 18:45:04 +0000

    (backends) removing unqualified uses of Cerb_runtime functions

--- backend/driver/main.ml
+++ backend/driver/main.ml
@@ -1,7 +1,6 @@
 open Cerb_frontend
 open Cerb_backend
 open Cerb_global
-open Cerb_runtime
 open Pipeline
 
 let (>>=) = Exception.except_bind
@@ -37,7 +36,7 @@ let frontend (conf, io) ~is_lib filename core_std =
                       "The file extention is not supported")
 
 let create_cpp_cmd cpp_cmd nostdinc macros_def macros_undef incl_dirs incl_files nolibc =
-  let libc_dirs = [in_runtime "bmc"; in_runtime "libc/include"; in_runtime "libc/include/posix"] in
+  let libc_dirs = Cerb_runtime.[in_runtime "bmc"; in_runtime "libc/include"; in_runtime "libc/include/posix"] in
   let incl_dirs = if nostdinc then incl_dirs else libc_dirs @ incl_dirs in
   let macros_def = if nolibc then macros_def else ("CERB_WITH_LIB", None) :: macros_def in
   let macros_def = if is_cheri_memory () then ("__CHERI__", None) :: macros_def else macros_def in
@@ -49,11 +48,11 @@ let create_cpp_cmd cpp_cmd nostdinc macros_def macros_undef incl_dirs incl_files
       ) macros_def @
     List.map (fun str -> "-U" ^ str) macros_undef @
     List.map (fun str -> "-I" ^ str) incl_dirs @
-    List.map (fun str -> "-include " ^ str) (in_runtime "libc/include/builtins.h" :: incl_files)
+    List.map (fun str -> "-include " ^ str) (Cerb_runtime.in_runtime "libc/include/builtins.h" :: incl_files)
   end
 
 let core_libraries incl lib_paths libs =
-  let lib_paths = if incl then in_runtime "libc" :: lib_paths else lib_paths in
+  let lib_paths = if incl then Cerb_runtime.in_runtime "libc" :: lib_paths else lib_paths in
   let libs =
     if incl then
       if Switches.is_CHERI () then
