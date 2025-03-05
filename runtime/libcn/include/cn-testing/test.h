rems-project/cerberus

commit ba4a38312f8e8470a628f47b2ede406d9f5ca130

[CN-Test-Gen] More test case sizing strategies (#871)

runtime/libcn/include/cn-testing/test.h
@@ -2,6 +2,7 @@
 #define CN_TEST_H
 
 #include <setjmp.h>
+#include <stdbool.h>
 #include <cn-executable/utils.h>
 #include <cn-testing/uniform.h>
 #include <cn-testing/result.h>
@@ -12,7 +13,13 @@ enum cn_test_gen_progress {
     CN_TEST_GEN_PROGRESS_ALL = 2
 };
 
-typedef enum cn_test_result cn_test_case_fn(enum cn_test_gen_progress, int);
+enum cn_gen_size_strategy {
+    CN_GEN_SIZE_UNIFORM = 0,
+    CN_GEN_SIZE_QUARTILE = 1,
+    CN_GEN_SIZE_QUICKCHECK = 2
+};
+
+typedef enum cn_test_result cn_test_case_fn(bool replay, enum cn_test_gen_progress, enum cn_gen_size_strategy, bool trap);
 
 void cn_register_test_case(const char* suite, const char* name, cn_test_case_fn* func);
 
@@ -20,6 +27,8 @@ void print_test_info(const char* suite, const char* name, int tests, int discard
 
 /** This function is called right before rerunning a failing test case. */
 void cn_trap(void);
+
+size_t cn_gen_compute_size(enum cn_gen_size_strategy strategy, int max_tests, size_t max_size, int max_discard_ratio, int successes, int recent_discards);
 
 #define CN_UNIT_TEST_CASE_NAME(FuncName) cn_test_const_##FuncName
 
@@ -30,7 +39,12 @@ void cn_trap(void);
         longjmp(buf_##FuncName, 1);                                                     \
     }                                                                                   \
                                                                                         \
-    enum cn_test_result cn_test_const_##FuncName () {                                   \
+    enum cn_test_result cn_test_const_##FuncName (                                      \
+        bool replay,                                                                    \
+        enum cn_test_gen_progress progress_level,                                       \
+        enum cn_gen_size_strategy size_strategy,                                        \
+        bool trap                                                                       \
+    ) {                                                                                 \
         if (setjmp(buf_##FuncName)) {                                                   \
             return CN_TEST_FAIL;                                                        \
         }                                                                               \
@@ -56,11 +70,13 @@ void cn_trap(void);
     }                                                                                   \
                                                                                         \
     enum cn_test_result cn_test_gen_##Name (                                            \
+        bool replay,                                                                    \
         enum cn_test_gen_progress progress_level,                                       \
-        int trap                                                                        \
+        enum cn_gen_size_strategy size_strategy,                                        \
+        bool trap                                                                       \
     ) {                                                                                 \
         cn_gen_rand_checkpoint checkpoint = cn_gen_rand_save();                         \
-        int i = 0, d = 0;                                                               \
+        int i = 0, d = 0, recentDiscards = 0;                                           \
         set_cn_failure_cb(&cn_test_gen_##Name##_fail);                                  \
         switch (setjmp(buf_##Name)) {                                                   \
             case CN_FAILURE_ASSERT:                                                     \
@@ -73,6 +89,7 @@ void cn_trap(void);
             case CN_FAILURE_ALLOC:                                                      \
                 cn_gen_rand_replace(checkpoint);                                        \
                 d++;                                                                    \
+                recentDiscards++;                                                       \
                 break;                                                                  \
         }                                                                               \
         for (; i < Samples; i++) {                                                      \
@@ -86,15 +103,28 @@ void cn_trap(void);
                 }                                                                       \
                 return CN_TEST_GEN_FAIL;                                                \
             }                                                                           \
+            if (!replay) {                                                              \
+                cn_gen_set_size(cn_gen_compute_size(                                    \
+                    size_strategy,                                                      \
+                    Samples,                                                            \
+                    cn_gen_get_max_size(),                                              \
+                    10,                                                                 \
+                    i,                                                                  \
+                    recentDiscards                                                      \
+                ));                                                                     \
+            }                                                                           \
             CN_TEST_INIT();                                                             \
-            size_t sz = cn_gen_uniform_cn_bits_u16(cn_gen_get_max_size())->val + 1;     \
-            cn_gen_set_size(sz);                                                        \
-            cn_gen_set_input_timer(cn_gen_get_milliseconds());                          \
+            if (!replay) {                                                              \
+                cn_gen_set_input_timer(cn_gen_get_milliseconds());                      \
+            } else {                                                                    \
+                cn_gen_set_input_timer(0);                                              \
+            }                                                                           \
             struct cn_gen_##Name##_record *res = cn_gen_##Name();                       \
             if (cn_gen_backtrack_type() != CN_GEN_BACKTRACK_NONE) {                     \
                 cn_gen_rand_replace(checkpoint);                                        \
                 i--;                                                                    \
                 d++;                                                                    \
+                recentDiscards++;                                                       \
                 continue;                                                               \
             }                                                                           \
             assume_##Name(__VA_ARGS__);                                                 \
@@ -104,6 +134,7 @@ void cn_trap(void);
             }                                                                           \
             Name(__VA_ARGS__);                                                          \
             cn_gen_rand_replace(checkpoint);                                            \
+            recentDiscards = 0;                                                         \
         }                                                                               \
                                                                                         \
         if (progress_level != CN_TEST_GEN_PROGRESS_NONE) {                              \

runtime/libcn/src/cn-testing/test.c
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <inttypes.h>
 #include <assert.h>
+#include <stdbool.h>
 
 #include <cn-executable/utils.h>
 
@@ -111,6 +112,70 @@ static inline void _cn_trap(void) { __asm__ __volatile__("NOP\n .word 0x10000000
 
 void cn_trap(void) { _cn_trap(); }
 
+size_t cn_gen_compute_size(enum cn_gen_size_strategy strategy, int max_tests, size_t max_size, int max_discard_ratio, int successes, int recent_discards) {
+    switch (strategy) {
+    case CN_GEN_SIZE_QUARTILE:
+        if (successes < max_tests / 4) {
+            max_size /= 4;
+        }
+        else if (successes < max_tests / 2) {
+            max_size /= 2;
+        }
+        else if (successes < 3 * (max_tests / 4)) {
+            max_size /= 4;
+            max_size *= 3;
+        }
+
+    case CN_GEN_SIZE_UNIFORM:
+        ;
+        size_t sz = cn_gen_uniform_cn_bits_u16(max_size + 1)->val + 1;
+        return sz;
+
+    case CN_GEN_SIZE_QUICKCHECK:
+        ;
+        size_t discard_divisor;
+        if (max_discard_ratio > 0) {
+            discard_divisor = (successes * max_discard_ratio / 3);
+            if (discard_divisor < 1) {
+                discard_divisor = 1;
+            }
+            else if (discard_divisor > 10) {
+                discard_divisor = 10;
+            }
+        }
+        else {
+            discard_divisor = 1;
+        }
+
+        size_t potential_size;
+        if ((successes / max_size) * max_size + max_size <= max_tests
+            || successes >= max_tests
+            || max_tests % max_size == 0) {
+            potential_size = (successes % max_tests + recent_discards / discard_divisor);
+        }
+        else {
+            potential_size = (successes % max_size) * max_size
+                / (successes % max_size) + recent_discards / discard_divisor;
+        }
+
+        if (potential_size < max_size) {
+            return potential_size + 1;
+        }
+
+        return max_size + 1;
+    }
+}
+
+struct cn_test_reproduction {
+    size_t size;
+    cn_gen_rand_checkpoint checkpoint;
+};
+
+void cn_test_reproduce(struct cn_test_reproduction* repro) {
+    cn_gen_set_size(repro->size);
+    cn_gen_rand_restore(repro->checkpoint);
+}
+
 int cn_test_main(int argc, char* argv[]) {
     int begin_time = cn_gen_get_milliseconds();
     set_cn_logging_level(CN_LOGGING_NONE);
@@ -123,6 +188,7 @@ int cn_test_main(int argc, char* argv[]) {
     int input_timeout = 5000;
     int exit_fast = 0;
     int trap = 0;
+    enum cn_gen_size_strategy sizing_strategy = 0;
     for (int i = 0; i < argc; i++) {
         char* arg = argv[i];
 
@@ -181,6 +247,10 @@ int cn_test_main(int argc, char* argv[]) {
         else if (strcmp("--trap", arg) == 0) {
             trap = 1;
         }
+        else if (strcmp("--sizing-strategy", arg) == 0) {
+            sizing_strategy = strtoul(argv[i + 1], NULL, 10);
+            i++;
+        }
     }
 
     if (timeout != 0) {
@@ -191,7 +261,7 @@ int cn_test_main(int argc, char* argv[]) {
     cn_gen_srand(seed);
     cn_gen_rand(); // Junk to get something to make a checkpoint from
 
-    cn_gen_rand_checkpoint checkpoints[CN_TEST_MAX_TEST_CASES];
+    struct cn_test_reproduction repros[CN_TEST_MAX_TEST_CASES];
     enum cn_test_result results[CN_TEST_MAX_TEST_CASES];
     memset(results, CN_TEST_SKIP, CN_TEST_MAX_TEST_CASES * sizeof(enum cn_test_result));
 
@@ -207,9 +277,10 @@ int cn_test_main(int argc, char* argv[]) {
             if (progress_level == CN_TEST_GEN_PROGRESS_ALL) {
                 print_test_info(test_case->suite, test_case->name, 0, 0);
             }
-            checkpoints[i] = cn_gen_rand_save();
+            repros[i].size = cn_gen_get_size();
+            repros[i].checkpoint = cn_gen_rand_save();
             cn_gen_set_input_timeout(input_timeout);
-            enum cn_test_result result = test_case->func(progress_level, 0);
+            enum cn_test_result result = test_case->func(false, progress_level, sizing_strategy, 0);
             if (!(results[i] == CN_TEST_PASS && result == CN_TEST_GEN_FAIL)) {
                 results[i] = result;
             }
@@ -224,13 +295,16 @@ int cn_test_main(int argc, char* argv[]) {
                 break;
             case CN_TEST_FAIL:
                 printf("FAILED\n");
+
                 set_cn_logging_level(logging_level);
                 cn_printf(CN_LOGGING_ERROR, "\n");
-                cn_gen_rand_restore(checkpoints[i]);
-                cn_gen_set_input_timeout(0);
-                test_case->func(CN_TEST_GEN_PROGRESS_NONE, trap);
+
+                cn_test_reproduce(&repros[i]);
+                test_case->func(true, CN_TEST_GEN_PROGRESS_NONE, sizing_strategy, trap);
+
                 set_cn_logging_level(CN_LOGGING_NONE);
                 printf("\n");
+
                 break;
             case CN_TEST_GEN_FAIL:
                 printf("FAILED TO GENERATE VALID INPUT\n");

backend/cn/bin/main.ml
@@ -481,6 +481,7 @@ let run_tests
   max_stack_depth
   allowed_depth_failures
   max_generator_size
+  sizing_strategy
   random_size_splits
   allowed_size_split_backtracks
   sized_null
@@ -529,6 +530,7 @@ let run_tests
           max_stack_depth;
           allowed_depth_failures;
           max_generator_size;
+          sizing_strategy;
           random_size_splits;
           allowed_size_split_backtracks;
           sized_null;
@@ -1081,6 +1083,16 @@ module Testing_flags = struct
       & info [ "max-generator-size" ] ~doc)
 
 
+  let sizing_strategy =
+    let doc =
+      "Strategy for deciding test case size (0 = Uniform, 1 = Quartile, 2 = QuickCheck)"
+    in
+    Arg.(
+      value
+      & opt (some int) TestGeneration.default_cfg.sizing_strategy
+      & info [ "sizing-strategy" ] ~doc)
+
+
   let random_size_splits =
     let doc = "Randomly split sizes between recursive generator calls" in
     Arg.(value & flag & info [ "random-size-splits" ] ~doc)
@@ -1168,6 +1180,7 @@ let testing_cmd =
     $ Testing_flags.max_stack_depth
     $ Testing_flags.allowed_depth_failures
     $ Testing_flags.max_generator_size
+    $ Testing_flags.sizing_strategy
     $ Testing_flags.random_size_splits
     $ Testing_flags.allowed_size_split_backtracks
     $ Testing_flags.sized_null

backend/cn/lib/testGeneration/buildScript.ml
@@ -173,6 +173,11 @@ let run () =
             [ "--max-generator-size"; string_of_int max_generator_size ])
           |> Option.to_list
           |> List.flatten)
+       @ (Config.has_sizing_strategy ()
+          |> Option.map (fun sizing_strategy ->
+            [ "--sizing-strategy"; string_of_int sizing_strategy ])
+          |> Option.to_list
+          |> List.flatten)
        @ (if Config.is_sized_null () then
             [ "--sized-null" ]
           else

backend/cn/lib/testGeneration/testGenConfig.ml
@@ -18,6 +18,7 @@ type t =
     max_stack_depth : int option;
     allowed_depth_failures : int option;
     max_generator_size : int option;
+    sizing_strategy : int option;
     random_size_splits : bool;
     allowed_size_split_backtracks : int option;
     sized_null : bool;
@@ -44,6 +45,7 @@ let default =
     max_stack_depth = None;
     allowed_depth_failures = None;
     max_generator_size = None;
+    sizing_strategy = None;
     random_size_splits = false;
     allowed_size_split_backtracks = None;
     sized_null = false;
@@ -90,6 +92,8 @@ let has_max_stack_depth () = !instance.max_stack_depth
 let has_allowed_depth_failures () = !instance.allowed_depth_failures
 
 let has_max_generator_size () = !instance.max_generator_size
+
+let has_sizing_strategy () = !instance.sizing_strategy
 
 let is_random_size_splits () = !instance.random_size_splits
 

backend/cn/lib/testGeneration/testGenConfig.mli
@@ -18,6 +18,7 @@ type t =
     max_stack_depth : int option;
     allowed_depth_failures : int option;
     max_generator_size : int option;
+    sizing_strategy : int option;
     random_size_splits : bool;
     allowed_size_split_backtracks : int option;
     sized_null : bool;
@@ -63,6 +64,8 @@ val has_max_stack_depth : unit -> int option
 val has_allowed_depth_failures : unit -> int option
 
 val has_max_generator_size : unit -> int option
+
+val has_sizing_strategy : unit -> int option
 
 val is_random_size_splits : unit -> bool
 

tests/run-cn-test-gen.sh
@@ -30,7 +30,7 @@ function separator() {
   printf '\n\n'
 }
 
-CONFIGS=("--coverage" "--with-static-hack --coverage --sanitize=undefined" "--sized-null" "--random-size-splits" "--random-size-splits --allowed-size-split-backtracks=10")
+CONFIGS=("--sized-null --sizing-strategy 0" "--coverage --sizing-strategy 1" "--with-static-hack --coverage --sanitize=undefined --sizing-strategy 2" "--random-size-splits" "--random-size-splits --allowed-size-split-backtracks=10")
 
 # For each configuration
 for CONFIG in "${CONFIGS[@]}"; do

commit bb0d566b632e64686a13ac79167323e9d381cee8

[CN-Test-Gen] Fix issue with printing errors
Introduced by ba4a38312

runtime/libcn/include/cn-testing/test.h
@@ -111,7 +111,7 @@ size_t cn_gen_compute_size(enum cn_gen_sizing_strategy strategy,
       if (!replay) {                                                                     \
         cn_gen_set_input_timer(cn_gen_get_milliseconds());                               \
       } else {                                                                           \
-        cn_gen_set_input_timer(0);                                                       \
+        cn_gen_set_input_timeout(0);                                                     \
       }                                                                                  \
       struct cn_gen_##Name##_record* res = cn_gen_##Name();                              \
       if (cn_gen_backtrack_type() != CN_GEN_BACKTRACK_NONE) {                            \

runtime/libcn/src/cn-testing/test.c
@@ -344,7 +344,12 @@ int cn_test_main(int argc, char* argv[]) {
           cn_printf(CN_LOGGING_ERROR, "\n");
 
           cn_test_reproduce(&repros[i]);
-          test_case->func(true, CN_TEST_GEN_PROGRESS_NONE, sizing_strategy, trap);
+          enum cn_test_result replay_result =
+              test_case->func(true, CN_TEST_GEN_PROGRESS_NONE, sizing_strategy, trap);
+          if (replay_result != CN_TEST_FAIL) {
+            fprintf(stderr, "Replay of failure did not fail.\n");
+            abort();
+          }
 
           set_cn_logging_level(CN_LOGGING_NONE);
 