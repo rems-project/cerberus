aa0e6093d76caccb018ef0047c3aa1d7383959f7
Dhruv Makwana <dc-mak@users.noreply.github.com>
2025-03-22 16:47:58 +0000

    Tidy up CN symbol printing functions
    This is the Cerberus part of fixing #427.

--- ocaml_frontend/pprinters/pp_symbol.mli
+++ ocaml_frontend/pprinters/pp_symbol.mli
@@ -4,8 +4,8 @@ open Symbol
 val to_string: sym -> string
 val to_string_pretty: ?is_human:bool -> sym -> string
 
-val to_string_cn: sym -> string
-val to_string_pretty_cn: sym -> string
+(** print_nums is false by default *)
+val to_string_pretty_cn: ?print_nums:bool -> sym -> string
 
 
 val pp_prefix: prefix -> PPrint.document
@@ -13,7 +13,4 @@ val pp_prefix: prefix -> PPrint.document
 
 
 val pp_identifier: ?clever:bool -> Symbol.identifier -> PPrint.document
-
-
-val pp_cn_sym_nums: bool ref
 
