3e0dfa768cc359d2de9c194e84f3c0e9d93f4631
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2025-04-28 00:31:43 +0100

    (cerb_runtime) repairing the discovery of the source tree runtime in build mode

--- util/cerb_runtime.ml
+++ util/cerb_runtime.ml
@@ -6,48 +6,52 @@ let cerblib_pkgname = "cerberus-lib"
     used to override the "detected" runtime path from the CLI for example. *)
 let specified_runtime : string option ref = ref None
 
-(** [find_build_install_path ()] attempts to locate the build-time install
+let (/) = Filename.concat
+
+(** [find_cerblib_sourceroot ()] attempts to locate the build-time source
     directory for Cerberus packages (inside the Dune [_build] directory) using
-    the PATH. Exception [Not_found] is raised in case of failure. *)
-let [@warning "-32"] find_build_install_path : unit -> string = fun _ ->
+    the PATH and by looking for [cerberus-lib.opam]. *)
+let find_cerblib_sourceroot () =
   let path = String.split_on_char ':' (Sys.getenv "PATH") in
-  let suffix_r = Str.regexp {|^\(.*/_build/install/[a-z][a-z\-]*\)/bin$|} in
+  let suffix_r = Str.regexp {|^\(.*\)/_build/install/[a-z][a-z\-]*/bin$|} in
   let rec find_prefix path =
     match path with
-    | []        -> raise Not_found
+    | []        -> None
     | p :: path ->
         if Str.string_match suffix_r p 0
-        then Str.matched_group 1 p
+        then Some (Str.matched_group 1 p)
         else find_prefix path
   in
-  let install_path = find_prefix path in
-  if not Sys.(file_exists install_path && is_directory install_path) then
-    raise Not_found;
-  install_path
+  Option.bind (find_prefix path) (fun z ->
+    if not (Sys.file_exists (z / cerblib_pkgname ^ ".opam")) then
+      None
+    else
+      Some z
+  )
 
 type runtime_loc = {
-  source: [ `SPECIFIED | `ENV_VAR | `BUILD_DIR | `OPAM ];
-  mk: pkg:string -> string;
+  cerblib_sourceroot_opt: string option;
+  mk: pkg:string -> [ `SPECIFIED | `ENV_VAR | `OPAM ] * string;
 }
 
 (** [detect_runtime ()] locates the cerberus runtime. Note that if opam is not
     properly initialized then the function may raise [Failure]. *)
 let detect_runtime : unit -> runtime_loc = fun _ ->
   (* First check for specified runtime. *)
-  let wrap source prefix =
-    { source
-    ; mk= fun ~pkg ->
-        Filename.(concat (concat prefix "lib") (concat pkg "runtime")) } in
+  let wrap ?(check_sourceroot=false) source prefix =
+    { cerblib_sourceroot_opt=
+        if check_sourceroot then
+          (* Then check for build time runtime (in repository). *)
+          find_cerblib_sourceroot ()
+        else None
+    ; mk= fun ~pkg -> source, prefix / "lib" / pkg / "runtime" } in
   match !specified_runtime with Some(path) -> wrap `SPECIFIED path | None ->
   (* Then check the CERB_INSTALL_PREFIX environment variable. *)
   try wrap `ENV_VAR (Sys.getenv "CERB_INSTALL_PREFIX") with Not_found ->
-  (* Then check for build time runtime (in repository). *)
-  (* TODO: temporarily disabling the dune runtime discovery for the CN split
-           because it does not work with pinned opam installs *)
-  (* try wrap `BUILD_DIR (find_build_install_path ()) with Not_found -> *)
   (* Fall back to installed runtime under the opam switch prefix. *)
-  try wrap `OPAM (Sys.getenv "OPAM_SWITCH_PREFIX") with Not_found ->
-  failwith "OPAM_SWITCH_PREFIX not set."
+  try wrap ~check_sourceroot:true `OPAM (Sys.getenv "OPAM_SWITCH_PREFIX")
+  with Not_found ->
+    failwith "OPAM_SWITCH_PREFIX not set."
 
 (** [runtime ()] is a memoised version of [detect_runtime ()]. This means that
     if [Failure] is not raised on its first call, it will not be raised later,
@@ -56,9 +60,17 @@ let runtime : unit -> runtime_loc =
   let runtime = Lazy.from_fun detect_runtime in
   fun _ -> Lazy.force runtime
 
-(** [in_runtime path] appends [path] to the Cerberus runtime path (as returned
-    by [runtime ()]. Exceptions [Failure] can be raised accordingly. *)
+(** [in_runtime pkg path] appends [path] to the Cerberus' package [pkg]
+    (["cerberus-lib"] when absent) runtime path (as returned by [runtime ()].
+    Exceptions [Failure] can be raised accordingly. *)
 let in_runtime ?pkg : string -> string = fun path ->
-  let runtime =
-    (runtime ()).mk ~pkg:(Option.fold ~none:cerblib_pkgname ~some:Fun.id pkg) in
-  Filename.concat runtime path
+  let pkg_is_cerblib, pkg = match pkg with
+    | None -> true, cerblib_pkgname
+    | Some pkg -> String.equal cerblib_pkgname pkg, pkg in
+  let runtime = runtime () in
+  (match runtime.cerblib_sourceroot_opt with
+    | Some sourceroot when pkg_is_cerblib ->
+        sourceroot / "runtime"
+    | _ ->
+        snd (runtime.mk ~pkg)
+  ) / path
