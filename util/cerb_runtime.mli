daea6fc0d11a1d2e52fcb30d3031f68ffecf3252
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2025-04-28 00:31:43 +0100

    (cerb_runtime) repairing the discovery of the source tree runtime in build mode

--- util/cerb_runtime.mli
+++ util/cerb_runtime.mli
@@ -7,26 +7,25 @@
 val specified_runtime : string option ref
 
 type runtime_loc = {
-  source: [ `SPECIFIED | `ENV_VAR | `BUILD_DIR | `OPAM ];
-  mk: pkg:string -> string;
+  cerblib_sourceroot_opt: string option;
+  mk: pkg:string -> [ `SPECIFIED | `ENV_VAR | `OPAM ] * string;
 }
-
-(* TODO: the discovery of dune local runtime is temporarily disabled
-         (see comment in cerb_runtime.ml) *)
 
 (** [runtime ()] detects and returns an absolute path to the Cerberus runtime.
     If [specified_runtime] is set then its value is used, and otherwise if the
     [CERB_INSTALL_PREFIX] environment variable is set then its value is used.
     If none of these are set and the function runs in build mode (i.e., from a
-    program run with [dune exec]) then the runtime is looked for under dune's
-    [_build] directory. Otherwise it is assumed that the runtime is installed
-    under the Opam switch prefix, as part of the Cerberus library folder. Note
-    that this relies on Opam being used and properly configured. The exception
-    [Failure] can be raised if that is not the case. Note also that the actual
-    detection of the runtime only runs for the first call to [runtime ()].
-    In subsequent calls the result is returned directly. *)
+    program run with [dune exec]) in the Cerberus source tree, the local
+    runtime is found by detecting dune's [_build] directory in the [PATH] and
+    the cerberus-lib.opam file. Otherwise it is assumed that the runtime is
+    installed under the Opam switch prefix, as part of the Cerberus library
+    folder. Note that this relies on Opam being used and properly configured.
+    The exception [Failure] can be raised if that is not the case. Note also
+    that the actual detection of the runtime only runs for the first call
+    to [runtime ()]. In subsequent calls the result is returned directly. *)
 val runtime : unit -> runtime_loc
 
-(** [in_runtime path] appends [path] to the Cerberus runtime path (as returned
-    by [runtime ()]. Exceptions [Failure] can be raised accordingly. *)
+(** [in_runtime pkg path] appends [path] to the Cerberus' package [pkg]
+    (["cerberus-lib"] when absent) runtime path (as returned by [runtime ()].
+    Exceptions [Failure] can be raised accordingly. *)
 val in_runtime : ?pkg:string -> string -> string
