5bf3a2bc5f3e2bf5161a7e000e0eea84b69efe8d
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2025-05-24 23:40:26 +0100

    (libc/setjump.h) making it possible for syntax-only runs to include this header

--- runtime/libc/include/setjmp.h
+++ runtime/libc/include/setjmp.h
@@ -1,12 +1,19 @@
 #ifndef	_SETJMP_H_
 #define	_SETJMP_H_
 
-typedef __cerbty_jmp_buf jmp_buf;
+/* TODO: this type is required to be an array type, but with no requirement on
+ * the element type or size. It would be better have some richer builtin
+ * mechanism keeping these opaque.
+ * Making an arbitrary choice for now (from macOS arm64)
+ */
+typedef int jmp_buf[48];
 
 int setjmp(jmp_buf env);
 _Noreturn void longjmp(jmp_buf env, int val);
 
+#if defined(__cerb__) && !defined(__cerb_syntax_only__)
 #error <setjmp.h> is not currently supported by Cerberus
+#endif
 
 #else
 #endif
