326349ab3d6b39b08fd73a27733b7634820ec87b
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2026-05-19 16:49:03 +0100

    (cheri) fix patch script for macos

--- coq/extracted/patch.sh
+++ coq/extracted/patch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-patch -p0 -r 0 -s --read-only=ignore --follow-symlinks -o - $1 -i extracted/CRelationClasses.mli.patch
 
+if patch --version | grep 'GNU' --silent; then
+GNU_ARGS="--read-only=ignore --follow-symlinks"
+fi
+patch -p0 -r 0 -s $GNU_ARGS -o - $1 -i extracted/CRelationClasses.mli.patch
